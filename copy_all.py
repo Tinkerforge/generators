e15cc48b84e9b78df2755af4a9668e9b66421f2b
Matthias Bolte <matthias@tinkerforge.com>
2012-06-05 11:15:50 +0200

    Remove unused dictionary from copy_all script

--- copy_all.py
+++ copy_all.py
@@ -23,31 +23,6 @@ def files_are_not_the_same(src_file, dest_path):
         
     return False
 
-bind_trans = [('brick_imu', 'imu-brick', 'imu', 'IMU_Brick'),
-              ('brick_servo', 'servo-brick', 'servo', 'Servo_Brick'),
-              ('brick_master', 'master-brick', 'master', 'Master_Brick'),
-              ('brick_dc', 'dc-brick', 'dc', 'DC_Brick'),
-              ('brick_stepper', 'stepper-brick', 'stepper', 'Stepper_Brick'),
-              ('bricklet_rotary_poti', 'rotary-poti-bricklet', 'rotary_poti', 'RotaryPoti_Bricklet'),
-              ('bricklet_linear_poti', 'linear-poti-bricklet', 'linear_poti', 'LinearPoti_Bricklet'),
-              ('bricklet_joystick', 'joystick-bricklet', 'joystick', 'Joystick_Bricklet'),
-              ('bricklet_humidity', 'humidity-bricklet', 'humidity', 'Humidity_Bricklet'),
-              ('bricklet_current25', 'current25-bricklet', 'current25', 'Current25_Bricklet'),
-              ('bricklet_current12', 'current12-bricklet', 'current12', 'Current12_Bricklet'),
-              ('bricklet_distance_ir', 'distance-ir-bricklet', 'distance_ir', 'DistanceIR_Bricklet'),
-              ('bricklet_voltage', 'voltage-bricklet', 'voltage', 'Voltage_Bricklet'),
-              ('bricklet_dual_relay', 'dual-relay-bricklet', 'dual_relay', 'DualRelay_Bricklet'),
-              ('bricklet_temperature', 'temperature-bricklet', 'temperature', 'Temperature_Bricklet'),
-              ('bricklet_piezo_buzzer', 'piezo-buzzer-bricklet', 'piezo_buzzer', 'PiezoBuzzer_Bricklet'),
-              ('bricklet_lcd_20x4', 'lcd-20x4-bricklet', 'lcd_20x4', 'LCD20x4_Bricklet'),
-              ('bricklet_lcd_16x2', 'lcd-16x2-bricklet', 'lcd_16x2', 'LCD16x2_Bricklet'),
-              ('bricklet_temperature_ir', 'temperature-ir-bricklet', 'temperature_ir', 'TemperatureIR_Bricklet'),
-              ('bricklet_io16', 'io16-bricklet', 'io16', 'IO16_Bricklet'),
-              ('bricklet_io4', 'io4-bricklet', 'io4', 'IO4_Bricklet'),
-              ('bricklet_ambient_light', 'ambient-light-bricklet', 'ambient_light', 'AmbientLight_Bricklet'),
-              ('bricklet_analog_in', 'analog-in-bricklet', 'analog_in', 'AnalogIn_Bricklet'),
-              ('bricklet_analog_out', 'analog-out-bricklet', 'analog_out', 'AnalogOut_Bricklet')]
-
 path = os.getcwd()
 start_path = path.replace('/generators', '')
 brickv_path_bindings = '{0}/{1}'.format(start_path, 'brickv/src/brickv/bindings')
