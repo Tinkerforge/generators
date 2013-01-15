4e2787928c8d69a612614eb80d7d762b5f5bfd6f
Olaf Lüke <olaf@tinkerforge.com>
2013-01-15 13:27:40 +0100

    Use link to device identifier page instead of whole list

--- configs/brick_commonconfig.py
+++ configs/brick_commonconfig.py
@@ -116,44 +116,7 @@ device identifier.
 
 The position can be '0'-'8' (stack position).
 
-The device identifiers are:
-
-.. csv-table::
- :header: "Device Identifier", "Device Name"
- :widths: 30, 100
-
- "11", "Brick DC"
- "13", "Brick Master"
- "14", "Brick Servo"
- "15", "Brick Stepper"
- "16", "Brick IMU"
- "", ""
- "21", "Bricklet Ambient Light"
- "23", "Bricklet Current12"
- "24", "Bricklet Current25"
- "25", "Bricklet Distance IR"
- "26", "Bricklet Dual Relay"
- "27", "Bricklet Humidity"
- "28", "Bricklet IO-16"
- "29", "Bricklet IO-4"
- "210", "Bricklet Joystick"
- "211", "Bricklet LCD 16x2"
- "212", "Bricklet LCD 20x4"
- "213", "Bricklet Linear Poti"
- "214", "Bricklet Piezo Buzzer"
- "215", "Bricklet Rotary Poti"
- "216", "Bricklet Temperature"
- "217", "Bricklet Temperature IR"
- "218", "Bricklet Voltage"
- "219", "Bricklet Analog In"
- "220", "Bricklet Analog Out"
- "221", "Bricklet Barometer"
- "222", "Bricklet GPS"
- "223", "Bricklet Industrial Digital In 4"
- "224", "Bricklet Industrial Digital Out 4"
- "225", "Bricklet Industrial Quad Relay"
- "226", "Bricklet PTC"
- "227", "Bricklet Voltage/Current"
+The device identifiers can be found :ref:`here <device_identifier>`.
 """,
 'de':
 """
@@ -163,44 +126,7 @@ zurück.
 
 Die Position kann '0'-'8' (Stack Position) sein.
 
-Die Device Identifiers sind:
-
-.. csv-table::
- :header: "Device Identifier", "Device Name"
- :widths: 30, 100
-
- "11", "Brick DC"
- "13", "Brick Master"
- "14", "Brick Servo"
- "15", "Brick Stepper"
- "16", "Brick IMU"
- "", ""
- "21", "Bricklet Ambient Light"
- "23", "Bricklet Current12"
- "24", "Bricklet Current25"
- "25", "Bricklet Distance IR"
- "26", "Bricklet Dual Relay"
- "27", "Bricklet Humidity"
- "28", "Bricklet IO-16"
- "29", "Bricklet IO-4"
- "210", "Bricklet Joystick"
- "211", "Bricklet LCD 16x2"
- "212", "Bricklet LCD 20x4"
- "213", "Bricklet Linear Poti"
- "214", "Bricklet Piezo Buzzer"
- "215", "Bricklet Rotary Poti"
- "216", "Bricklet Temperature"
- "217", "Bricklet Temperature IR"
- "218", "Bricklet Voltage"
- "219", "Bricklet Analog In"
- "220", "Bricklet Analog Out"
- "221", "Bricklet Barometer"
- "222", "Bricklet GPS"
- "223", "Bricklet Industrial Digital In 4"
- "224", "Bricklet Industrial Digital Out 4"
- "225", "Bricklet Industrial Quad Relay"
- "226", "Bricklet PTC"
- "227", "Bricklet Voltage/Current"
+Die Device Identifiers sind :ref:`hier <device_identifier>` zu finden.
 """
 }]
 })
