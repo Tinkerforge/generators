4a698da02dbac7b1abef73f61e8ecc7323eb4a79
Matthias Bolte <matthias@tinkerforge.com>
2013-02-06 11:31:36 +0100

    Update API version of Master Brick due to addition of get/set_long_wifi_key

--- configs/brick_master_config.py
+++ configs/brick_master_config.py
@@ -4,7 +4,7 @@
 
 com = {
     'author': 'Olaf Lüke <olaf@tinkerforge.com>',
-    'api_version': [2, 0, 0],
+    'api_version': [2, 0, 1],
     'category': 'Brick',
     'device_identifier': 13,
     'name': ('Master', 'master', 'Master'),
