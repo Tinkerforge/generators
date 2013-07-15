7eec23a4e6cd34438c6cf3ce5e0096028ebb0a02
Matthias Bolte <matthias@tinkerforge.com>
2013-07-15 14:31:24 +0200

    shell: Add first version

---
+++ shell/generate_shell_doc.py
@@ -0,0 +1,424 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Shell Documentation Generator
+Copyright (C) 2012-2013 Matthias Bolte <matthias@tinkerforge.com>
+Copyright (C) 2011-2013 Olaf Lüke <olaf@tinkerforge.com>
+
+generator_shell_doc.py: Generator for Shell documentation
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+import sys
+import os
+import shutil
+import subprocess
+import glob
+import re
+
+sys.path.append(os.path.split(os.getcwd())[0])
+import common
+import shell_common
+
+device = None
+
+def type_to_pytype(element):
+    type_dict = {
+        'int8': 'int',
+        'uint8': 'int',
+        'int16': 'int',
+        'uint16': 'int',
+        'int32': 'int',
+        'uint32': 'int',
+        'int64': 'int',
+        'uint64': 'int',
+        'bool': 'bool',
+        'char': 'chr',
+        'string': 'str',
+        'float': 'float'
+    }
+
+    t = type_dict[element[1]]
+
+    if element[2] == 1 or t == 'str':
+        return t
+
+    return '[' + ', '.join([t]*element[2]) + ']'
+
+def format_doc(packet):
+    text = common.select_lang(packet.get_doc()[1])
+    cls = device.get_camel_case_name()
+    for other_packet in device.get_packets():
+        name_false = ':func:`{0}`'.format(other_packet.get_camel_case_name())
+        if other_packet.get_type() == 'callback':
+            name_upper = other_packet.get_upper_case_name()
+            name_right = ':sh:attr:`CALLBACK_{1} <{0}.CALLBACK_{1}>`'.format(cls, name_upper)
+        else:
+            name_right = ':sh:func:`{1}() <{0}.{1}>`'.format(cls, other_packet.get_underscore_name())
+        text = text.replace(name_false, name_right)
+
+    text = common.handle_rst_word(text)
+    text = common.handle_rst_if(text, device)
+
+    prefix = device.get_category() + device.get_camel_case_name() + '.'
+    if packet.get_underscore_name() == 'set_response_expected':
+        text += common.format_function_id_constants(prefix, device)
+    else:
+        text += common.format_constants(prefix, packet)
+
+    text += common.format_since_firmware(device, packet)
+
+    return common.shift_right(text, 1)
+
+def make_examples():
+    def title_from_file(f):
+        f = f.replace('example_', '')
+        f = f.replace('.sh', '')
+        s = ''
+        for l in f.split('_'):
+            s += l[0].upper() + l[1:] + ' '
+        return s[:-1]
+
+    return common.make_rst_examples(title_from_file, device, common.path_binding,
+                                    'shell', 'example_', '.sh', 'Shell', 'bash')
+
+def make_parameter_desc(packet, io):
+    desc = '\n'
+    param = ' :param {0}: {1}\n'
+    for element in packet.get_elements(io):
+        t = type_to_pytype(element)
+        desc += param.format(element[0], t)
+
+    return desc
+
+def make_return_desc(packet):
+    ret = ' :rtype: {0}\n'
+    ret_list = []
+    for element in packet.get_elements('out'):
+        ret_list.append(type_to_pytype(element))
+    if len(ret_list) == 0:
+        return ret.format(None)
+    elif len(ret_list) == 1:
+        return ret.format(ret_list[0])
+
+    return ret.format('(' + ', '.join(ret_list) + ')')
+
+def make_object_desc(packet):
+    if len(packet.get_elements('out')) < 2:
+        return ''
+
+    desc = {
+    'en': """
+ The returned namedtuple has the variables {0}.
+""",
+    'de': """
+ Das zurückgegebene namedtuple enthält die Variablen {0}.
+"""
+    }
+
+    and_ = {
+    'en': ' and ',
+    'de': ' und '
+    }
+
+    var = []
+    for element in packet.get_elements('out'):
+        var.append('``{0}``'.format(element[0]))
+
+    if len(var) == 1:
+        return common.select_lang(desc).format(var[0])
+
+    if len(var) == 2:
+        return common.select_lang(desc).format(var[0] + common.select_lang(and_) + var[1])
+
+    return common.select_lang(desc).format(', '.join(var[:-1]) + common.select_lang(and_) + var[-1])
+
+def make_methods(typ):
+    methods = ''
+    func_start = '.. sh:function:: '
+    cls = device.get_camel_case_name()
+    for packet in device.get_packets('function'):
+        if packet.get_doc()[0] != typ:
+            continue
+        name = packet.get_underscore_name()
+        params = shell_common.make_parameter_list(packet)
+        pd = make_parameter_desc(packet, 'in')
+        r = make_return_desc(packet)
+        d = format_doc(packet)
+        obj_desc = make_object_desc(packet)
+        desc = '{0}{1}{2}{3}'.format(pd, r, d, obj_desc)
+        func = '{0}{1}.{2}({3})\n{4}'.format(func_start,
+                                             cls,
+                                             name,
+                                             params,
+                                             desc)
+        methods += func + '\n'
+
+    return methods
+
+def make_callbacks():
+    cbs = ''
+    func_start = '.. sh:attribute:: '
+    cls = device.get_camel_case_name()
+    for packet in device.get_packets('callback'):
+        param_desc = make_parameter_desc(packet, 'out')
+        desc = format_doc(packet)
+
+        func = '{0}{1}.CALLBACK_{2}\n{3}\n{4}'.format(func_start,
+                                                      cls,
+                                                      packet.get_upper_case_name(),
+                                                      param_desc,
+                                                      desc)
+        cbs += func + '\n'
+
+    return cbs
+
+def make_api():
+    create_str = {
+    'en': """
+.. sh:function:: {1}(uid, ipcon)
+
+ :param uid: string
+ :param ipcon: IPConnection
+
+ Creates an object with the unique device ID *uid*:
+
+ .. code-block:: bash
+
+    {0} = {1}("YOUR_DEVICE_UID", ipcon)
+
+ This object can then be used after the IP Connection is connected
+ (see examples :ref:`above <{0}_{2}_shell_examples>`).
+""",
+    'de': """
+.. sh:function:: {1}(uid, ipcon)
+
+ :param uid: string
+ :param ipcon: IPConnection
+
+ Erzeugt ein Objekt mit der eindeutigen Geräte ID *uid*:
+
+ .. code-block:: bash
+
+    {0} = {1}("YOUR_DEVICE_UID", ipcon)
+
+ Dieses Objekt kann benutzt werden, nachdem die IP Connection verbunden ist
+ (siehe Beispiele :ref:`oben <{0}_{2}_shell_examples>`).
+"""
+    }
+
+    register_str = {
+    'en': """
+.. sh:function:: {1}.register_callback(id, callback)
+
+ :param id: int
+ :param callback: callable
+ :rtype: None
+
+ Registers a callback with ID *id* to the function *callback*. The available
+ IDs with corresponding function signatures are listed
+ :ref:`below <{0}_{2}_shell_callbacks>`.
+""",
+    'de': """
+.. sh:function:: {1}.register_callback(id, callback)
+
+ :param id: int
+ :param callback: callable
+ :rtype: None
+
+ Registriert einen Callback mit der ID *id* mit der Funktion *callback*. Die
+ verfügbaren IDs mit den zugehörigen Funktionssignaturen sind
+ :ref:`unten <{0}_{2}_shell_callbacks>` zu finden.
+"""
+    }
+
+    c_str = {
+    'en': """
+.. _{1}_{2}_shell_callbacks:
+
+Callbacks
+^^^^^^^^^
+
+Callbacks can be registered to receive
+time critical or recurring data from the device. The registration is done
+with the :sh:func:`register_callback() <{3}.register_callback>` function of
+the device object. The first parameter is the callback ID and the second
+parameter the callback function:
+
+.. code-block:: bash
+
+    def my_callback(param):
+        print(param)
+
+    {1}.register_callback({3}.CALLBACK_EXAMPLE, my_callback)
+
+The available constants with inherent number and type of parameters are
+described below.
+
+.. note::
+ Using callbacks for recurring events is *always* preferred
+ compared to using getters. It will use less USB bandwidth and the latency
+ will be a lot better, since there is no round trip time.
+
+{0}
+""",
+    'de': """
+.. _{1}_{2}_shell_callbacks:
+
+Callbacks
+^^^^^^^^^
+
+Callbacks können registriert werden um zeitkritische
+oder wiederkehrende Daten vom Gerät zu erhalten. Die Registrierung kann
+mit der Funktion :sh:func:`register_callback() <{3}.register_callback>` des
+Geräte Objektes durchgeführt werden. Der erste Parameter ist die Callback ID
+und der zweite Parameter die Callback-Funktion:
+
+.. code-block:: bash
+
+    def my_callback(param):
+        print(param)
+
+    {1}.register_callback({3}.CALLBACK_EXAMPLE, my_callback)
+
+Die verfügbaren Konstanten mit der dazugehörigen Parameteranzahl und -typen werden
+weiter unten beschrieben.
+
+.. note::
+ Callbacks für wiederkehrende Ereignisse zu verwenden ist
+ *immer* zu bevorzugen gegenüber der Verwendung von Abfragen.
+ Es wird weniger USB-Bandbreite benutzt und die Latenz ist
+ erheblich geringer, da es keine Paketumlaufzeit gibt.
+
+{0}
+"""
+    }
+
+    api = {
+    'en': """
+{0}
+API
+---
+
+All methods listed below are thread-safe.
+
+{1}
+
+{2}
+""",
+    'de': """
+{0}
+API
+---
+
+Alle folgend aufgelisteten Funktionen sind Thread-sicher.
+
+{1}
+
+{2}
+"""
+    }
+
+    const_str = {
+    'en' : """
+Constants
+^^^^^^^^^
+
+.. sh:attribute:: {0}.DEVICE_IDENTIFIER
+
+ This constant is used to identify a {3} {4}.
+
+ The :sh:func:`get_identity() <{3}.get_identity>` function and the
+ :sh:attr:`CALLBACK_ENUMERATE <IPConnection.CALLBACK_ENUMERATE>`
+ callback of the IP Connection have a ``device_identifier`` parameter to specify
+ the Brick's or Bricklet's type.
+""",
+    'de' : """
+Konstanten
+^^^^^^^^^^
+
+.. sh:attribute:: {0}.DEVICE_IDENTIFIER
+
+ Diese Konstante wird verwendet um {2} {3} {4} zu identifizieren.
+
+ Die :sh:func:`get_identity() <{3}.get_identity>` Funktion und der
+ :sh:attr:`CALLBACK_ENUMERATE <IPConnection.CALLBACK_ENUMERATE>`
+ Callback der IP Connection haben ein ``device_identifier`` Parameter um den Typ
+ des Bricks oder Bricklets anzugeben.
+"""
+    }
+
+    cre = common.select_lang(create_str).format(device.get_underscore_name(),
+                                                device.get_camel_case_name(),
+                                                device.get_category().lower())
+    reg = common.select_lang(register_str).format(device.get_underscore_name(),
+                                                  device.get_camel_case_name(),
+                                                  device.get_category().lower())
+
+    bf = make_methods('bf')
+    af = make_methods('af')
+    ccf = make_methods('ccf')
+    c = make_callbacks()
+    api_str = ''
+    if bf:
+        api_str += common.select_lang(common.bf_str).format(cre, bf)
+    if af:
+        api_str += common.select_lang(common.af_str).format(af)
+    if c:
+        api_str += common.select_lang(common.ccf_str).format(reg, ccf)
+        api_str += common.select_lang(c_str).format(c, device.get_underscore_name(),
+                                                    device.get_category().lower(),
+                                                    device.get_camel_case_name())
+
+    article = 'ein'
+    if device.get_category() == 'Brick':
+        article = 'einen'
+    api_str += common.select_lang(const_str).format(device.get_camel_case_name(),
+                                                    device.get_category(),
+                                                    article,
+                                                    device.get_camel_case_name(),
+                                                    device.get_category())
+
+    ref = '.. _{0}_{1}_shell_api:\n'.format(device.get_underscore_name(),
+                                            device.get_category().lower())
+
+    api_desc = ''
+    if 'api' in device.com:
+        api_desc = common.select_lang(device.com['api'])
+
+    return common.select_lang(api).format(ref, api_desc, api_str)
+
+def make_files(device_, directory):
+    global device
+    device = device_
+    file_name = '{0}_{1}_Shell'.format(device.get_camel_case_name(), device.get_category())
+    title = {
+    'en': 'Shell bindings',
+    'de': 'Shell Bindings'
+    }
+    directory = os.path.join(directory, 'doc', common.lang)
+    f = file('{0}/{1}.rst'.format(directory, file_name), "w")
+    f.write(common.make_rst_header(device, 'shell', 'Shell'))
+    f.write(common.make_rst_summary(device, common.select_lang(title)))
+    f.write(make_examples())
+    f.write(make_api())
+
+if __name__ == "__main__":
+    for lang in ['en', 'de']:
+        print("=== Generating %s ===" % lang)
+        common.generate(os.getcwd(), lang, make_files, common.prepare_doc, None, True)
---
+++ shell/generate_shell_zip.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Shell ZIP Generator
+Copyright (C) 2012-2013 Matthias Bolte <matthias@tinkerforge.com>
+Copyright (C) 2011 Olaf Lüke <olaf@tinkerforge.com>
+
+generator_shell_zip.py: Generator for Shell ZIP
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+import sys
+import os
+import shutil
+import subprocess
+import glob
+import re
+
+sys.path.append(os.path.split(os.getcwd())[0])
+import common
+
+device = None
+
+def copy_examples_for_zip():
+    examples = common.find_examples(device, common.path_binding, 'shell', 'example_', '.sh')
+    dest = os.path.join('/tmp/generator/examples/',
+                        device.get_category().lower(),
+                        device.get_underscore_name())
+
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+
+    for example in examples:
+        shutil.copy(example[1], dest)
+
+def make_files(device_, directory):
+    global device
+    device = device_
+
+    copy_examples_for_zip()
+
+def generate(path):
+    # Make temporary generator directory
+    if os.path.exists('/tmp/generator'):
+        shutil.rmtree('/tmp/generator/')
+    os.makedirs('/tmp/generator')
+    os.chdir('/tmp/generator')
+
+    # Copy examples
+    common.generate(path, 'en', make_files, None, None, False)
+    shutil.copy(common.path_binding.replace('/generators/shell', '/doc/en/source/Software/example.sh'),
+                '/tmp/generator/examples/example_enumerate.sh')
+
+    # Copy bindings and readme
+    shutil.copy(path + '/tinkerforge', '/tmp/generator')
+    shutil.copy(path + '/tinkerforge.bash_completion', '/tmp/generator')
+    shutil.copy(path + '/changelog.txt', '/tmp/generator')
+    shutil.copy(path + '/readme.txt', '/tmp/generator')
+
+    # Make zip
+    version = common.get_changelog_version(path)
+    common.make_zip('shell', '/tmp/generator', path, version)
+
+if __name__ == "__main__":
+    generate(os.getcwd())
