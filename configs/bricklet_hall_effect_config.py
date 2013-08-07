b0b4005c83dd51bbf5e75d88e5be59be0418ffc2
Olaf Lüke <olaf@tinkerforge.com>
2013-08-07 13:46:21 +0200

    Add Hall Effect and Line Bricket, remove Water Bricklet (is integrated into Moisture Bricklet in newest version)

---
+++ configs/bricklet_hall_effect_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Hall Effect Bricklet communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 240,
+    'name': ('HallEffect', 'hall_effect', 'Hall Effect'),
+    'manufacturer': 'Tinkerforge',
+    'description': 'TODO',
+    'released': False,
+    'packets': []
+}
+
+
---
+++ configs/bricklet_line_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Line Bricklet communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 241,
+    'name': ('Line', 'line', 'Line'),
+    'manufacturer': 'Tinkerforge',
+    'description': 'TODO',
+    'released': False,
+    'packets': []
+}
+
+
