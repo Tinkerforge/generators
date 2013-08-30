22cee09957b296c086534608fbcf844aadcc7936
Olaf Lüke <olaf@tinkerforge.com>
2013-08-30 17:25:16 +0200

    Add API description for LED Strip Bricklet

--- configs/bricklet_led_strip_config.py
+++ configs/bricklet_led_strip_config.py
@@ -9,7 +9,7 @@ com = {
     'device_identifier': 231,
     'name': ('LEDStrip', 'led_strip', 'LED Strip'),
     'manufacturer': 'Tinkerforge',
-    'description': 'TODO',
+    'description': 'Device to control up to 320 RGB LEDs',
     'released': False,
     'packets': []
 }
@@ -27,9 +27,69 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Sets the *rgb* values for the LEDs with the given *length* starting 
+from *index*.
+
+The maximum length is 16, the index goes from 0 to 319 and the rgb values
+have 8 bits each.
+
+Example: If you set index to 5, length to 3, r to [255, 0, 0], 
+g to [0, 255, 0] and b to [0, 0, 255] the LEDs with index 5, 6
+and 7 will get the color red, green and blue respectively.
+
+The colors will be transfered to actual LEDs when the next
+frame duration ends, see :func:`SetFrameDuration`.
+
+Generic approach: 
+
+* Set the frame duration to a value that represents
+  the number of frames per second you want to achieve. 
+
+* Set all of the LED colors for one frame.
+
+* Wait for the :func:`FrameRendered` callback.
+
+* Set all of the LED colors for next frame.
+
+* Wait for the :func:`FrameRendered` callback.
+
+* and so on.
+
+This approach ensures that you can change the LED colors with
+a fixed frame rate.
 """,
 'de':
 """
+Setzt die *rgb* Werte für die LEDs mit der angegebenen *length* und
+startend vom angegebenen *index*.
+
+Die maximale Länge ist 16. Der Index geht von 0 bis 319 und die
+rgb Werte haben jeweils 8 Bit.
+
+Beispiel: Wenn der Index auf 5, die Länge auf 3, r auf [255, 0, 0],
+g auf [0, 255, 0] und b auf [0, 0, 255] gesetzt wird, werden die LEDs
+mit den Indizes 5, 6 und 7 die Farben Rot, Grün und Blau annehmen.
+
+Die Farben werden auf die tatsächlichen LEDs transferriert wenn die
+nächste *frame duration* abgelaufen ist, siehe :func:`SetFrameDuration`.
+
+Genereller Ansatz:
+
+* Setze *frame duration* auf einen Wert welcher der Anzahl der
+  Bilder pro Sekunde entspricht die erreicht werden sollen.
+
+* Setze alle LEDs für einen Frame.
+
+* Warte auf :func:`FrameRendered` callback.
+
+* Setze alle LEDs für den nächsten Frame.
+
+* Warte auf :func:`FrameRendered` callback.
+
+* Und so weiter.
+
+Dieser Ansatz garantiert das die LED Farben mit einer
+festen Framerate angezeigt werden.
 """
 }]
 })
@@ -46,9 +106,17 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns the rgb with the given *length* starting from the
+given *index*.
+
+The values are the last values that were set by :func:`SetRGBValues`.
 """,
 'de':
 """
+Gibt rgb Werte mit der übergebenen *length* zurück, startend vom
+übergebenen *index*.
+
+Die Werte sind die letzten von :func:`SetRGBValues` gesetzten Werte.
 """
 }]
 })
@@ -61,9 +129,25 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Sets the frame duration in ms.
+
+Example: If you want to achieve 20 frames per second, you should
+set the frame duration to 50ms (50ms * 20 = 1 second). 
+
+For an explanation of the general approach see :func:`SetRGBValues`.
+
+Default value: 100ms (10 frames per second).
 """,
 'de':
 """
+Setzt die *frame duration* (Länge des Frames) in ms.
+
+Beispiel: Wenn 20 Frames pro Sekunde erreicht werden sollen, muss
+die Länge des Frames auf 50ms gesetzt werden (50ms * 20 = 1 Sekunde).
+
+Für eine Erklärung des generellen Ansatzes siehe :func:`SetRGBValues`.
+
+Standardwert: 100ms (10 Frames pro Sekunde).
 """
 }]
 })
@@ -76,9 +160,12 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns the frame duration as set by :func:`SetFrameDuration`.
 """,
 'de':
 """
+Gibt die *frame duration* (Länge des Frames) zurück, wie von
+:func:`SetFrameDuration` gesetzt.
 """
 }]
 })
@@ -91,9 +178,13 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns the current supply voltage of the LEDs. The voltage is given
+in mv. 
 """,
 'de':
 """
+Gibt die aktuelle Versorgungsspannung der LEDs zurück. Die Spannung ist
+in mv angegeben.
 """
 }]
 })
@@ -106,9 +197,21 @@ com['packets'].append({
 'doc': ['c', {
 'en':
 """
+This callback is triggered directly after a new frame is rendered.
+
+You should send the data for the next frame directly after this callback
+was triggered.
+
+For an explanation of the general approach see :func:`SetRGBValues`.
 """,
 'de':
 """
+Dieser Callback wird direkt direkt nachdem ein Frame gerendert wurde ausgelöst.
+
+Die Daten für das nächste Frame sollten direkt nach dem auslösen dieses
+Callbacks übertragen werden.
+
+Für eine Erklärung des generellen Ansatzes siehe :func:`SetRGBValues`.
 """
 }]
 })
