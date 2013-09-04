7e4773dad84849a8010b80114ad3298e208eb5be
Olaf Lüke <olaf@tinkerforge.com>
2013-09-04 14:56:59 +0200

    Add calibration function to Piezo Speaker Bricklet config

--- configs/bricklet_piezo_speaker_config.py
+++ configs/bricklet_piezo_speaker_config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Piezo Buzzer Bricklet communication config
+# Piezo Speaker Bricklet communication config
 
 com = {
     'author': 'Olaf Lüke <olaf@tinkerforge.com>',
@@ -23,40 +23,28 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-Beeps with the given frequency value for the duration in ms. For example: 
-If you set a duration of 1000, with a frequency value of 220
+Beeps with the given frequency for the duration in ms. For example: 
+If you set a duration of 1000, with a frequency value of 2000
 the piezo buzzer will beep for one second with a frequency of
 approximately 2 kHz.
 
-*frequency* can be set between 0 and 512.
+*frequency* can be set between 585 and 7100.
 
-Below you can find a graph that shows the relation between the frequency
-value parameter and a frequency in Hz of the played tone:
-
-.. image:: /Images/Bricklets/bricklet_piezo_speaker_value_to_frequency_graph.png
-   :scale: 100 %
-   :alt: Relation between value and frequency 
-   :align: center
-
+The Piezo Speaker Bricklet can only approximate the frequency, it will play
+the best possible match by applying the calibration (see :func:`Calibrate`).
 """,
 'de':
 """
-Erzeugt einen Piepton mit dem gegebenen Frequenzwert für eine Dauer in ms. 
-Beispiel: Wenn *duration* auf 1000 und *frequency* auf 220 gesetzt wird, 
+Erzeugt einen Piepton mit der gegebenen Frequenz für eine Dauer in ms. 
+Beispiel: Wenn *duration* auf 1000 und *frequency* auf 2000 gesetzt wird, 
 erzeugt der Piezosummer einen Piepton für eine Sekunde mit einer Frequenz 
 von ca. 2 kHz.
 
-*frequency* kann die Werte 0 bis 512 annehmen.
+*frequency* kann die Werte 585 bis 7100 annehmen.
 
-Im folgenden befindet sich ein Graph der die Relation zwischen dem
-angegeben Frequenzwert und der Frequenz in Hz des gespieltens Tons
-darstellt:
-
-.. image:: /Images/Bricklets/bricklet_piezo_speaker_value_to_frequency_graph.png
-   :scale: 100 %
-   :alt: Relation zwischen Wert und Frequenz
-   :align: center
-
+Das Piezo Speaker Bricklet kann die angegebenen Frequenzen nur approximieren,
+es wählt die bestmögliche Zuordnung anhand der Kalibrierung 
+(siehe :func:`Calibrate`).
 """
 }]
 })
@@ -73,7 +61,7 @@ com['packets'].append({
 Sets morse code that will be played by the piezo buzzer. The morse code
 is given as a string consisting of "." (dot), "-" (minus) and " " (space)
 for *dits*, *dahs* and *pauses*. Every other character is ignored.
-The second parameter is the frequency value (see :func:`Beep`).
+The second parameter is the frequency (see :func:`Beep`).
 
 For example: If you set the string "...---...", the piezo buzzer will beep
 nine times with the durations "short short short long long long short 
@@ -87,12 +75,43 @@ Setzt Morsecode welcher vom Piezosummer abgespielt wird. Der Morsecode wird
 als Zeichenkette, mit den Zeichen "." (Punkt), "-" (Minus) und " " (Leerzeichen)
 für *kurzes Signale*, *langes Signale* und *Pausen*. Alle anderen Zeichen
 werden ignoriert.
-Der zweite Parameter ist die Frequenzwert (see :func:`Beep`).
+Der zweite Parameter ist die Frequenz (see :func:`Beep`).
 
 Beispiel: Wenn die Zeichenkette "...---..." gesetzt wird, gibt der Piezosummer neun
 Pieptöne aus mit den Dauern "kurz kurz kurz lang lang lang kurz kurz kurz".
 
 Die maximale Zeichenkettenlänge ist 60.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('Calibrate', 'calibrate'), 
+'elements': [('calibration', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+The Pizeo Speaker Bricklet can play 512 different tones. This function
+plays each tone and measures the exact frequency back. The result is a
+mapping between setting value and frequency. This mapping is stored
+in the EEPROM and loaded on startup.
+
+The Bricklet should come calibrated, you only need to call this
+function (once) every time you reflash the Bricklet plugin.
+""",
+'de':
+"""
+Das Piezo Speaker Bricklet kann 512 unterschiedliche Töne spielen. Diese
+Funktion spielt jeden Ton einmal und misst die exakte Frequenz zurück.
+Das Ergebnis ist eine Zuordnung von Stellwerten zu Frequenzen. Diese
+Zuordnung wird im EEPROM gespeichert und bei jedem start des Bricklets
+geladen.
+
+Das Bricklet sollte bei Auslieferung bereits kalibriert sein. Diese
+Funktion muss lediglich (einmalig) nach jedem neuflashen des Bricklet-Plugins
+ausgeführt werden.
 """
 }]
 })
