f761620a65593693b7ba24d28531f2486a6101e3
Matthias Bolte <matthias@tinkerforge.com>
2014-02-05 15:43:04 +0100

    javascript: Add minimal working version of ZIP generator

--- javascript/generate_javascript_zip.py
+++ javascript/generate_javascript_zip.py
@@ -3,7 +3,7 @@
 
 """
 JavaScript ZIP Generator
-Copyright (C) 2012-2013 Matthias Bolte <matthias@tinkerforge.com>
+Copyright (C) 2012-2014 Matthias Bolte <matthias@tinkerforge.com>
 Copyright (C) 2011 Olaf Lüke <olaf@tinkerforge.com>
 
 generator_javascript_zip.py: Generator for JavaScript ZIP
@@ -38,21 +38,17 @@ class JavaScriptZipGenerator(common.Generator):
         return 'javascript'
 
     def prepare(self):
-        return
-
         common.recreate_directory('/tmp/generator')
-        os.makedirs('/tmp/generator/egg/source/tinkerforge')
-        os.makedirs('/tmp/generator/egg/examples')
+        os.makedirs('/tmp/generator/npn/source/tinkerforge')
+        os.makedirs('/tmp/generator/npn/examples')
 
     def generate(self, device):
-        return
-
         if not device.is_released():
             return
 
         # Copy examples
-        examples = common.find_examples(device, self.get_bindings_root_directory(), self.get_bindings_name(), 'example_', '.js')
-        dest = os.path.join('/tmp/generator/egg/examples', device.get_category().lower(), device.get_underscore_name())
+        examples = common.find_examples(device, self.get_bindings_root_directory(), self.get_bindings_name(), 'Example', '.js')
+        dest = os.path.join('/tmp/generator/npn/examples', device.get_category(), device.get_camel_case_name())
 
         if not os.path.exists(dest):
             os.makedirs(dest)
@@ -61,59 +57,24 @@ class JavaScriptZipGenerator(common.Generator):
             shutil.copy(example[1], dest)
 
     def finish(self):
-        return
-
         root = self.get_bindings_root_directory()
 
         # Copy examples
-        shutil.copy(root.replace('/generators/javascript', '/doc/en/source/Software/example.js'),
-                    '/tmp/generator/egg/examples/example_enumerate.js')
+        #shutil.copy(root.replace('/generators/javascript', '/doc/en/source/Software/Example.js'),
+        #            '/tmp/generator/npn/examples/ExampleEnumerate.js')
 
         # Copy bindings and readme
         for filename in released_files:
-            shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/egg/source/tinkerforge')
+            shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npn/source/tinkerforge')
 
-        shutil.copy(os.path.join(root, 'ip_connection.js'), '/tmp/generator/egg/source/tinkerforge')
-        shutil.copy(os.path.join(root, 'changelog.txt'), '/tmp/generator/egg')
-        shutil.copy(os.path.join(root, 'readme.txt'), '/tmp/generator/egg')
-
-        # Write setup.js
-        version = common.get_changelog_version(root)
-        file('/tmp/generator/egg/source/setup.js', 'wb').write("""
-#!/usr/bin/env python
-
-from setuptools import setup
-
-setup(name='tinkerforge',
-      version='{0}.{1}.{2}',
-      description='TCP/IP based library for Bricks and Bricklets',
-      author='Tinkerforge GmbH',
-      author_email='olaf@tinkerforge.com',
-      url='http://www.tinkerforge.com',
-      packages=['tinkerforge'])
-""".format(*version))
-
-        # Make egg
-        os.chdir('/tmp/generator/egg/source')
-        args = ['/usr/bin/python',
-                'setup.js',
-                'bdist_egg']
-        if subprocess.call(args) != 0:
-            raise Exception("Command '{0}' failed".format(' '.join(args)))
-
-        # Remove build stuff
-        shutil.rmtree('/tmp/generator/egg/source/build')
-        shutil.rmtree('/tmp/generator/egg/source/tinkerforge.egg-info')
-        shutil.copy('/tmp/generator/egg/source/dist/' +
-                    os.listdir('/tmp/generator/egg/source/dist')[0],
-                    '/tmp/generator/egg/tinkerforge.egg')
-        shutil.rmtree('/tmp/generator/egg/source/dist')
-
-        # Make __init__.js
-        file('/tmp/generator/egg/source/tinkerforge/__init__.js', 'wb').write(' ')
+        shutil.copy(os.path.join(root, 'IPConnection.js'), '/tmp/generator/npn/source/tinkerforge')
+        shutil.copy(os.path.join(root, 'Device.js'), '/tmp/generator/npn/source/tinkerforge')
+        shutil.copy(os.path.join(root, 'changelog.txt'), '/tmp/generator/npn')
+        #shutil.copy(os.path.join(root, 'readme.txt'), '/tmp/generator/npn')
 
         # Make zip
-        common.make_zip(self.get_bindings_name(), '/tmp/generator/egg', root, version)
+        version = common.get_changelog_version(root)
+        common.make_zip(self.get_bindings_name(), '/tmp/generator/npn', root, version)
 
 def generate(bindings_root_directory):
     common.generate(bindings_root_directory, 'en', JavaScriptZipGenerator)
