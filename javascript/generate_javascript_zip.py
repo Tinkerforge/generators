f7854c495deddb4a0a01099c92d68de97f80dc32
Matthias Bolte <matthias@tinkerforge.com>
2014-02-21 11:32:02 +0100

    javascript: Fix typo in directory name

--- javascript/generate_javascript_zip.py
+++ javascript/generate_javascript_zip.py
@@ -39,12 +39,12 @@ class JavaScriptZipGenerator(common.Generator):
 
     def prepare(self):
         common.recreate_directory('/tmp/generator')
-        os.makedirs('/tmp/generator/npn/nodejs/source/Tinkerforge')
-        os.makedirs('/tmp/generator/npn/nodejs/examples')
-        os.makedirs('/tmp/generator/npn/nodejs/npm_pkg_dir')
-        os.makedirs('/tmp/generator/npn/nodejs/npm_pkg_dir/lib')
-        os.makedirs('/tmp/generator/npn/browser/source')
-        os.makedirs('/tmp/generator/npn/browser/examples')
+        os.makedirs('/tmp/generator/npm/nodejs/source/Tinkerforge')
+        os.makedirs('/tmp/generator/npm/nodejs/examples')
+        os.makedirs('/tmp/generator/npm/nodejs/npm_pkg_dir')
+        os.makedirs('/tmp/generator/npm/nodejs/npm_pkg_dir/lib')
+        os.makedirs('/tmp/generator/npm/browser/source')
+        os.makedirs('/tmp/generator/npm/browser/examples')
 
     def generate(self, device):
         if not device.is_released():
@@ -53,8 +53,8 @@ class JavaScriptZipGenerator(common.Generator):
         # Copy examples
         examples_nodejs = common.find_examples(device, self.get_bindings_root_directory(), self.get_bindings_name(), 'Example', '.js')
         examples_browser = common.find_examples(device, self.get_bindings_root_directory(), self.get_bindings_name(), 'Example', '.html')
-        dest_nodejs = os.path.join('/tmp/generator/npn/nodejs/examples/', device.get_category(), device.get_camel_case_name())
-        dest_browser = os.path.join('/tmp/generator/npn/browser/examples/', device.get_category(), device.get_camel_case_name())
+        dest_nodejs = os.path.join('/tmp/generator/npm/nodejs/examples/', device.get_category(), device.get_camel_case_name())
+        dest_browser = os.path.join('/tmp/generator/npm/browser/examples/', device.get_category(), device.get_camel_case_name())
 
         if not os.path.exists(dest_nodejs):
             os.makedirs(dest_nodejs)
@@ -72,19 +72,19 @@ class JavaScriptZipGenerator(common.Generator):
 
         # Copy examples
         #shutil.copy(root.replace('/generators/javascript', '/doc/en/source/Software/Example.js'),
-        #            '/tmp/generator/npn/examples/ExampleEnumerate.js')
+        #            '/tmp/generator/npm/examples/ExampleEnumerate.js')
 
         # Copy bindings and readme
         for filename in released_files:
             print filename
             if(filename == os.path.join(root, 'bindings', 'TinkerforgeMain.js')):
-                shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npn/nodejs/npm_pkg_dir/Tinkerforge.js')
+                shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npm/nodejs/npm_pkg_dir/Tinkerforge.js')
                 continue
             if(filename == os.path.join(root, 'bindings', 'BrowserAPI.js')):
-                shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npn/nodejs/source/Tinkerforge/')
+                shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npm/nodejs/source/Tinkerforge/')
                 continue
-            shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npn/nodejs/source/Tinkerforge/')
-            shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npn/nodejs/npm_pkg_dir/lib/')
+            shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npm/nodejs/source/Tinkerforge/')
+            shutil.copy(os.path.join(root, 'bindings', filename), '/tmp/generator/npm/nodejs/npm_pkg_dir/lib/')
 
         dot_version = '.'.join(str(i) for i in common.get_changelog_version(root))
 
@@ -94,16 +94,16 @@ class JavaScriptZipGenerator(common.Generator):
         for i, package_json_line in enumerate(package_json_lines):
             package_json_lines[i] = package_json_line.replace("<TF_API_VERSION>", dot_version)
         package_json_file.close()
-        package_json_file = open('/tmp/generator/npn/nodejs/npm_pkg_dir/package.json', 'wb+')
+        package_json_file = open('/tmp/generator/npm/nodejs/npm_pkg_dir/package.json', 'wb+')
         for package_json_line in package_json_lines:
             package_json_file.write(str(package_json_line))
         package_json_file.close()
 
-        #shutil.copy(os.path.join(root, 'package.json'), '/tmp/generator/npn/nodejs/npm_pkg_dir/package.json')
-        shutil.copy(os.path.join(root, 'README.md'), '/tmp/generator/npn/nodejs/npm_pkg_dir/README.md')
-        shutil.copy(os.path.join(root, 'LICENCE'), '/tmp/generator/npn/nodejs/npm_pkg_dir/LICENCE')
-        shutil.copy(os.path.join(root, 'IPConnection.js'), '/tmp/generator/npn/nodejs/npm_pkg_dir/lib/IPConnection.js')
-        shutil.copy(os.path.join(root, 'Device.js'), '/tmp/generator/npn/nodejs/npm_pkg_dir/lib/Device.js')
+        #shutil.copy(os.path.join(root, 'package.json'), '/tmp/generator/npm/nodejs/npm_pkg_dir/package.json')
+        shutil.copy(os.path.join(root, 'README.md'), '/tmp/generator/npm/nodejs/npm_pkg_dir/README.md')
+        shutil.copy(os.path.join(root, 'LICENCE'), '/tmp/generator/npm/nodejs/npm_pkg_dir/LICENCE')
+        shutil.copy(os.path.join(root, 'IPConnection.js'), '/tmp/generator/npm/nodejs/npm_pkg_dir/lib/IPConnection.js')
+        shutil.copy(os.path.join(root, 'Device.js'), '/tmp/generator/npm/nodejs/npm_pkg_dir/lib/Device.js')
 
         # Replace <TF_API_VERSION> in readme.txt
         readme_txt_file = open(os.path.join(root, 'readme.txt'), 'rb')
@@ -111,48 +111,47 @@ class JavaScriptZipGenerator(common.Generator):
         for i, readme_txt_line in enumerate(readme_txt_lines):
             readme_txt_lines[i] = readme_txt_line.replace("<TF_API_VERSION>", dot_version)
         readme_txt_file.close()
-        readme_txt_file = open('/tmp/generator/npn/readme.txt', 'wb+')
+        readme_txt_file = open('/tmp/generator/npm/readme.txt', 'wb+')
         for readme_txt_line in readme_txt_lines:
             readme_txt_file.write(str(readme_txt_line))
         readme_txt_file.close()
 
-        shutil.copy(os.path.join(root, 'IPConnection.js'), '/tmp/generator/npn/nodejs/source/Tinkerforge')
-        shutil.copy(os.path.join(root, 'Device.js'), '/tmp/generator/npn/nodejs/source/Tinkerforge')
-        shutil.copy(os.path.join(root, 'changelog.txt'), '/tmp/generator/npn')
+        shutil.copy(os.path.join(root, 'IPConnection.js'), '/tmp/generator/npm/nodejs/source/Tinkerforge')
+        shutil.copy(os.path.join(root, 'Device.js'), '/tmp/generator/npm/nodejs/source/Tinkerforge')
+        shutil.copy(os.path.join(root, 'changelog.txt'), '/tmp/generator/npm')
 
         # Copy browser specific files
-        shutil.copy(os.path.join(root, 'es5-shim.js'), '/tmp/generator/npn/nodejs/source/Tinkerforge')
-        shutil.copy(os.path.join(root, 'es5-sham.js'), '/tmp/generator/npn/nodejs/source/Tinkerforge')
+        shutil.copy(os.path.join(root, 'es5-shim.js'), '/tmp/generator/npm/nodejs/source/Tinkerforge')
+        shutil.copy(os.path.join(root, 'es5-sham.js'), '/tmp/generator/npm/nodejs/source/Tinkerforge')
 
         # Make Tinkerforge.js for browser with browserify
-        os.chdir('/tmp/generator/npn/nodejs/source/Tinkerforge/')
+        os.chdir('/tmp/generator/npm/nodejs/source/Tinkerforge/')
         browserify_args = ['browserify']
-        browserify_args.extend(os.listdir('/tmp/generator/npn/nodejs/source/Tinkerforge/'))
+        browserify_args.extend(os.listdir('/tmp/generator/npm/nodejs/source/Tinkerforge/'))
         browserify_args.append('-o')
-        browserify_args.append('/tmp/generator/npn/browser/source/Tinkerforge.js')
+        browserify_args.append('/tmp/generator/npm/browser/source/Tinkerforge.js')
         if subprocess.call(browserify_args) != 0:
             raise Exception("Command '{0}' failed".format(' '.join(browserify_args)))
 
         # Remove browser specific files
-        os.remove('/tmp/generator/npn/nodejs/source/Tinkerforge/BrowserAPI.js')
-        os.remove('/tmp/generator/npn/nodejs/source/Tinkerforge/es5-shim.js')
-        os.remove('/tmp/generator/npn/nodejs/source/Tinkerforge/es5-sham.js')
+        os.remove('/tmp/generator/npm/nodejs/source/Tinkerforge/BrowserAPI.js')
+        os.remove('/tmp/generator/npm/nodejs/source/Tinkerforge/es5-shim.js')
+        os.remove('/tmp/generator/npm/nodejs/source/Tinkerforge/es5-sham.js')
 
         # Generate the NPM package and put it on the root of ZIP archive
-        os.chdir('/tmp/generator/npn/nodejs/npm_pkg_dir')
-        
+        os.chdir('/tmp/generator/npm/nodejs/npm_pkg_dir')
 
         if subprocess.call('npm pack', shell=True) != 0:
             raise Exception("Command npm pack failed")
         
-        shutil.copy(os.path.join('tinkerforge-'+dot_version+'.tgz'), '/tmp/generator/npn/tinkerforge-'+dot_version+'.tgz')    
+        shutil.copy(os.path.join('tinkerforge-'+dot_version+'.tgz'), '/tmp/generator/npm/tinkerforge-'+dot_version+'.tgz')
 
         # Remove directory npm_pkg_dir
-        shutil.rmtree('/tmp/generator/npn/nodejs/npm_pkg_dir/')
+        shutil.rmtree('/tmp/generator/npm/nodejs/npm_pkg_dir/')
 
         # Make zip
         version = common.get_changelog_version(root)
-        common.make_zip(self.get_bindings_name(), '/tmp/generator/npn', root, version)
+        common.make_zip(self.get_bindings_name(), '/tmp/generator/npm', root, version)
 
 def generate(bindings_root_directory):
     common.generate(bindings_root_directory, 'en', JavaScriptZipGenerator)
