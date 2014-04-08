3731af0898dd50ba948ba888cfb97f1ab1b0c0df
Matthias Bolte <matthias@tinkerforge.com>
2014-04-08 10:22:29 +0200

    Update inline documentation for authenticate function

--- php/IPConnection.php
+++ php/IPConnection.php
@@ -613,13 +613,14 @@ class IPConnection
 
     /**
      * Performs an authentication handshake with the connected Brick Daemon or
-     * WIFI/Ethernet Extension. On success the connection switches from
-     * non-authenticated to authenticated state and communication can continue as
-     * normal. On failure the connection gets closed by the server side. Authentication
-     * can fail if the authentication secrets mismatch or if authentication is not
-     * enabled at all on the Brick Daemon or WIFI/Ethernet Extension.
+     * WIFI/Ethernet Extension. If the handshake succeeds the connection switches
+     * from non-authenticated to authenticated state and communication can
+     * continue as normal. If the handshake fails then the connection gets closed.
+     * Authentication can fail if the wrong secret was used or if authentication
+     * is not enabled at all on the Brick Daemon or the WIFI/Ethernet Extension.
      *
-     * For more information about authentication see TODO.
+     * For more information about authentication see
+     * http://www.tinkerforge.com/en/doc/Tutorials/Tutorial_Authentication/Tutorial.html
      *
      * @param string $secret
      *
