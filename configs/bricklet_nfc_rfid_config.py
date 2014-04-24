57f38ac94ecb5b34e5d54a12fe8ac081fe782214
Olaf Lüke <olaf@tinkerforge.com>
2014-04-24 13:44:10 +0200

    Add stub for NFC/RFID Bricklet config

---
+++ configs/bricklet_nfc_rfid_config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Heart Rate Bricklet communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 245,
+    'name': ('NFCRFID', 'nfc_rfid', 'NFC/RFID'),
+    'manufacturer': 'Tinkerforge',
+    'description': 'Device that can read and write NFC and RFID tags',
+    'released': False,
+    'packets': []
+}
+
