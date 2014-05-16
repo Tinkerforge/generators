9efa91ac0ab8e8eb58f510fbbc0d104e2d91fedf
Olaf Lüke <olaf@tinkerforge.com>
2014-05-16 14:49:21 +0200

    Add API for NFC/RFID Bricklet

--- configs/bricklet_nfc_rfid_config.py
+++ configs/bricklet_nfc_rfid_config.py
@@ -18,3 +18,211 @@ com = {
     'packets': []
 }
 
+com['packets'].append({
+'type': 'function',
+'name': ('ReadPassiveTargetID', 'read_passive_target_id'), 
+'elements': [('target_type', 'uint8', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetPassiveTargetID', 'get_passive_target_id'), 
+'elements': [('target_type', 'uint8', 1, 'out'),
+             ('tid_length', 'uint16', 1, 'out'),
+             ('tid', 'uint8', 7, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetState', 'get_state'), 
+'elements': [('state', 'uint8', 1, 'out'),
+             ('idle', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('AuthenticatePageMifareClassic', 'authenticate_page_mifare_classic'), 
+'elements': [('page', 'uint8', 1, 'in'),
+             ('key_number', 'uint8', 1, 'in'),
+             ('key', 'uint8', 7, 'in'),
+             ('tid', 'uint8', 4, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('WritePageMifareClassic', 'write_page_mifare_classic'), 
+'elements': [('page', 'uint8', 1, 'in'),
+             ('data', 'uint8', 16, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('WritePageType1', 'write_page_type1'), 
+'elements': [('page', 'uint8', 1, 'in'),
+             ('data', 'uint8', 8, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('WritePageType2', 'write_page_type2'), 
+'elements': [('page', 'uint8', 1, 'in'),
+             ('data', 'uint8', 4, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('ReadPage', 'read_page'), 
+'elements': [('target_type', 'uint8', 1, 'in'),
+             ('page', 'uint8', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetPageMifareClassic', 'get_page_mifare_classic'), 
+'elements': [('data', 'uint8', 16, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetPageType1', 'get_page_mifare_classic'), 
+'elements': [('data', 'uint8', 8, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetPageType2', 'get_page_mifare_classic'), 
+'elements': [('data', 'uint8', 4, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('NewPassiveTargetID', 'new_passive_target_id'), 
+'elements': [('target_type', 'uint8', 1, 'out'),
+             ('tid_length', 'uint16', 1, 'out'),
+             ('tid', 'uint8', 7, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('StateChanged', 'state_changed'), 
+'elements': [('state', 'uint8', 1, 'out'),
+             ('error', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
