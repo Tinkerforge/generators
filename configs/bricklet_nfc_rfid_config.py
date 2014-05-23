c74d5a46243c07c8e1800fb2b17067401bb2d12a
Olaf Lüke <olaf@tinkerforge.com>
2014-05-23 18:30:21 +0200

    Add German translation for NFC/RFID Bricklet API documentation

--- configs/bricklet_nfc_rfid_config.py
+++ configs/bricklet_nfc_rfid_config.py
@@ -52,9 +52,15 @@ first have to call this function with the expected tag type as parameter.
 It is no problem if you don't know the tag type. You can cycle through 
 the available tag types until the tag gives an answer to the request.
 
-After you call this function the NFC/RFID Bricklet will try to read the tag 
-ID from the tag. After this process is done the state will change. You can 
-either register the :func:`StateChanged` callback or you can poll
+Current the following tag types are supported:
+
+* Mifare Classic
+* NFC Forum Type 1
+* NFC Forum Type 2
+
+After you call :func:`RequestTagID` the NFC/RFID Bricklet will try to read 
+the tag ID from the tag. After this process is done the state will change.
+You can either register the :func:`StateChanged` callback or you can poll
 :func:`GetState` to find out about the state change.
 
 If the state changes to *RequestTagIDError* it means that either there was 
@@ -67,11 +73,42 @@ If two tags are in the proximity of the NFC/RFID Bricklet, this
 function will cycle through the tags. To select a specific tag you have
 to call :func:`RequestTagID` until the correct tag id is found.
 
-In case of any error state the selection is lost and you have to
+In case of any *Error* state the selection is lost and you have to
 start again by calling :func:`RequestTagID`.
 """,
 'de':
 """
+Um ein Tag welches sich in der nähe des NFC/RFID Bricklets befindet zu
+lesen oder zu schreiben muss zuerst diese Funktion mit dem erwarteten
+Tag Typ aufgerufen werden. Es ist kein Problem wenn der Typ nicht bekannt
+ist. Es ist möglich die verügbaren Tag Typen einfach nacheinander
+durchzutesten bis das Tag antwortet.
+
+Aktuell werden die folgenden Tag Typen unterstützt:
+
+* Mifare Classic
+* NFC Forum Type 1
+* NFC Forum Type 2
+
+Beim Aufruf von :func:`RequestTagID` probiert das NFC/RFID Bricklet die tag ID
+eines Tags auszulesen. Nachdem dieser Prozess beendet ist ändert sich
+der Zustand des Bricklets. Es ist möglich den Callback :func:`StateChanged` zu
+registrieren oder den Zustand über :func:`GetState` zu pollen.
+
+Wenn der Zustand auf *RequestTagIDError* wechselt ist ein Fehler aufgetreten.
+Dies bedeutet, dass entweder kein Tag oder kein Tag vom passenden Typ gefunden
+werden konnte. Wenn der Zustand auf *RequestTagIDReady* wechselt ist ein
+kompatibles Tag gefunden worden und die Tag ID wurde gespeichert. Die
+Tag ID kann nun über :func:`GetTagID` ausgelesen werden.
+
+Wenn sich zwei Tags gleichzeitig in der Nähe des NFC/RFID Bricklets befinden
+werden diese nacheinander ausgelesen. Um ein spezifisches Tag zu selektieren
+muss :func:`RequestTagID` so lange aufgerufen werden bis das korrekte Tag
+gefunden wurde.
+
+Falls sich das NFC/RFID Bricklet in einem der *Error* Zustände befindet
+ist die Selektion aufgehoben und :func:`RequestTagID` muss erneut
+aufgerufen werden.
 """
 }]
 })
@@ -90,7 +127,7 @@ com['packets'].append({
 """
 Returns the tag type, tag ID and the length of the tag ID 
 (4 or 7 bytes are possible length). This function can only be called if the
-NFC/RFID is currently in one of the *ready* states. The returned ID
+NFC/RFID is currently in one of the *Ready* states. The returned ID
 is the ID that was saved through the last call of :func:`RequestTagID`.
 
 To get the tag ID of a tag the approach is as follows:
@@ -102,6 +139,18 @@ To get the tag ID of a tag the approach is as follows:
 """,
 'de':
 """
+Gibt den Tag Typ, die Tag ID und die Länge der Tag ID (4 oder 7 Byte
+möglich) zurück. Diese Funktion kann  nur aufgerufen werden wenn
+sich das Bricklet gerade in einem der *Ready*-Zustände befindet. Die
+zurückgegebene ID ist die letzte ID die durch einen Aufruf von 
+:func:`RequestTagID` gefunden wurde.
+
+Der Ansatz um die Tag ID eines Tags zu bekommen sieht wie folgt aus:
+
+* Rufe :func:`RequestTagID` auf
+* Warte auf einen Zustandswechsel auf *RequestTagIDReady* (siehe :func:`GetState` oder :func:`StateChanged`)
+* Rufe :func:`GetTagID` auf
+
 """
 }]
 })
@@ -146,6 +195,23 @@ The same approach is used analogously for the other API functions.
 """,
 'de':
 """
+Gibt den aktuellen Zustand des NFC/RFID Bricklets aus.
+
+Während der Startphase ist der Zustand *Initialization*. Die 
+Initialisierung dauert etwa 20ms. Danach ändert sich der Zustand zu
+*Idle*.
+
+Die Funktionen dieses Bricklets können aufgerufen werden wenn der Zustand
+entweder *Idle* ist oder einer der *Ready* oder *Error*-Zustände
+erreicht wurde.
+
+Beispiel: Wenn :func:`RequestPage` aufgerufen wird, änder sich der
+Zustand zu *RequestPage* solange der Leseprozess noch nicht abgeschlossen
+ist. Danach ändert sich der Zustand zu *RequestPageReady* wenn das lesen
+funktioniert hat oder zu *RequestPageError* wenn nicht. Wenn die Anfrage
+erfolgreich war kann die Page mit :func:`GetPage` abgerufen werden.
+
+Der gleiche Ansatz kann analog für andere API Funktionen verwendet werden.
 """
 }]
 })
@@ -162,7 +228,7 @@ com['packets'].append({
 'en':
 """
 Mifare Classic tags use authentication. If you want to read from or write to
-a Mifare Classic page you have to authenticate it in beforehand.
+a Mifare Classic page you have to authenticate it beforehand.
 Each page can be authenticated with two keys (A and B). A new Mifare Classic
 tag that has not yet been written to can can be accessed with key number A
 and the default key *[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]*.
@@ -179,6 +245,23 @@ The approach to read or write a Mifare Classic page is as follows:
 """,
 'de':
 """
+Mifare Classic Tags nutzen Authentifizierung. Wenn eine Page eines 
+Mifare Classic Tags gelesen oder geschrieben werden soll muss diese
+zuvor Authentifiziert werden. Jede Page kann mit zwei Schlüsseln (A und B)
+authentifiziert werden. Ein neues Mifare Classic Tag welches noch nicht
+beschrieben wurde kann über Schlüsselnummer A mit dem Standardschlüssel
+*[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]* genutzt werden.
+
+Der Ansatz um eine Mifare Classic Page zu lesen oder zu schreiben sieht wie
+folgt aus:
+
+* Rufe :func:`RequestTagID` auf
+* Warte auf einen Zustandswechsel auf *RequestTagIDReady* (siehe :func:`GetState` oder :func:`StateChanged`)
+* Rufe :func:`GetTagID` auf und überprüfe ob Tag ID korrekt ist.
+* Rufe :func:`AuthenticateMifareClassicPage` mit Page und Schlüssel für die Page auf
+* Warte auf einen Zustandswechsel auf *AuthenticatingMifareClassicPageReady*
+* Rufe :func:`RequestPage` oder :func`WritePage` zum lesen/schreiben einer Page auf
+
 """
 }]
 })
@@ -212,6 +295,24 @@ can write to it. See :func:`AuthenticateMifareClassicPage`.
 """,
 'de':
 """
+Schreibt 16 Bytes startend von der übergebenen Page. Wie viele Pages
+dadurch geschrieben werden hängt vom Typ des Tags ab. Die Pagegrößen
+verhalten sich wie folgt:
+
+* Mifare Classic Pagegröße: 16 byte (1 Page wird geschrieben)
+* NFC Forum Type 1 Pagegröße: 8 byte (2 Pages werden geschrieben)
+* NFC Forum Type 2 Pagegröße: 4 byte (4 Pages werden geschrieben)
+
+Der generelle Ansatz zum Schreiben eines Tags sieht wie folgt aus:
+
+* Rufe :func:`RequestTagID` auf
+* Warte auf einen Zustandswechsel auf *RequestTagIDReady* (siehe :func:`GetState` oder :func:`StateChanged`)
+* Rufe :func:`GetTagID` auf und überprüfe ob Tag ID korrekt ist.
+* Rufe :func:`WritePage` mit der Page sowie den zu schreibenden Daten auf.
+* Warte auf einen Zustandswechsel auf *WritePageReady*
+
+Wenn ein Mifare Classic Tag verwendet wird muss die Page authentifiziert
+werden bevor sie geschrieben werden kann. Siehe :func:`AuthenticateMifareClassicPage`.
 """
 }]
 })
@@ -247,6 +348,26 @@ can read it. See :func:`AuthenticateMifareClassicPage`.
 """,
 'de':
 """
+Liest 16 Bytes startend von der übergebenen Page und speichert sie in
+einem Buffer. Dieser Buffer kann mit :func:`GetPage` ausgelesen werden. 
+Wie viele Pages dadurch gelesen werden hängt vom Typ des Tags ab. 
+Die Pagegrößen verhalten sich wie folgt:
+
+* Mifare Classic Pagegröße: 16 byte (1 Page wird gelesen)
+* NFC Forum Type 1 Pagegröße: 8 byte (2 Pages werden gelesen)
+* NFC Forum Type 2 Pagegröße: 4 byte (4 Pages werden gelesen)
+
+Der generelle Ansatz zum Lesen eines Tags sieht wie folgt aus:
+
+* Rufe :func:`RequestTagID` auf
+* Warte auf einen Zustandswechsel auf *RequestTagIDReady* (siehe :func:`GetState` oder :func:`StateChanged`)
+* Rufe :func:`GetTagID` auf und überprüfe ob Tag ID korrekt ist.
+* Rufe :func:`ReadPage` mit der zu lesenden Page auf
+* Warte auf einen Zustandswechsel auf *ReadPageReady*
+* Rufe :func:`GetPage` auf um die gespeicherte Page abzufragen
+
+Wenn ein Mifare Classic Tag verwendet wird muss die Page authentifiziert
+werden bevor sie gelesen werden kann. Siehe :func:`AuthenticateMifareClassicPage`.
 """
 }]
 })
@@ -260,10 +381,13 @@ com['packets'].append({
 'en':
 """
 Returns 16 bytes of data from an internal buffer. To fill the buffer
-you have to call :func:`RequestPage`.
+with specific pages you have to call :func:`RequestPage` beforehand.
 """,
 'de':
 """
+Gibt 16 Bytes Daten aus einem internen Buffer zurück. Der Buffer
+kann zuvor mit spezifischen Pages über einen Aufruf von  
+:func:`RequestPage` gefüllt werden.
 """
 }]
 })
@@ -297,6 +421,9 @@ See :func:`GetState` for more information about the possible states.
 """,
 'de':
 """
+Dieser Callback wird ausgelöst wenn der Zustand des NFC/RFID Bricklets
+sich verändert. Siehe :func:`GetState` für mehr Informationen
+über die möglichen Zustände des Bricklets.
 """
 }]
 })
