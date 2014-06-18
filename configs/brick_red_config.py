5d86c5ae3766bea38a0b98277f999fd69a331bec
Matthias Bolte <matthias@tinkerforge.com>
2014-06-18 18:39:52 +0200

    Start adding RED Brick API

--- configs/brick_red_config.py
+++ configs/brick_red_config.py
@@ -17,3 +17,341 @@ com = {
     'released': False,
     'packets': []
 }
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetLastError', 'get_last_error'),
+'elements': [('error_code', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetObjectType', 'get_object_type'),
+'elements': [('object_id', 'uint16', 1, 'in'),
+             ('object_type', 'int8', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetNextObjectTableEntry', 'get_next_object_table_entry'),
+'elements': [('object_type', 'uint8', 1, 'in'),
+             ('object_id', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('RewindObjectTable', 'rewind_object_table'),
+'elements': [('object_type', 'uint8', 1, 'in'),
+             ('success', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('AcquireString', 'acquire_string'),
+'elements': [('length_to_reserve', 'uint32', 1, 'in'),
+             ('string_id', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('ReleaseString', 'release_string'),
+'elements': [('string_id', 'uint16', 1, 'in'),
+             ('success', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('TruncateString', 'truncate_string'),
+'elements': [('string_id', 'uint16', 1, 'in'),
+             ('length', 'uint32', 1, 'in'),
+             ('success', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetStringLength', 'get_string_length'),
+'elements': [('string_id', 'uint16', 1, 'in'),
+             ('length', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('SetStringChunk', 'set_string_chunk'),
+'elements': [('string_id', 'uint16', 1, 'in'),
+             ('offset', 'uint32', 1, 'in'),
+             ('buffer', 'string', 58, 'in'),
+             ('success', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetStringChunk', 'get_string_chunk'),
+'elements': [('string_id', 'uint16', 1, 'in'),
+             ('offset', 'uint32', 1, 'in'),
+             ('buffer', 'string', 64, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('OpenFile', 'open_file'),
+'elements': [('name_string_id', 'uint16', 1, 'in'),
+             ('flags', 'uint32', 1, 'in', ('FileFlag', 'file_flag', [('ReadOnly', 'read_only', 0x0001),
+                                                                     ('WriteOnly', 'write_only', 0x0002),
+                                                                     ('ReadWrite', 'read_write', 0x0004),
+                                                                     ('Append', 'append', 0x0008),
+                                                                     ('Create', 'create', 0x0010),
+                                                                     ('Truncate', 'truncate', 0x0020)])),
+             ('permissions', 'uint32', 1, 'in'),
+             ('file_id', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('CloseFile', 'close_file'),
+'elements': [('file_id', 'uint16', 1, 'in'),
+             ('success', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('GetFileName', 'get_file_name'),
+'elements': [('file_id', 'uint16', 1, 'in'),
+             ('name_string_id', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('WriteFile', 'write_file'),
+'elements': [('file_id', 'uint16', 1, 'in'),
+             ('buffer', 'uint8', 61, 'in'),
+             ('length_to_write', 'uint8', 1, 'in'),
+             ('length_written', 'int8', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('WriteFileUnchecked', 'write_file_unchecked'),
+'elements': [('file_id', 'uint16', 1, 'in'),
+             ('buffer', 'uint8', 61, 'in'),
+             ('length_to_write', 'uint8', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('WriteFileAsync', 'write_file_async'),
+'elements': [('file_id', 'uint16', 1, 'in'),
+             ('buffer', 'uint8', 61, 'in'),
+             ('length_to_write', 'uint8', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('ReadFile', 'read_file'),
+'elements': [('file_id', 'uint16', 1, 'in'),
+             ('length_to_read', 'uint8', 1, 'in'),
+             ('buffer', 'uint8', 63, 'out'),
+             ('length_read', 'int8', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': ('ReadFileAsync', 'read_file_async'),
+'elements': [('file_id', 'uint16', 1, 'in'),
+             ('length_to_read', 'uint64', 1, 'in'),
+             ('success', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': ('AsyncFileRead', 'async_file_read'),
+'elements': [('file_id', 'uint16', 1, 'out'),
+             ('buffer', 'uint8', 61, 'out'),
+             ('length_read', 'int8', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': ('AsyncFileWrite', 'async_file_write'),
+'elements': [('file_id', 'uint16', 1, 'out'),
+             ('length_written', 'int8', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+""",
+'de':
+"""
+"""
+}]
+})
