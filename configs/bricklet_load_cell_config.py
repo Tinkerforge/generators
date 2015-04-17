cb76afe9fb0d52ee156e1c03aac459d95a4f4968
Olaf Lüke <olaf@tinkerforge.com>
2015-04-17 16:35:36 +0200

    Add missing Load Cell Bricklet API documentation

--- configs/bricklet_load_cell_config.py
+++ configs/bricklet_load_cell_config.py
@@ -21,12 +21,12 @@ com = {
 com['packets'].append({
 'type': 'function',
 'name': ('GetWeight', 'get_weight'), 
-'elements': [('weight', 'uint32', 1, 'out')],
+'elements': [('weight', 'int32', 1, 'out')],
 'since_firmware': [1, 0, 0],
 'doc': ['bf', {
 'en':
 """
-TODO
+Returns the currently measured weight in grams.
 
 If you want to get the weight periodically, it is recommended 
 to use the callback :func:`Weight` and set the period with 
@@ -34,7 +34,7 @@ to use the callback :func:`Weight` and set the period with
 """,
 'de':
 """
-TODO
+Gibt das aktuell gemessene Gewicht in Gramm zurück.
 
 Wenn das Gewicht periodisch abgefragt werden soll, wird empfohlen
 den Callback :func:`Weight` zu nutzen und die Periode mit 
@@ -278,9 +278,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Turns the LED on.
 """,
 'de':
 """
+Aktiviert die LED.
 """
 }]
 })
@@ -293,9 +295,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Turns the LED off.
 """,
 'de':
 """
+Deaktiviert die LED.
 """
 }]
 })
@@ -308,9 +312,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns *true* if the led is on, *false* otherwise.
 """,
 'de':
 """
+Gibt *true* zurück wenn die LED aktiviert ist, *false* sonst.
 """
 }]
 })
@@ -324,11 +330,32 @@ com['packets'].append({
 'doc': ['af', {
 'en':
 """
-* 0   = set zero point
-* > 0 = set gain
+To calibrate your Load Cell Bricklet you have to
+
+* empty the scale and call this function with 0 and
+* add a known weight to the scale and call this function with the weight in 
+  grams.
+
+The calibration is saved in the EEPROM of the Bricklet and only
+needs to be done once.
+
+We recommend to use the Brick Viewer for calibration, you don't need
+to call this function in your source code.
 """,
 'de':
 """
+Zum Kalibrieren des Load Cell Bricklet müssen die folgenden zwei 
+Schritte durchgeführt werden:
+
+* Die Wage leeren und die Funktion mit 0 aufrufen.
+* Eine bekanntes gewicht auf die Waage legen und die Funktion mit dem
+  Gewicht in Gramm aufrufen.
+
+Die Kalibrierung wird auf dem EEPROM des Bricklets gespeichert und muss
+nur einmal gesetzt werden.
+
+Wir empfehlen die Kalibrierung über den Brick Viewer zu setzen, diese
+Funktion muss nicht im Quellcode genutzt werden.
 """
 }]
 })
@@ -336,15 +363,60 @@ com['packets'].append({
 com['packets'].append({
 'type': 'function',
 'name': ('SetConfiguration', 'set_configuration'), 
-'elements': [('rate', 'uint8', 1, 'in'),
-             ('gain', 'uint8', 1, 'in')],
+'elements': [('rate', 'uint8', 1, 'in', ('Rate', 'rate', [('10Hz', '10hz', 0),
+                                                          ('80Hz', '80hz', 1)])),
+             ('gain', 'uint8', 1, 'in', ('Gain', 'gain', [('128x', '128x', 0),
+                                                          ('64x', '64x', 1),
+                                                          ('32x', '32x', 2)]))],
 'since_firmware': [1, 0, 0],
 'doc': ['af', {
 'en':
 """
+The measurement rate and gain are configurable.
+
+The rate can be either 10Hz or 80Hz. A faster rate will produce more noise.
+It is additionally possible to add a moving average
+(see :func:`SetMovingAverage`) to the measurements.
+
+The gain can be 128x, 64x or 32x. It represents a measurement range of
++-20mV, +-40mV and +-80mV respectively. The Load Cell Bricklet uses an
+excitation voltage of 5V and most load cells use an output of 2mV/V. That
+means the voltage range is +-15mV for most load cells (i.e. gain of 128 
+is best). If you don't know what all of this means you should keep it at 
+128x, it will most likely be correct.
+
+The configuration is saved in the EEPROM of the Bricklet and only
+needs to be done once.
+
+We recommend to use the Brick Viewer for configuration, you don't need
+to call this function in your source code.
+
+The default rate is 10Hz and the default gain is 128x.
 """,
 'de':
 """
+Für die Messungen sind Rate und Gain konfigurierbar.
+
+Die Rate kann auf 10Hz oder 80Hz gesetzt werden. Eine schnellere Rate
+erzeugt mehr Störungen. Zusätzlich ist es möglich einen gleitenden
+Mittelwert auf die Werte anzuwenden (siehe :func:`SetMovingAverage`).
+
+Der Gain kann zwischen 128x, 64x und 32x konfiguriert werden. Er
+respräsentiert einenen Messbereich von +-20mV, +-40mV und +-80mV
+respektive. Das Load Cell Bricklet nutzt eine
+Erregerspannung (Excitation Voltage) von 5V und die meisten Wägezellen
+haben eine Ausgabe von 2mV/V. Dies bedeutet, der Spannungsbereich ist
++-15mV für die meisten Wägezellen (d.h. ein Gain von 128 ist am
+geeignetsten). Falls nicht klar ist was dies alles bedeutet, ein
+Gain von 128x ist höchstwahrscheinlich korrekt.
+
+Die Konfiguration wird auf dem EEPROM des Bricklets gespeichert und muss
+nur einmal gesetzt werden.
+
+Wir empfehlen die Konfiguration über den Brick Viewer zu setzen, diese
+Funktion muss nicht im Quellcode genutzt werden.
+
+Die Standardwerte sind 10Hz für die Rate und 128x für den Gain.
 """
 }]
 })
@@ -352,20 +424,20 @@ com['packets'].append({
 com['packets'].append({
 'type': 'function',
 'name': ('GetConfiguration', 'get_configuration'), 
-'elements': [('rate', 'uint8', 1, 'out'),
-             ('gain', 'uint8', 1, 'out')],
+'elements': [('rate', 'uint8', 1, 'out', ('Rate', 'rate', [('10Hz', '10hz', 0),
+                                                           ('80Hz', '80hz', 1)])),
+             ('gain', 'uint8', 1, 'out', ('Gain', 'gain', [('128x', '128x', 0),
+                                                           ('64x', '64x', 1),
+                                                           ('32x', '32x', 2)]))],
 'since_firmware': [1, 0, 0],
 'doc': ['af', {
 'en':
 """
-rate: 0 = 10Hz
-rate: 1 = 80Hz
-gain: 0 = +-3.3*0.5V/128
-gain: 1 = +-3.3*0.5V/64
-gain: 2 = +-3.3*0.5V/32
+Returns the configuration as set by :func:`SetConfiguration`.
 """,
 'de':
 """
+Gibt die Konfiguration zurück, wie von :func:`SetConfiguration` gesetzt.
 """
 }]
 })
