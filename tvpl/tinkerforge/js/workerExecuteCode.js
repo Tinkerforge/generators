c5bd786ecce39110c1838c15a3598a80e58debf8
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-09-25 16:25:14 +0200

    tpvl: Using Babel for ES6 compatibility

--- tvpl/tinkerforge/js/workerExecuteCode.js
+++ tvpl/tinkerforge/js/workerExecuteCode.js
@@ -1,11 +1,17 @@
 importScripts('Tinkerforge.js');
 importScripts('workerProtocol.js');
+importScripts('babelPolyfill.js');
+importScripts('babelRuntime.js');
+importScripts('babelBrowser.js');
 
 handlerOnMessage = function(e) {
   try {
-    eval(e.data);
+    //eval(e.data);
+    babel.run(babel.transform(e.data).code);
   } catch(e) {
-      postMessage(String('ERROR: ' + e + '\n'));
+      postMessage();
+      postMessage(workerProtocolSendMessage(WORKER_PROTOCOL_TYPE_MESSAGE,
+                                            String('ERROR: ' + e + '\n')));
       postMessage(workerProtocolSendMessage(WORKER_PROTOCOL_TYPE_COMMAND,
                                             WORKER_PROTOCOL_TYPE_DATA_COMMAND_END));
   }
