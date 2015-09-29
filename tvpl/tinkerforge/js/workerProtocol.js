bdffe155df11eca68fa9046be9d23298dc3a6d93
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-09-29 16:39:08 +0200

    tvpl: Program end behaviour to support GUI interaction.
    At the end of the program there is yield so the program is still running unless
    explicitly asked to be ended. This will help to support GUI interaction.

--- tvpl/tinkerforge/js/workerProtocol.js
+++ tvpl/tinkerforge/js/workerProtocol.js
@@ -5,7 +5,6 @@ workerProtocol.TYPE_COMMAND_MASTER_CODE_EXEC_START = 2;
 workerProtocol.TYPE_COMMAND_MASTER_CODE_EXEC_STOP = 3;
 workerProtocol.TYPE_COMMAND_WORKER_GENERIC_ERROR = 4;
 workerProtocol.TYPE_COMMAND_WORKER_CODE_EXEC_ERROR = 5;
-workerProtocol.TYPE_COMMAND_WORKER_CODE_EXEC_END = 6;
 
 workerProtocol.isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
