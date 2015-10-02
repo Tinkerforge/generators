ce578cedb21ade728861880deb2eaf86017752c8
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-10-02 15:04:46 +0200

    tvpl: Specify direction in protocol message types

--- tvpl/tinkerforge/js/workerProtocol.js
+++ tvpl/tinkerforge/js/workerProtocol.js
@@ -1,8 +1,13 @@
 var workerProtocol = {};
 
-workerProtocol.TYPE_MESSAGE = 1;
-workerProtocol.TYPE_COMMAND_MASTER_STOP = 2;
-workerProtocol.TYPE_COMMAND_WORKER_ERROR = 3;
+/*
+ * Requests are master --> worker
+ * Responses are master <-- worker
+ */
+workerProtocol.TYPE_REQUEST_CALL_FUNCTION = 1;
+workerProtocol.TYPE_REQUEST_STOP_EXECUTION = 2;
+workerProtocol.TYPE_RESPONSE_ERROR = 3;
+workerProtocol.TYPE_RESPONSE_CONSOLE_MESSAGE = 4;
 
 workerProtocol.isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
