255cf3cb75ac60f82ef68f1ae4a0ae35b7cd9281
Matthias Bolte <matthias@tinkerforge.com>
2015-10-16 10:32:51 +0200

    shell: Dispatch callbacks in background in examples

--- shell/example-authenticate.sh
+++ shell/example-authenticate.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-tinkerforge --secret "My Authentication Secret" enumerate
+# Connects to localhost:4223 by default, use --host and --port to change this
+
+# Trigger enumerate with authentication
+tinkerforge --secret "My Authentication Secret" enumerate &
+
+echo "Press key to exit"; read dummy
+
+kill -- -$$ # Stop callback dispatch in background
--- shell/example-enumerate.sh
+++ shell/example-enumerate.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-tinkerforge enumerate
+# Connects to localhost:4223 by default, use --host and --port to change this
+
+# Trigger enumerate
+tinkerforge enumerate &
+
+echo "Press key to exit"; read dummy
+
+kill -- -$$ # Stop callback dispatch in background
