35a6f8a57119627c0fc24eab69237940399e00af
Olaf Lüke <olaf@tinkerforge.com>
2015-12-23 13:19:26 +0100

    Add configs for UV Light and Motorized Poti

---
+++ configs/bricklet_motorized_poti_config.py
@@ -0,0 +1,587 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Motorized Poti Bricklet communication config
+
+from commonconstants import THRESHOLD_OPTION_CONSTANTS
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 267,
+    'name': ('Motorized Poti', 'Motorized Poti', 'Motorized Poti Bricklet'),
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'TODO',
+        'de': 'TODO'
+    },
+    'released': False,
+    'packets': [],
+    'examples': []
+}
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Position',
+'elements': [('Position', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Returns the position of the Linear Potentiometer. The value is  
+between 0 (slider down) and 100 (slider up).
+
+If you want to get the position periodically, it is recommended to use the
+callback :func:`Position` and set the period with 
+:func:`SetPositionCallbackPeriod`.
+""",
+'de':
+"""
+Gibt die Position des Linearpotentiometers zurück. Der Wertebereich
+ist von 0 (Schieberegler unten) und 100 (Schieberegler oben).
+
+Wenn die Position periodisch abgefragt werden soll, wird empfohlen
+den Callback :func:`Position` zu nutzen und die Periode mit 
+:func:`SetPositionCallbackPeriod` vorzugeben.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Analog Value',
+'elements': [('Value', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+Returns the value as read by a 12-bit analog-to-digital converter.
+The value is between 0 and 4095.
+
+.. note::
+ The value returned by :func:`GetPosition` is averaged over several samples
+ to yield less noise, while :func:`GetAnalogValue` gives back raw
+ unfiltered analog values. The only reason to use :func:`GetAnalogValue` is,
+ if you need the full resolution of the analog-to-digital converter.
+
+If you want the analog value periodically, it is recommended to use the 
+callback :func:`AnalogValue` and set the period with 
+:func:`SetAnalogValueCallbackPeriod`.
+""",
+'de':
+"""
+Gibt den Wert, wie vom 12-Bit Analog-Digital-Wandler gelesen, zurück. Der
+Wertebereich ist 0 bis 4095.
+
+.. note::
+ Der von :func:`GetPosition` zurückgegebene Wert ist über mehrere
+ Messwerte gemittelt um das Rauschen zu vermindern, während :func:`GetAnalogValue`
+ unverarbeitete Analogwerte zurück gibt. Der einzige Grund :func:`GetAnalogValue`
+ zu nutzen, ist die volle Auflösung des Analog-Digital-Wandlers zu erhalten.
+ 
+Wenn der Analogwert periodisch abgefragt werden soll, wird empfohlen
+den Callback :func:`AnalogValue` zu nutzen und die Periode mit 
+:func:`SetAnalogValueCallbackPeriod` vorzugeben.
+"""
+}]
+})
+
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Position Callback Period',
+'elements': [('Period', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the :func:`Position` callback is triggered
+periodically. A value of 0 turns the callback off.
+
+:func:`Position` is only triggered if the position has changed since the
+last triggering.
+
+The default value is 0.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher der :func:`Position` Callback ausgelöst wird.
+Ein Wert von 0 deaktiviert den Callback.
+
+:func:`Position` wird nur ausgelöst wenn sich die Position seit der
+letzten Auslösung geändert hat.
+
+Der Standardwert ist 0.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Position Callback Period',
+'elements': [('Period', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the period as set by :func:`SetPositionCallbackPeriod`.
+""",
+'de':
+"""
+Gibt die Periode zurück, wie von :func:`SetPositionCallbackPeriod`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Analog Value Callback Period',
+'elements': [('Period', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the :func:`AnalogValue` callback is triggered
+periodically. A value of 0 turns the callback off.
+
+:func:`AnalogValue` is only triggered if the analog value has changed since the
+last triggering.
+
+The default value is 0.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher der :func:`AnalogValue` Callback ausgelöst wird.
+Ein Wert von 0 deaktiviert den Callback.
+
+:func:`AnalogValue` wird nur ausgelöst wenn sich der Analogwert seit der
+letzten Auslösung geändert hat.
+
+Der Standardwert ist 0.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Analog Value Callback Period',
+'elements': [('Period', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the period as set by :func:`SetAnalogValueCallbackPeriod`.
+""",
+'de':
+"""
+Gibt die Periode zurück, wie von :func:`SetAnalogValueCallbackPeriod`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Position Callback Threshold',
+'elements': [('Option', 'char', 1, 'in', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'uint16', 1, 'in'),
+             ('Max', 'uint16', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the thresholds for the :func:`PositionReached` callback. 
+
+The following options are possible:
+
+.. csv-table::
+ :header: "Option", "Description"
+ :widths: 10, 100
+
+ "'x'",    "Callback is turned off"
+ "'o'",    "Callback is triggered when the position is *outside* the min and max values"
+ "'i'",    "Callback is triggered when the position is *inside* the min and max values"
+ "'<'",    "Callback is triggered when the position is smaller than the min value (max is ignored)"
+ "'>'",    "Callback is triggered when the position is greater than the min value (max is ignored)"
+
+The default value is ('x', 0, 0).
+""",
+'de':
+"""
+Setzt den Schwellwert für den :func:`PositionReached` Callback.
+
+Die folgenden Optionen sind möglich:
+
+.. csv-table::
+ :header: "Option", "Beschreibung"
+ :widths: 10, 100
+ 
+ "'x'",    "Callback ist inaktiv"
+ "'o'",    "Callback wird ausgelöst wenn die Position *außerhalb* der min und max Werte ist"
+ "'i'",    "Callback wird ausgelöst wenn die Position *innerhalb* der min und max Werte ist"
+ "'<'",    "Callback wird ausgelöst wenn die Position kleiner als der min Wert ist (max wird ignoriert)"
+ "'>'",    "Callback wird ausgelöst wenn die Position größer als der min Wert ist (max wird ignoriert)"
+ 
+Der Standardwert ist ('x', 0, 0).
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Position Callback Threshold',
+'elements': [('Option', 'char', 1, 'out', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'uint16', 1, 'out'),
+             ('Max', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the threshold as set by :func:`SetPositionCallbackThreshold`.
+""",
+'de':
+"""
+Gibt den Schwellwert zurück, wie von :func:`SetPositionCallbackThreshold`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Analog Value Callback Threshold',
+'elements': [('Option', 'char', 1, 'in', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'uint16', 1, 'in'),
+             ('Max', 'uint16', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the thresholds for the :func:`AnalogValueReached` callback. 
+
+The following options are possible:
+
+.. csv-table::
+ :header: "Option", "Description"
+ :widths: 10, 100
+
+ "'x'",    "Callback is turned off"
+ "'o'",    "Callback is triggered when the analog value is *outside* the min and max values"
+ "'i'",    "Callback is triggered when the analog value is *inside* the min and max values"
+ "'<'",    "Callback is triggered when the analog value is smaller than the min value (max is ignored)"
+ "'>'",    "Callback is triggered when the analog value is greater than the min value (max is ignored)"
+
+The default value is ('x', 0, 0).
+""",
+'de':
+"""
+Setzt den Schwellwert für den :func:`AnalogValueReached` Callback.
+
+Die folgenden Optionen sind möglich:
+
+.. csv-table::
+ :header: "Option", "Beschreibung"
+ :widths: 10, 100
+ 
+ "'x'",    "Callback ist inaktiv"
+ "'o'",    "Callback wird ausgelöst wenn der Analogwert *außerhalb* der min und max Werte ist"
+ "'i'",    "Callback wird ausgelöst wenn der Analogwert *innerhalb* der min und max Werte ist"
+ "'<'",    "Callback wird ausgelöst wenn der Analogwert kleiner als der min Wert ist (max wird ignoriert)"
+ "'>'",    "Callback wird ausgelöst wenn der Analogwert größer als der min Wert ist (max wird ignoriert)"
+ 
+Der Standardwert ist ('x', 0, 0).
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Analog Value Callback Threshold',
+'elements': [('Option', 'char', 1, 'out', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'uint16', 1, 'out'),
+             ('Max', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the threshold as set by :func:`SetAnalogValueCallbackThreshold`.
+""",
+'de':
+"""
+Gibt den Schwellwert zurück, wie von :func:`SetAnalogValueCallbackThreshold`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Debounce Period',
+'elements': [('Debounce', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the threshold callbacks
+
+* :func:`PositionReached`,
+* :func:`AnalogValueReached`
+
+are triggered, if the thresholds
+
+* :func:`SetPositionCallbackThreshold`,
+* :func:`SetAnalogValueCallbackThreshold`
+
+keep being reached.
+
+The default value is 100.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher die Schwellwert Callbacks
+
+* :func:`PositionReached`,
+* :func:`AnalogValueReached`
+ 
+ausgelöst werden, wenn die Schwellwerte 
+
+* :func:`SetPositionCallbackThreshold`,
+* :func:`SetAnalogValueCallbackThreshold`
+ 
+weiterhin erreicht bleiben.
+
+Der Standardwert ist 100.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Debounce Period',
+'elements': [('Debounce', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the debounce period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Gibt die Entprellperiode zurück, wie von :func:`SetDebouncePeriod`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Position',
+'elements': [('Position', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered periodically with the period that is set by
+:func:`SetPositionCallbackPeriod`. The :word:`parameter` is the position of the
+Linear Potentiometer.
+
+:func:`Position` is only triggered if the position has changed since the
+last triggering.
+""",
+'de':
+"""
+Dieser Callback wird mit der Periode, wie gesetzt mit :func:`SetPositionCallbackPeriod`,
+ausgelöst. Der :word:`parameter` ist die Position des Linearpotentiometers.
+
+:func:`Position` wird nur ausgelöst wenn sich die Position seit der
+letzten Auslösung geändert hat.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Analog Value',
+'elements': [('Value', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered periodically with the period that is set by
+:func:`SetAnalogValueCallbackPeriod`. The :word:`parameter` is the analog value of the
+Linear Potentiometer.
+
+:func:`AnalogValue` is only triggered if the position has changed since the
+last triggering.
+""",
+'de':
+"""
+Dieser Callback wird mit der Periode, wie gesetzt mit :func:`SetAnalogValueCallbackPeriod`,
+ausgelöst. Der :word:`parameter` ist der Analogwert des Linearpotentiometers.
+
+:func:`AnalogValue` wird nur ausgelöst wenn sich der Analogwert seit der
+letzten Auslösung geändert hat.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Position Reached',
+'elements': [('Position', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered when the threshold as set by
+:func:`SetPositionCallbackThreshold` is reached.
+The :word:`parameter` is the position of the Linear Potentiometer.
+
+If the threshold keeps being reached, the callback is triggered periodically
+with the period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn der Schwellwert, wie von 
+:func:`SetPositionCallbackThreshold` gesetzt, erreicht wird.
+Der :word:`parameter` ist die Position des Linearpotentiometers.
+
+Wenn der Schwellwert erreicht bleibt, wird der Callback mit der Periode, wie
+mit :func:`SetDebouncePeriod` gesetzt, ausgelöst.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Analog Value Reached',
+'elements': [('Value', 'uint16', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered when the threshold as set by
+:func:`SetAnalogValueCallbackThreshold` is reached.
+The :word:`parameter` is the analog value of the Linear Potentiometer.
+
+If the threshold keeps being reached, the callback is triggered periodically
+with the period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn der Schwellwert, wie von 
+:func:`SetAnalogValueCallbackThreshold` gesetzt, erreicht wird.
+Der :word:`parameter` ist der Analogwert des Linearpotentiometers.
+
+Wenn der Schwellwert erreicht bleibt, wird der Callback mit der Periode, wie
+mit :func:`SetDebouncePeriod` gesetzt, ausgelöst.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Motor Position',
+'elements': [('Position', 'uint16', 1, 'in'),
+             ('Disable After Reach', 'bool', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+TODO
+""",
+'de':
+"""
+TODO
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Motor Position',
+'elements': [('Position', 'uint16', 1, 'out'),
+             ('Disable After Reach', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+TODO
+""",
+'de':
+"""
+TODO
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Enable Motor',
+'elements': [],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+TODO
+""",
+'de':
+"""
+TODO
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Disable Motor',
+'elements': [],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+TODO
+""",
+'de':
+"""
+TODO
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Is Motor Enabled',
+'elements': [('Enabled', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+TODO
+""",
+'de':
+"""
+TODO
+"""
+}]
+})
+
+# TODO: Add API: Max Motor Position Offset (1-X)
+
+
+com['examples'].append({
+'name': 'Simple',
+'functions': [('getter', ('Get Position', 'position'), [(('Position', 'Position'), 'uint16', None, None, None, (0, 100))], [])]
+})
+
+com['examples'].append({
+'name': 'Callback',
+'functions': [('callback', ('Position', 'position'), [(('Position', 'Position'), 'uint16', None, None, None, (0, 100))], None, None),
+              ('callback_period', ('Position', 'position'), [], 50)]
+})
+
---
+++ configs/bricklet_uv_light_config.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# UV Light Bricklet communication config
+
+from commonconstants import THRESHOLD_OPTION_CONSTANTS
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 265,
+    'name': ('UV Light', 'UV Light', 'UV Light Bricklet'),
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'Measures UV Light',
+        'de': 'Misst UV Light'
+    },
+    'released': False,
+    'packets': [],
+    'examples': []
+}
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get UV Light',
+'elements': [('UV Light', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Returns the UV Light intensity of the sensor, the intensity is given 
+in µW/cm².
+
+To get UV Index you have to divide the value by 250. E.g. a UV Light
+intensity of 500µW/cm² is equivalent to an UV Index of 2.
+
+If you want to get the intensity periodically, it is recommended to use the
+callback :func:`UVLight` and set the period with 
+:func:`SetUVLightCallbackPeriod`.
+""",
+'de':
+"""
+Gibt die UV-Licht-Intensität des Sensors zurück. Die Intensität wird
+in der Einheit µW/cm² gegeben.
+
+Die Intensität kann einfach durch 250 geteilt werden um den UV Index zu
+bestimmen. Beispiel: Eine UV-Lich-Intensität von 500µW/cm² entspricht
+einem UV Index von 2.
+
+Wenn die Intensität periodisch abgefragt werden soll, wird empfohlen
+den Callback :func:`UVLight` zu nutzen und die Periode mit 
+:func:`SetUVLightCallbackPeriod` vorzugeben.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set UV Light Callback Period',
+'elements': [('Period', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the :func:`UVLight` callback is triggered
+periodically. A value of 0 turns the callback off.
+
+:func:`UVLight` is only triggered if the intensity has changed since the
+last triggering.
+
+The default value is 0.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher der :func:`UVLight` Callback ausgelöst wird.
+Ein Wert von 0 deaktiviert den Callback.
+
+:func:`UVLight` wird nur ausgelöst wenn sich die Intensität seit der
+letzten Auslösung geändert hat.
+
+Der Standardwert ist 0.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get UV Light Callback Period',
+'elements': [('Period', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the period as set by :func:`SetUVLightCallbackPeriod`.
+""",
+'de':
+"""
+Gibt die Periode zurück, wie von :func:`SetUVLightCallbackPeriod`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set UV Light Callback Threshold', 
+'elements': [('Option', 'char', 1, 'in', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'uint32', 1, 'in'),
+             ('Max', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the thresholds for the :func:`UVLightReached` callback. 
+
+The following options are possible:
+
+.. csv-table::
+ :header: "Option", "Description"
+ :widths: 10, 100
+
+ "'x'",    "Callback is turned off"
+ "'o'",    "Callback is triggered when the intensity is *outside* the min and max values"
+ "'i'",    "Callback is triggered when the intensity is *inside* the min and max values"
+ "'<'",    "Callback is triggered when the intensity is smaller than the min value (max is ignored)"
+ "'>'",    "Callback is triggered when the intensity is greater than the min value (max is ignored)"
+
+The default value is ('x', 0, 0).
+""",
+'de':
+"""
+Setzt den Schwellwert für den :func:`UVLightReached` Callback.
+
+Die folgenden Optionen sind möglich:
+
+.. csv-table::
+ :header: "Option", "Beschreibung"
+ :widths: 10, 100
+ 
+ "'x'",    "Callback ist inaktiv"
+ "'o'",    "Callback wird ausgelöst wenn die Intensität *außerhalb* des min und max Wertes ist"
+ "'i'",    "Callback wird ausgelöst wenn die Intensität *innerhalb* des min und max Wertes ist"
+ "'<'",    "Callback wird ausgelöst wenn die Intensität kleiner als der min Wert ist (max wird ignoriert)"
+ "'>'",    "Callback wird ausgelöst wenn dieIntensität  größer als der min Wert ist (max wird ignoriert)"
+ 
+Der Standardwert ist ('x', 0, 0).
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get UV Light Callback Threshold',
+'elements': [('Option', 'char', 1, 'out', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'uint32', 1, 'out'),
+             ('Max', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the threshold as set by :func:`SetUVLightCallbackThreshold`.
+""",
+'de':
+"""
+Gibt den Schwellwert zurück, wie von :func:`SetUVLightCallbackThreshold`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Debounce Period',
+'elements': [('Debounce', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the threshold callbacks
+
+* :func:`UVLightReached`,
+
+are triggered, if the thresholds
+
+* :func:`SetUVLightCallbackThreshold`,
+
+keep being reached.
+
+The default value is 100.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher die Schwellwert Callbacks
+
+* :func:`UVLightReached`,
+ 
+ausgelöst werden, wenn die Schwellwerte 
+
+* :func:`SetUVLightCallbackThreshold`,
+ 
+weiterhin erreicht bleiben.
+
+Der Standardwert ist 100.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Debounce Period',
+'elements': [('Debounce', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the debounce period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Gibt die Entprellperiode zurück, wie von :func:`SetDebouncePeriod`
+gesetzt.
+"""
+}]
+})
+
+
+com['packets'].append({
+'type': 'callback',
+'name': 'UV Light',
+'elements': [('UV Light', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered periodically with the period that is set by
+:func:`SetUVLightCallbackPeriod`. The :word:`parameter` is the UV Light 
+intensity of the sensor.
+
+:func:`UVLight` is only triggered if the intensity has changed since the
+last triggering.
+""",
+'de':
+"""
+Dieser Callback wird mit der Periode, wie gesetzt mit :func:`SetUVLightCallbackPeriod`,
+ausgelöst. Der :word:`parameter` ist die UV-Licht-Intensität des Sensors.
+
+:func:`UVLight` wird nur ausgelöst wenn sich die Intensität seit der
+letzten Auslösung geändert hat.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'UV Light Reached',
+'elements': [('UV Light', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered when the threshold as set by
+:func:`SetUVLightCallbackThreshold` is reached.
+The :word:`parameter` is the UV Light intensity of the sensor.
+
+If the threshold keeps being reached, the callback is triggered periodically
+with the period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn der Schwellwert, wie von 
+:func:`SetUVLightCallbackThreshold` gesetzt, erreicht wird.
+Der :word:`parameter` ist die UV-Licht-Intensität des Sensors.
+
+Wenn der Schwellwert erreicht bleibt, wird der Callback mit der Periode, wie
+mit :func:`SetDebouncePeriod` gesetzt, ausgelöst.
+"""
+}]
+})
+
+com['examples'].append({
+'name': 'Simple',
+'functions': [('getter', ('Get UV Light', 'uv light'), [(('UV Light', 'UV Light'), 'uint32', None, 'µW/cm²', 'µW/cm²', None)], [])]
+})
+
+com['examples'].append({
+'name': 'Callback',
+'functions': [('callback', ('UV Light', 'uv light'), [(('UV Light', 'UV Light'), 'uint32', None, 'µW/cm²', 'µW/cm²', None)], None, None),
+              ('callback_period', ('UV Light', 'uv light'), [], 1000)]
+})
+
+com['examples'].append({
+'name': 'Threshold',
+'functions': [('debounce_period', 10000),
+              ('callback', ('UV Light Reached', 'uv light reached'), [(('UV Light', 'UV Light'), 'uint32', None, 'µW/cm²', 'µW/cm²', None)], None, 'UV Index > 3. Use sunscreen!'),
+              ('callback_threshold', ('UV Light', 'uv light'), [], '>', [(250*3, 0)])]
+})
