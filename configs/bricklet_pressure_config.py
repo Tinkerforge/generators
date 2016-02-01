364d4d9d24a9f49e8894cf7d04dbfbc74af154e4
Matthias Bolte <matthias@tinkerforge.com>
2016-02-01 14:10:14 +0100

    Add Pressure Bricklet config

---
+++ configs/bricklet_pressure_config.py
@@ -0,0 +1,587 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Pressure Bricklet communication config
+
+from commonconstants import THRESHOLD_OPTION_CONSTANTS
+
+com = {
+    'author': 'Matthias Bolte <matthias@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 269,
+    'name': ('Pressure', 'Pressure', 'Pressure Bricklet'),
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'Measures pressure with various pressure sensors',
+        'de': 'Misst Druck mit verschiedenen Drucksensoren'
+    },
+    'released': False,
+    'packets': [],
+    'examples': []
+}
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Pressure',
+'elements': [('Pressure', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Returns the measured pressure in Pa.
+
+If you want to get the pressure periodically, it is recommended to use the
+callback :func:`Pressure` and set the period with
+:func:`SetPressureCallbackPeriod`.
+""",
+'de':
+"""
+Gibt den gemessenen Druck in Pa zurück.
+
+Wenn der Druck periodisch abgefragt werden soll, wird empfohlen
+den Callback :func:`Pressure` zu nutzen und die Periode mit
+:func:`SetPressureCallbackPeriod` vorzugeben.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Analog Value',
+'elements': [('Value', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+Returns the value as read by a 12-bit analog-to-digital converter.
+The value is between 0 and 4095.
+
+If you want the analog value periodically, it is recommended to use the
+callback :func:`AnalogValue` and set the period with
+:func:`SetAnalogValueCallbackPeriod`.
+""",
+'de':
+"""
+Gibt den Wert, wie vom 12-Bit Analog-Digital-Wandler gelesen, zurück. Der
+Wertebereich ist 0 bis 4095.
+
+Wenn der Analogwert periodisch abgefragt werden soll, wird empfohlen
+den Callback :func:`AnalogValue` zu nutzen und die Periode mit
+:func:`SetAnalogValueCallbackPeriod` vorzugeben.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Pressure Callback Period',
+'elements': [('Period', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the :func:`Pressure` callback is triggered
+periodically. A value of 0 turns the callback off.
+
+:func:`Pressure` is only triggered if the pressure has changed since the
+last triggering.
+
+The default value is 0.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher der :func:`Pressure` Callback ausgelöst wird.
+Ein Wert von 0 deaktiviert den Callback.
+
+:func:`Pressure` wird nur ausgelöst wenn sich der Druck seit der
+letzten Auslösung geändert hat.
+
+Der Standardwert ist 0.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Pressure Callback Period',
+'elements': [('Period', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the period as set by :func:`SetPressureCallbackPeriod`.
+""",
+'de':
+"""
+Gibt die Periode zurück, wie von :func:`SetPressureCallbackPeriod`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Analog Value Callback Period',
+'elements': [('Period', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the :func:`AnalogValue` callback is triggered
+periodically. A value of 0 turns the callback off.
+
+:func:`AnalogValue` is only triggered if the analog value has changed since the
+last triggering.
+
+The default value is 0.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher der :func:`AnalogValue` Callback ausgelöst wird.
+Ein Wert von 0 deaktiviert den Callback.
+
+:func:`AnalogValue` wird nur ausgelöst wenn sich der Analogwert seit der
+letzten Auslösung geändert hat.
+
+Der Standardwert ist 0.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Analog Value Callback Period',
+'elements': [('Period', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the period as set by :func:`SetAnalogValueCallbackPeriod`.
+""",
+'de':
+"""
+Gibt die Periode zurück, wie von :func:`SetAnalogValueCallbackPeriod`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Pressure Callback Threshold',
+'elements': [('Option', 'char', 1, 'in', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'int32', 1, 'in'),
+             ('Max', 'int32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the thresholds for the :func:`PressureReached` callback.
+
+The following options are possible:
+
+.. csv-table::
+ :header: "Option", "Description"
+ :widths: 10, 100
+
+ "'x'",    "Callback is turned off"
+ "'o'",    "Callback is triggered when the pressure is *outside* the min and max values"
+ "'i'",    "Callback is triggered when the pressure is *inside* the min and max values"
+ "'<'",    "Callback is triggered when the pressure is smaller than the min value (max is ignored)"
+ "'>'",    "Callback is triggered when the pressure is greater than the min value (max is ignored)"
+
+The default value is ('x', 0, 0).
+""",
+'de':
+"""
+Setzt den Schwellwert für den :func:`PressureReached` Callback.
+
+Die folgenden Optionen sind möglich:
+
+.. csv-table::
+ :header: "Option", "Beschreibung"
+ :widths: 10, 100
+
+ "'x'",    "Callback ist inaktiv"
+ "'o'",    "Callback wird ausgelöst wenn der Druck *außerhalb* des min und max Wertes ist"
+ "'i'",    "Callback wird ausgelöst wenn der Druck *innerhalb* des min und max Wertes ist"
+ "'<'",    "Callback wird ausgelöst wenn der Druck kleiner als der min Wert ist (max wird ignoriert)"
+ "'>'",    "Callback wird ausgelöst wenn der Druck größer als der min Wert ist (max wird ignoriert)"
+
+Der Standardwert ist ('x', 0, 0).
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Pressure Callback Threshold',
+'elements': [('Option', 'char', 1, 'out', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'int32', 1, 'out'),
+             ('Max', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the threshold as set by :func:`SetPressureCallbackThreshold`.
+""",
+'de':
+"""
+Gibt den Schwellwert zurück, wie von :func:`SetPressureCallbackThreshold`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Analog Value Callback Threshold',
+'elements': [('Option', 'char', 1, 'in', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'int32', 1, 'in'),
+             ('Max', 'int32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the thresholds for the :func:`AnalogValueReached` callback.
+
+The following options are possible:
+
+.. csv-table::
+ :header: "Option", "Description"
+ :widths: 10, 100
+
+ "'x'",    "Callback is turned off"
+ "'o'",    "Callback is triggered when the analog value is *outside* the min and max values"
+ "'i'",    "Callback is triggered when the analog value is *inside* the min and max values"
+ "'<'",    "Callback is triggered when the analog value is smaller than the min value (max is ignored)"
+ "'>'",    "Callback is triggered when the analog value is greater than the min value (max is ignored)"
+
+The default value is ('x', 0, 0).
+""",
+'de':
+"""
+Setzt den Schwellwert für den :func:`AnalogValueReached` Callback.
+
+Die folgenden Optionen sind möglich:
+
+.. csv-table::
+ :header: "Option", "Beschreibung"
+ :widths: 10, 100
+
+ "'x'",    "Callback ist inaktiv"
+ "'o'",    "Callback wird ausgelöst wenn der Analogwert *außerhalb* des min und max Wertes ist"
+ "'i'",    "Callback wird ausgelöst wenn der Analogwert *innerhalb* des min und max Wertes ist"
+ "'<'",    "Callback wird ausgelöst wenn der Analogwert kleiner als der min Wert ist (max wird ignoriert)"
+ "'>'",    "Callback wird ausgelöst wenn der Analogwert größer als der min Wert ist (max wird ignoriert)"
+
+Der Standardwert ist ('x', 0, 0).
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Analog Value Callback Threshold',
+'elements': [('Option', 'char', 1, 'out', THRESHOLD_OPTION_CONSTANTS),
+             ('Min', 'int32', 1, 'out'),
+             ('Max', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the threshold as set by :func:`SetAnalogValueCallbackThreshold`.
+""",
+'de':
+"""
+Gibt den Schwellwert zurück, wie von :func:`SetAnalogValueCallbackThreshold`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Debounce Period',
+'elements': [('Debounce', 'uint32', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Sets the period in ms with which the threshold callbacks
+
+* :func:`PressureReached`,
+* :func:`AnalogValueReached`
+
+are triggered, if the thresholds
+
+* :func:`SetPressureCallbackThreshold`,
+* :func:`SetAnalogValueCallbackThreshold`
+
+keep being reached.
+
+The default value is 100.
+""",
+'de':
+"""
+Setzt die Periode in ms mit welcher die Schwellwert Callbacks
+
+* :func:`PressureReached`,
+* :func:`AnalogValueReached`
+
+ausgelöst werden, wenn die Schwellwerte
+
+* :func:`SetPressureCallbackThreshold`,
+* :func:`SetAnalogValueCallbackThreshold`
+
+weiterhin erreicht bleiben.
+
+Der Standardwert ist 100.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Debounce Period',
+'elements': [('Debounce', 'uint32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['ccf', {
+'en':
+"""
+Returns the debounce period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Gibt die Entprellperiode zurück, wie von :func:`SetDebouncePeriod`
+gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Sensor Type',
+'elements': [('Sensor', 'uint8', 1, 'in', ('Sensor Type', [('MPX5500', 0),
+                                                           ('MPXV5004', 1)]))],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Sets the sensor type. Possible values are:
+
+* 0 = MPX5500 (0-500 kPa)
+* 1 = MPXV5004, MPVZ5004 (0-3.92 kPa)
+
+The default value is 0.
+""",
+'de':
+"""
+Setzt den Sensortyp. Mögliche Werte sind:
+
+* 0 = MPX5500 (0-500 kPa)
+* 1 = MPXV5004, MPVZ5004 (0-3,92 kPa)
+
+Der Standardwert ist 0.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Sensor Type',
+'elements': [('Sensor', 'uint8', 1, 'out', ('Sensor Type', [('MPX5500', 0),
+                                                            ('MPXV5004', 1)]))],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Returns the sensor type as set by :func:`SetSensorType`.
+""",
+'de':
+"""
+Gibt der Sensortyp zurück, wie von :func:`SetSensorType` gesetzt.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Moving Average',
+'elements': [('Average', 'uint8', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+Sets the length of a `moving averaging <https://en.wikipedia.org/wiki/Moving_average>`__
+for the pressure.
+
+Setting the length to 1 will turn the averaging off. With less
+averaging, there is more noise on the data.
+
+The range for the averaging is 1-50.
+
+The default value is 50.
+""",
+'de':
+"""
+Setzt die Länge eines `gleitenden Mittelwerts <https://de.wikipedia.org/wiki/Gleitender_Mittelwert>`__
+für den Druck.
+
+Wenn die Länge auf 1 gesetzt wird, ist das Averaging aus. Desto kleiner
+die Länge des Mittelwerts ist, desto mehr Rauschen ist auf den Daten.
+
+Der Wertebereich liegt bei 1-50.
+
+Der Standardwert ist 50.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Moving Average',
+'elements': [('Average', 'uint8', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['af', {
+'en':
+"""
+Returns the length of the moving average as set by :func:`SetMovingAverage`.
+""",
+'de':
+"""
+Gibt die Länge des gleitenden Mittelwerts zurück, wie von
+:func:`SetMovingAverage` gesetzt.
+"""
+}]
+})
+
+# FIXME: add calibrate function
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Pressure',
+'elements': [('Pressure', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered periodically with the period that is set by
+:func:`SetPressureCallbackPeriod`. The :word:`parameter` is the pressure of the
+sensor.
+
+:func:`Pressure` is only triggered if the pressure has changed since the
+last triggering.
+""",
+'de':
+"""
+Dieser Callback wird mit der Periode, wie gesetzt mit :func:`SetPressureCallbackPeriod`,
+ausgelöst. Der :word:`parameter` ist der gemessene Druck des Sensors.
+
+:func:`Pressure` wird nur ausgelöst wenn sich der Druck seit der
+letzten Auslösung geändert hat.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Analog Value',
+'elements': [('Value', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered periodically with the period that is set by
+:func:`SetAnalogValueCallbackPeriod`. The :word:`parameter` is the analog value of the
+sensor.
+
+:func:`AnalogValue` is only triggered if the pressure has changed since the
+last triggering.
+""",
+'de':
+"""
+Dieser Callback wird mit der Periode, wie gesetzt mit :func:`SetAnalogValueCallbackPeriod`,
+ausgelöst. Der :word:`parameter` ist der Analogwert des Sensors.
+
+:func:`AnalogValue` wird nur ausgelöst wenn sich der Analogwert seit der
+letzten Auslösung geändert hat.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Pressure Reached',
+'elements': [('Pressure', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered when the threshold as set by
+:func:`SetPressureCallbackThreshold` is reached.
+The :word:`parameter` is the pressure of the sensor.
+
+If the threshold keeps being reached, the callback is triggered periodically
+with the period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn der Schwellwert, wie von
+:func:`SetPressureCallbackThreshold` gesetzt, erreicht wird.
+Der :word:`parameter` ist der gemessene Druck des Sensors.
+
+Wenn der Schwellwert erreicht bleibt, wird der Callback mit der Periode, wie
+mit :func:`SetDebouncePeriod` gesetzt, ausgelöst.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Analog Value Reached',
+'elements': [('Value', 'int32', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered when the threshold as set by
+:func:`SetAnalogValueCallbackThreshold` is reached.
+The :word:`parameter` is the analog value of the sensor.
+
+If the threshold keeps being reached, the callback is triggered periodically
+with the period as set by :func:`SetDebouncePeriod`.
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn der Schwellwert, wie von
+:func:`SetAnalogValueCallbackThreshold` gesetzt, erreicht wird.
+Der :word:`parameter` ist der Analogwert des Sensors.
+
+Wenn der Schwellwert erreicht bleibt, wird der Callback mit der Periode, wie
+mit :func:`SetDebouncePeriod` gesetzt, ausgelöst.
+"""
+}]
+})
+
+com['examples'].append({
+'name': 'Simple',
+'functions': [('getter', ('Get Pressure', 'pressure'), [(('Pressure', 'Pressure'), 'int32', 1000.0, 'Pa', 'kPa', None)], [])]
+})
+
+com['examples'].append({
+'name': 'Callback',
+'functions': [('callback', ('Pressure', 'pressure'), [(('Pressure', 'Pressure'), 'int32', 1000.0, 'Pa', 'kPa', None)], None, None),
+              ('callback_period', ('Pressure', 'pressure'), [], 1000)]
+})
+
+com['examples'].append({
+'name': 'Threshold',
+'functions': [('debounce_period', 10000),
+              ('callback', ('Pressure Reached', 'pressure reached'), [(('Pressure', 'Pressure'), 'int32', 1000.0, 'Pa', 'kPa', None)], None, None),
+              ('callback_threshold', ('Pressure', 'pressure'), [], '>', [(10, 0)])]
+})
