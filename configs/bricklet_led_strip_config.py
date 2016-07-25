9b36fe7964993b9907fff2bbdc42e2b6464aa5e9
Matthias Bolte <matthias@tinkerforge.com>
2016-07-25 15:47:56 +0200

    led-strip: Add RGBW documentation

--- configs/bricklet_led_strip_config.py
+++ configs/bricklet_led_strip_config.py
@@ -35,7 +35,7 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-Sets the *rgb* values for the LEDs with the given *length* starting 
+Sets the RGB values for the LEDs with the given *length* starting
 from *index*.
 
 The maximum length is 16, the index goes from 0 to 319 and the rgb values
@@ -56,10 +56,10 @@ the LED with index 5 will be red, 6 will be green and 7 will be blue.
 The colors will be transfered to actual LEDs when the next
 frame duration ends, see :func:`SetFrameDuration`.
 
-Generic approach: 
+Generic approach:
 
 * Set the frame duration to a value that represents
-  the number of frames per second you want to achieve. 
+  the number of frames per second you want to achieve.
 * Set all of the LED colors for one frame.
 * Wait for the :func:`FrameRendered` callback.
 * Set all of the LED colors for next frame.
@@ -76,7 +76,7 @@ bounds is ignored completely.
 """,
 'de':
 """
-Setzt die *rgb* Werte für die LEDs mit der angegebenen *length*,
+Setzt die RGB Werte für die LEDs mit der angegebenen *length*,
 beginnend vom angegebenen *index*.
 
 Die maximale Länge ist 16. Der Index geht von 0 bis 319 und die
@@ -93,7 +93,7 @@ Beispiel: Wenn
 gesetzt wird, wird die LED an Index 5 die Farbe Rot annehmen, 6 wird Grün und 7
 wird Blau.
 
-.. note:: Abhängig von der internen LED Schaltung können die Farben vertauscht 
+.. note:: Abhängig von der internen LED Schaltung können die Farben vertauscht
    sein.
 
 Die Farben werden auf die tatsächlichen LEDs transferiert wenn die
@@ -114,7 +114,7 @@ festen Framerate angezeigt werden.
 
 Die effektive Anzahl steuerbarer LEDs ist abhängig von der Anzahl
 der freien Bricklet Ports (siehe :ref:`hier <led_strip_bricklet_ram_constraints>`).
-Ein Aufruf von :func:`SetRGBValues` mit index + length größer als die 
+Ein Aufruf von :func:`SetRGBValues` mit index + length größer als die
 Begrenzung werden komplett ingnoriert.
 """
 },
@@ -142,14 +142,14 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-Returns the rgb with the given *length* starting from the
+Returns RGB value with the given *length* starting from the
 given *index*.
 
 The values are the last values that were set by :func:`SetRGBValues`.
 """,
 'de':
 """
-Gibt rgb Werte mit der übergebenen *length* zurück, beginnend vom
+Gibt RGB Werte mit der übergebenen *length* zurück, beginnend vom
 übergebenen *index*.
 
 Die Werte sind die letzten von :func:`SetRGBValues` gesetzten Werte.
@@ -168,7 +168,7 @@ com['packets'].append({
 Sets the frame duration in ms.
 
 Example: If you want to achieve 20 frames per second, you should
-set the frame duration to 50ms (50ms * 20 = 1 second). 
+set the frame duration to 50ms (50ms * 20 = 1 second).
 
 For an explanation of the general approach see :func:`SetRGBValues`.
 
@@ -395,21 +395,101 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-FIXME
+Sets the RGBW values for the LEDs with the given *length* starting
+from *index*.
+
+The maximum length is 12, the index goes from 0 to 319 and the rgb values
+have 8 bits each.
+
+Example: If you set
+
+* index to 5,
+* length to 4,
+* r to |r_values|,
+* g to |g_values|,
+* b to |b_values| and
+* w to |b_values|
+
+the LED with index 5 will be red, 6 will be green, 7 will be blue and 8 will be white.
+
+.. note:: Depending on the LED circuitry colors can be permuted.
+
+The colors will be transfered to actual LEDs when the next
+frame duration ends, see :func:`SetFrameDuration`.
+
+Generic approach:
+
+* Set the frame duration to a value that represents
+  the number of frames per second you want to achieve.
+* Set all of the LED colors for one frame.
+* Wait for the :func:`FrameRendered` callback.
+* Set all of the LED colors for next frame.
+* Wait for the :func:`FrameRendered` callback.
+* and so on.
+
+This approach ensures that you can change the LED colors with
+a fixed frame rate.
+
+The actual number of controllable LEDs depends on the number of free
+Bricklet ports. See :ref:`here <led_strip_bricklet_ram_constraints>` for more
+information. A call of :func:`SetRGBValues` with index + length above the
+bounds is ignored completely.
 """,
 'de':
 """
-FIXME
+Setzt die RGBW Werte für die LEDs mit der angegebenen *length*,
+beginnend vom angegebenen *index*.
+
+Die maximale Länge ist 16. Der Index geht von 0 bis 319 und die
+rgb Werte haben jeweils 8 Bit.
+
+Beispiel: Wenn
+
+* index auf 5,
+* length auf 4,
+* r auf |r_values|,
+* g auf |g_values|,
+* b auf |b_values| und
+* w auf |w_values|
+
+gesetzt wird, wird die LED an Index 5 die Farbe Rot annehmen, 6 wird Grün, 7
+wird Blau und 8 wird Weiß.
+
+.. note:: Abhängig von der internen LED Schaltung können die Farben vertauscht
+   sein.
+
+Die Farben werden auf die tatsächlichen LEDs transferiert wenn die
+nächste *frame duration* abgelaufen ist, siehe :func:`SetFrameDuration`.
+
+Genereller Ansatz:
+
+* Setze *frame duration* auf einen Wert welcher der Anzahl der
+  Bilder pro Sekunde entspricht die erreicht werden sollen.
+* Setze alle LEDs für einen Frame.
+* Warte auf :func:`FrameRendered` Callback.
+* Setze alle LEDs für den nächsten Frame.
+* Warte auf :func:`FrameRendered` Callback.
+* Und so weiter.
+
+Dieser Ansatz garantiert das die LED Farben mit einer
+festen Framerate angezeigt werden.
+
+Die effektive Anzahl steuerbarer LEDs ist abhängig von der Anzahl
+der freien Bricklet Ports (siehe :ref:`hier <led_strip_bricklet_ram_constraints>`).
+Ein Aufruf von :func:`SetRGBValues` mit index + length größer als die
+Begrenzung werden komplett ingnoriert.
 """
 },
 {
 '*': {
-'r_values': {'php': 'array(255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
-             '*': '[255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]'},
-'g_values': {'php': 'array(0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
-             '*': '[0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]'},
-'b_values': {'php': 'array(0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
-             '*': '[0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]'}
+'r_values': {'php': 'array(255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
+             '*': '[255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]'},
+'g_values': {'php': 'array(0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
+             '*': '[0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]'},
+'b_values': {'php': 'array(0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
+             '*': '[0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0]'},
+'b_values': {'php': 'array(0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0)',
+             '*': '[0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0]'}
 }
 }]
 })
@@ -427,14 +507,14 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-Returns the rgbw with the given *length* starting from the
+Returns RGBW values with the given *length* starting from the
 given *index*.
 
 The values are the last values that were set by :func:`SetRGBWValues`.
 """,
 'de':
 """
-Gibt rgbw Werte mit der übergebenen *length* zurück, beginnend vom
+Gibt RGBW Werte mit der übergebenen *length* zurück, beginnend vom
 übergebenen *index*.
 
 Die Werte sind die letzten von :func:`SetRGBWValues` gesetzten Werte.
