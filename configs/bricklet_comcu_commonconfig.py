7e36f4d7c86dd76250ac87b4f54e7ac00b4c3f43
Olaf Lüke <olaf@tinkerforge.com>
2017-01-20 15:43:22 +0100

    add common comcu documentation

--- configs/bricklet_comcu_commonconfig.py
+++ configs/bricklet_comcu_commonconfig.py
@@ -20,11 +20,33 @@ common_packets.append({
 'doc': ['af', {
 'en':
 """
-TODO
+Returns the error count for the communication between Brick and Bricklet.
+
+The errors are divided into
+
+* ack checksum errors,
+* message checksum errors,
+* frameing errors and
+* overflow errors.
+
+The errors counts are for errors that occur on the Bricklet side. All
+Bricks have a similar function that returns the errors on the Brick side.
 """,
 'de':
 """
-TODO
+Gibt die Anzahl der Fehler die während der Kommunikation zwischen Brick und
+Bricklet aufgetreten sind zurück.
+
+Die Fehler sind aufgeteilt in
+
+* ACK-Checksummen Fehler,
+* Message-Checksummen Fehler,
+* Framing Fehler und
+* Overflow Fehler.
+
+Die Fehlerzähler sind für Fehler die auf der Seite des Bricklets auftreten.
+Jedes Bricklet hat eine ähnliche Funktion welche die Fehler auf Brickseite
+ausgibt.
 """
 }]
 })
@@ -48,11 +70,28 @@ common_packets.append({
 'doc': ['af', {
 'en':
 """
-TODO
+Sets the bootloader mode and returns the status after the requested
+mode change was instigated.
+
+You can change from bootloader mode to firmware mode and vice versa. A change
+from bootloader mode to firmware mode will only take place if the entry function,
+device identifier und crc are present and correct.
+
+This function is used by Brick Viewer during flashing. It should not be
+necessary to call it in a normal user program.
 """,
 'de':
 """
-TODO
+Setzt den Bootloader-Modus und gibt den Status zurück nachdem die 
+Modusänderungsanfrage bearbeitet wurde.
+
+Mit dieser Funktion ist es möglich vom Bootloader- in den Firmware-Modus zu
+wechseln und umgekehrt. Ein Welchsel vom Bootlodaer- in der den Firmware-Modus
+ist nur möglich wenn Entry-Funktion, Device Identifier und CRC vorhanden und
+korrekt sind.
+
+Diese Funktion wird vom Brick Viewer während des flashens benutzt. In einem
+normalem Nutzerprogramm sollte diese Funktion nicht benötigt werden.
 """
 }]
 })
@@ -70,11 +109,11 @@ common_packets.append({
 'doc': ['af', {
 'en':
 """
-TODO
+Returns the current bootloader mode, see :func:`SetBootloaderMode`.
 """,
 'de':
 """
-TODO
+Gibt den aktuellen Bootloader-Modus zurück, siehe :func:`SetBootloaderMode`.
 """
 }]
 })
@@ -89,11 +128,21 @@ common_packets.append({
 'doc': ['af', {
 'en':
 """
-TODO
+Sets the firmware pointer for func:`WriteFirmware`. The pointer has
+to be increased by chunks of size 64. The data is written to flash
+every 4 chunks (which equals to one page of size 256).
+
+This function is used by Brick Viewer during flashing. It should not be
+necessary to call it in a normal user program.
 """,
 'de':
 """
-TODO
+Setzt den Firmware-Pointer für func:`WriteFirmware`. Der Pointer
+muss um je 64 Byte erhöht werden. Die Daten werden alle 4 Datenblöcke
+in den Flash geschrieben (4 Datenblöcke entsprechen einer Page mit 256 Byte).
+
+Diese Funktion wird vom Brick Viewer während des flashens benutzt. In einem
+normalem Nutzerprogramm sollte diese Funktion nicht benötigt werden.
 """
 }]
 })
@@ -110,11 +159,25 @@ common_packets.append({
 'doc': ['af', {
 'en':
 """
-TODO
+Writes 64 Bytes of firmware at the position as written by 
+:func:`SetWriteFirmwarePointer` before. The firmware is written
+to flash every 4 chunks.
+
+You can only write firmware in bootloader mode.
+
+This function is used by Brick Viewer during flashing. It should not be
+necessary to call it in a normal user program.
 """,
 'de':
 """
-TODO
+Schreibt 64 Bytes Firmware an die Position die vorher von
+:func:`SetWriteFirmwarePointer` gesetzt wurde. Die Firmware wird
+alle 4 Datenblöcke in den Flash geschrieben.
+
+Eine Firmware kann nur im Bootloader-Mode geschrieben werden.
+
+Diese Funktion wird vom Brick Viewer während des flashens benutzt. In einem
+normalem Nutzerprogramm sollte diese Funktion nicht benötigt werden.
 """
 }]
 })
@@ -230,3 +293,50 @@ Funktionsaufrufe auf bestehende führt zu undefiniertem Verhalten.
 }]
 })
 
+common_packets.append({
+'type': 'function',
+'function_id': 248,
+'name': 'Write UID',
+'elements': [('UID', 'uint32', 1, 'in')],
+'since_firmware': {'*': [1, 0, 0]},
+'doc': ['af', {
+'en':
+"""
+Writes a new UID into flash. If you want to set a new UID
+you have to decode the Base58 encoded UID string into an
+integer first.
+
+We recommend that you use Brick Viewer to change the UID.
+""",
+'de':
+"""
+Schreibt eine neue UID in den Flash. Die UID muss zuerst
+vom Base58 encodierten String in einen Integer decodiert
+werden.
+
+Wir empfehlen die Nutzung des Brick Viewers zum ändern
+der UID.
+"""
+}]
+})
+
+common_packets.append({
+'type': 'function',
+'function_id': 249,
+'name': 'Read UID',
+'elements': [('UID', 'uint32', 1, 'out')],
+'since_firmware': {'*': [1, 0, 0]},
+'doc': ['af', {
+'en':
+"""
+Returns the current UID as an integer. Encode as
+Base58 to get the usual string version.
+""",
+'de':
+"""
+Gibt die aktuelle UID als Integer zurück. Dieser Integer
+kann als Base58 encodiert werden um an den üblichen
+UID-String zu gelangen.
+"""
+}]
+})
