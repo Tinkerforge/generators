5dfa9b192a0d5d0bc43e88747e6f0da2a9356489
Olaf Lüke <olaf@tinkerforge.com>
2017-03-28 16:24:05 +0200

    Add config for XMC1400 breakout

---
+++ configs/bricklet_xmc1400_breakout_config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# GPS Bricklet communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 279,
+    'name': 'XMC1400 Breakout',
+    'display_name': 'XMC1400 Breakout',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'breakout board for development',
+        'de': 'Breakout Board für Entwicklung'
+    },
+    'has_comcu': True,
+    'released': False,
+    'documented': False,
+    'packets': [],
+    'examples': []
+}
+
+
