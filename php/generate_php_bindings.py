c14fae8b20cdd21bd3ff2af47cea614a66776bdf
Olaf Lüke <olaf@tinkerforge.com>
2017-04-12 15:09:58 +0200

    Use cardinality for for-loop

--- php/generate_php_bindings.py
+++ php/generate_php_bindings.py
@@ -205,7 +205,7 @@ class {0} extends Device
 
                 if element.get_type() == 'bool':
                     if cardinality > 1:
-                        pack.append('        for ($i = 0; $i < {0}; $i++) {{'.format(element[2]))
+                        pack.append('        for ($i = 0; $i < {0}; $i++) {{'.format(cardinality))
                         pack.append('            $payload .= pack(\'{0}\', intval((bool)${1}[$i]));\n        }}'.format(pack_format, underscore_name))
                     else:
                         pack.append('        $payload .= pack(\'{0}\', intval((bool)${1}));'.format(pack_format, underscore_name))
