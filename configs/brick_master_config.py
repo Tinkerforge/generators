4a4ac329b6ce3fb16da59a56d50dd2c04d687f5d
Olaf Lüke <olaf@tinkerforge.com>
2017-05-05 16:33:39 +0200

    Add notes that WIFI passwords are not returned anymore to the apropriate API

--- configs/brick_master_config.py
+++ configs/brick_master_config.py
@@ -985,11 +985,15 @@ com['packets'].append({
 'en':
 """
 Returns the encryption as set by :func:`Set Wifi Encryption`.
+
+Note: Since Master Brick Firmware version 2.4.4 the key is not returned anymore.
 """,
 'de':
 """
 Gibt die Verschlüsselungseinstellungen zurück, wie von
 :func:`Set Wifi Encryption` gesetzt.
+
+Hinweis: Seit Master Brick Firmware Version 2.4.4 wird der Schlüssel nicht mehr zurückgegeben.
 """
 }]
 })
@@ -1366,11 +1370,16 @@ com['packets'].append({
 'en':
 """
 Returns the encryption key as set by :func:`Set Long Wifi Key`.
+
+Note: Since Master Brick firmware version 2.4.4 the key is not returned anymore.
 """,
 'de':
 """
 Gibt den Verschlüsselungsschlüssel zurück, wie von
 :func:`Set Long Wifi Key` gesetzt.
+
+Hinweis: Seit Master Brick Firmware Version 2.4.4 wird der Schlüssel nicht mehr
+zurückgegeben.
 """
 }]
 })
@@ -2917,11 +2926,17 @@ com['packets'].append({
 'en':
 """
 Returns the client password as set by :func:`Set Wifi2 Client Password`.
+
+Note: Since WIFI Extension 2.0 firmware version 2.1.3 the password is not
+returned anymore.
 """,
 'de':
 """
 Gibt das Client-Passwort zurück, wie von :func:`Set Wifi2 Client Password`
 gesetzt.
+
+Hinweis: Seit WIFI Extension 2.0 Firmware Version 2.1.3 wird das Passwort
+nicht mehr zurückgegeben
 """
 }]
 })
@@ -3089,10 +3104,16 @@ com['packets'].append({
 'en':
 """
 Returns the access point password as set by :func:`Set Wifi2 AP Password`.
+
+Note: Since WIFI Extension 2.0 firmware version 2.1.3 the password is not
+returned anymore.
 """,
 'de':
 """
 Gibt das Access-Point-Passwort zurück, wie von :func:`Set Wifi2 AP Password` gesetzt.
+
+Hinweis: Seit WIFI Extension 2.0 Firmware Version 2.1.3 wird das Passwort
+nicht mehr zurückgegeben
 """
 }]
 })
