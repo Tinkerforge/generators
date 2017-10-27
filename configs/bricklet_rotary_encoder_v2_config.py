7c4de92f375eaa6388503036a91dcbe9635300c0
Olaf Lüke <olaf@tinkerforge.com>
2017-10-27 18:01:05 +0200

    Add Rotary Encoder V2 and Solid State Relay V2 config

---
+++ configs/bricklet_rotary_encoder_v2_config.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Rotary Encoder Bricklet 2.0 communication config
+
+from commonconstants import THRESHOLD_OPTION_CONSTANTS
+from commonconstants import add_callback_value_function
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 294,
+    'name': 'Rotary Encoder V2',
+    'display_name': 'Rotary Encoder 2.0',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '360° rotary encoder with push-button',
+        'de': '360° Drehgeber/Drehencoder mit Taster'
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'packets': [],
+    'examples': []
+}
+
+
+
+count_doc = {
+'en':
+"""
+Returns the current count of the encoder. If you set reset
+to true, the count is set back to 0 directly after the
+current count is read.
+
+The encoder has 24 steps per rotation
+
+Turning the encoder to the left decrements the counter,
+so a negative count is possible.
+""",
+'de':
+"""
+Gibt den aktuellen Zählerwert des Encoders zurück. Wenn
+reset auf true gesetzt wird, wird der Zählerstand
+direkt nach dem auslesen auf 0 zurück gesetzt.
+
+Der Encoder hat 24 Schritte pro Umdrehung.
+
+Wenn der Encoder nach links gedreht wird wird der Zählerwert
+dekrementiert, d.h. negative Zählerwerte sind möglich.
+"""
+}
+
+add_callback_value_function(
+    packets   = com['packets'], 
+    name      = 'Get Count', 
+    data_name = 'Count',
+    data_type = 'int32',
+    doc       = count_doc
+)
+
+com['packets'][0]['elements'].insert(0, ('Reset', 'bool', 1, 'in'))
+
+com['packets'].append({
+'type': 'function',
+'name': 'Is Pressed',
+'elements': [('Pressed', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Returns *true* if the button is pressed and *false* otherwise.
+
+It is recommended to use the :cb:`Pressed` and :cb:`Released` callbacks
+to handle the button.
+""",
+'de':
+"""
+Gibt *true* zurück wenn der Taster gedrückt ist und sonst *false*.
+
+Es wird empfohlen die :cb:`Pressed` und :cb:`Released` Callbacks
+zu nutzen, um den Taster programmatisch zu behandeln.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Pressed',
+'elements': [],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered when the button is pressed.
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn der Taster gedrückt wird.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Released',
+'elements': [],
+'since_firmware': [1, 0, 0],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered when the button is released.
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn der Taster losgelassen wird.
+"""
+}]
+})
+
+
+com['examples'].append({
+'name': 'Simple',
+'functions': [('getter', ('Get Count', 'count without reset'), [(('Count', 'Count'), 'int32', None, None, None, None)], [('bool', False)])]
+})
---
+++ configs/bricklet_solid_state_relay_v2_config.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Solid State Relay Bricklet 2.0 communication config
+
+from commonconstants import THRESHOLD_OPTION_CONSTANTS
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 296,
+    'name': 'Solid State Relay V2',
+    'display_name': 'Solid State Relay 2.0',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'packets': [],
+    'examples': []
+}
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set State',
+'elements': [('State', 'bool', 1, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Sets the state of the relays *true* means on and *false* means off.
+
+Running monoflop timers will be overwritten if this function is called.
+
+The default value is *false*.
+""",
+'de':
+"""
+Setzt den Zustand des Relais, *true* bedeutet ein und *false* aus.
+
+Laufende Monoflop Timer werden überschrieben wenn diese Funktion aufgerufen wird.
+
+Der Standardwert ist *false*.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get State',
+'elements': [('State', 'bool', 1, 'out')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Returns the state of the relay, *true* means on and *false* means off.
+""",
+'de':
+"""
+Gibt den Zustand der Relais zurück, *true* bedeutet ein und *false* aus.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Monoflop',
+'elements': [('State', 'bool', 1, 'in'),
+             ('Time', 'uint32', 1, 'in')],
+'since_firmware': [1, 1, 1],
+'doc': ['af', {
+'en':
+"""
+The first parameter  is the desired state of the relay (*true* means on
+and *false* means off). The second parameter indicates the time (in ms) that
+the relay should hold the state.
+
+If this function is called with the parameters (true, 1500):
+The relay will turn on and in 1.5s it will turn off again.
+
+A monoflop can be used as a failsafe mechanism. For example: Lets assume you
+have a RS485 bus and a Solid State Relay Bricklet connected to one of the slave
+stacks. You can now call this function every second, with a time parameter
+of two seconds. The relay will be on all the time. If now the RS485
+connection is lost, the relay will turn off in at most two seconds.
+""",
+'de':
+"""
+Der erste Parameter ist der gewünschte Zustand des Relais
+(*true* bedeutet ein und *false* aus). Der zweite Parameter stellt die Zeit
+(in ms) dar, welche das Relais den Zustand halten soll.
+
+Wenn diese Funktion mit den Parametern (true, 1500) aufgerufen wird:
+Das Relais wird angeschaltet und nach 1,5s wieder ausgeschaltet.
+
+Ein Monoflop kann als Ausfallsicherung verwendet werden. Beispiel:
+Angenommen ein RS485 Bus und ein Dual Relay Bricklet ist an ein Slave Stapel
+verbunden. Jetzt kann diese Funktion sekündlich, mit einem Zeitparameter
+von 2 Sekunden, aufgerufen werden.
+Das Relais wird die gesamte Zeit ein sein. Wenn jetzt die RS485 Verbindung
+getrennt wird, wird das Relais nach spätestens zwei Sekunden ausschalten.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'function',
+'name': 'Get Monoflop',
+'elements': [('State', 'bool', 1, 'out'),
+             ('Time', 'uint32', 1, 'out'),
+             ('Time Remaining', 'uint32', 1, 'out')],
+'since_firmware': [1, 1, 1],
+'doc': ['af', {
+'en':
+"""
+Returns the current state and the time as set by
+:func:`Set Monoflop` as well as the remaining time until the state flips.
+
+If the timer is not running currently, the remaining time will be returned
+as 0.
+""",
+'de':
+"""
+Gibt den aktuellen Zustand und die Zeit, wie von
+:func:`Set Monoflop` gesetzt, sowie die noch verbleibende Zeit bis zum
+Zustandswechsel, zurück.
+
+Wenn der Timer aktuell nicht läuft, ist die noch verbleibende Zeit 0.
+"""
+}]
+})
+
+com['packets'].append({
+'type': 'callback',
+'name': 'Monoflop Done',
+'elements': [('State', 'bool', 1, 'out')],
+'since_firmware': [1, 1, 1],
+'doc': ['c', {
+'en':
+"""
+This callback is triggered whenever the monoflop timer reaches 0.
+The parameter is the current state of the relay
+(the state after the monoflop).
+""",
+'de':
+"""
+Dieser Callback wird ausgelöst wenn ein Monoflop Timer abläuft (0 erreicht).
+Der Parameter ist der aktuellen Zustand des Relais
+(der Zustand nach dem Monoflop).
+"""
+}]
+})
+
+com['examples'].append({
+'name': 'Simple',
+'functions': [('loop_header', 5, 'Turn relay on/off 10 times with 1 second delay'),
+              ('sleep', 1000, None, None),
+              ('setter', 'Set State', [('bool', True)], None, None),
+              ('sleep', 1000, None, None),
+              ('setter', 'Set State', [('bool', False)], None, None),
+              ('loop_footer',)]
+})
