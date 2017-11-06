22a976e96b7705120cd4abf728d78fbbdf854bfe
Olaf Lüke <olaf@tinkerforge.com>
2017-11-06 15:33:30 +0100

    Add Humidity V2 documentation (english and german)

--- configs/bricklet_humidity_v2_config.py
+++ configs/bricklet_humidity_v2_config.py
@@ -31,15 +31,15 @@ com = {
 humidity_doc = {
 'en':
 """
-Returns the humidity of the sensor. The value
-has a range of 0 to 1000 and is given in %RH/10 (Relative Humidity),
-i.e. a value of 421 means that a humidity of 42.1 %RH is measured.
+Returns the humidity measured by the sensor. The value
+has a range of 0 to 10000 and is given in %RH/100 (Relative Humidity),
+i.e. a value of 4223 means that a humidity of 42.23 %RH is measured.
 """,
 'de':
 """
 Gibt die gemessene Luftfeuchtigkeit des Sensors zurück. Der Wertebereich ist von
-0 bis 1000 und wird in %RH/10 angegeben (relative Luftfeuchtigkeit), z.B. bedeutet
-ein Wert von 421 eine gemessene Luftfeuchtigkeit von 42,1 %RH.
+0 bis 10000 und wird in %RH/100 angegeben (relative Luftfeuchtigkeit), z.B. bedeutet
+ein Wert von 4223 eine gemessene Luftfeuchtigkeit von 42,23 %RH.
 """
 }
 
@@ -54,11 +54,16 @@ add_callback_value_function(
 temperature_doc = {
 'en':
 """
+Returns the temperature measured by the sensor. The value
+has a range of -4000 to 16500 and is given in °C/100 (Relative Humidity),
+i.e. a value of 3200 means that a humidity of 32.00 %RH is measured.
 TODO
 """,
 'de':
 """
-TODO
+Gibt die gemessene Temperatur des Sensors zurück. Der Wertebereich ist von
+-4000 bis 16500 und wird in °C/100 angegeben, z.B. bedeutet
+ein Wert von 3200 eine gemessene Temperatur von 32,00 °C.
 """
 }
 
@@ -66,7 +71,7 @@ add_callback_value_function(
     packets   = com['packets'], 
     name      = 'Get Temperature', 
     data_name = 'Temperature',
-    data_type = 'uint16',
+    data_type = 'int16',
     doc       = temperature_doc
 )
 
@@ -81,9 +86,13 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Enables/Disables the heater. The heater can be used to dry the sensor in 
+extremely wet conditions.
 """,
 'de':
 """
+Aktiviert/Deaktiviert das Heizelement. Das Heizelement kann genutzt werden
+um den Sensor bei extremer Feuchtigkeit zu trocknen.
 """
 }]
 })
@@ -96,9 +105,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns the heater configuration as set by :func:`Set Heater Configuration`.
 """,
 'de':
 """
+Gibt die Heizelement-Konfiguration zurück, wie von :func:`Set Heater Configuration` gesetzt.
 """
 }]
 })
@@ -112,9 +123,35 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Sets the length of a `moving averaging <https://en.wikipedia.org/wiki/Moving_average>`__
+for the humidity and temperature.
+
+Setting the length to 1 will turn the averaging off. With less
+averaging, there is more noise on the data.
+
+The range for the averaging is 1-1000.
+
+New data is gathered every 50ms. With a moving average of length 1000 the resulting
+averaging window has a length of 50s. If you want to do long term measurments the longest
+moving average will give the cleanest results.
+
+The default value is 100.
 """,
 'de':
 """
+Setzt die Länge eines `gleitenden Mittelwerts <https://de.wikipedia.org/wiki/Gleitender_Mittelwert>`__
+für die Luftfeuchtigkeit und Temperatur.
+
+Wenn die Länge auf 1 gesetzt wird, ist das Averaging aus. Desto kleiner
+die Länge des Mittelwerts ist, desto mehr Rauschen ist auf den Daten.
+
+Der Wertebereich liegt bei 1-1000.
+
+Neue Werte werden alle 50ms gemessen. Mit einer Mittelwerts-Länge von 1000 hat das
+resultierende gleitende Fenster eine Zeitspanne von 50s. Bei Langzeitmessungen gibt
+ein langer Mittelwert die saubersten Resultate.
+
+Der Standardwert ist 50.
 """
 }]
 })
@@ -129,27 +166,16 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns the moving average configuration as set by :func:`Set Moving Average Configuration`.
 """,
 'de':
 """
+Gibt die Moving Average-Konfiguration zurück, wie von :func:`Set Moving Average Configuration` gesetzt.
 """
 }]
 })
 
 com['examples'].append({
 'name': 'Simple',
-'functions': [('getter', ('Get Humidity', 'humidity'), [(('Humidity', 'Humidity'), 'uint16', 10.0, '%RH/10', '%RH', None)], [])]
-})
-
-com['examples'].append({
-'name': 'Callback',
-'functions': [('callback', ('Humidity', 'humidity'), [(('Humidity', 'Humidity'), 'uint16', 10.0, '%RH/10', '%RH', None)], None, None),
-              ('callback_period', ('Humidity', 'humidity'), [], 1000)]
-})
-
-com['examples'].append({
-'name': 'Threshold',
-'functions': [('debounce_period', 10000),
-              ('callback', ('Humidity Reached', 'humidity reached'), [(('Humidity', 'Humidity'), 'uint16', 10.0, '%RH/10', '%RH', None)], None, 'Recommended humiditiy for human comfort is 30 to 60 %RH.'),
-              ('callback_threshold', ('Humidity', 'humidity'), [], 'o', [(30, 60)])]
+'functions': [('getter', ('Get Humidity', 'humidity'), [(('Humidity', 'Humidity'), 'uint16', 100.0, '%RH/100', '%RH', None)], [])]
 })
