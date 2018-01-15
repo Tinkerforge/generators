5e4bba4768ef1e49e9c3a478b3773cb9bb3a0710
Matthias Bolte <matthias@tinkerforge.com>
2018-01-15 17:01:51 +0100

    remote-switch-v2: Better example name

--- configs/bricklet_remote_switch_v2_config.py
+++ configs/bricklet_remote_switch_v2_config.py
@@ -486,7 +486,7 @@ com['examples'].append({
 })
 
 com['examples'].append({
-'name': 'Callback',
+'name': 'Remote Callback',
 'functions': [('setter', 'Set Remote Configuration', [('uint8:constant', 0), ('uint8', 1), ('bool', 1)], 'Configure to receive from remote type A with minimum repeats set to 1 and enable callback', None),
               ('callback', ('Remote Status A', 'remote status a'), [
                 (('House Code', 'House Code'), 'uint8', 1, None, None, None, None),
