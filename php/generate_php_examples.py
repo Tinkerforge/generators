63bc33cad4158ca37eb956a8dc5bf3d5c9ce58e0
Matthias Bolte <matthias@tinkerforge.com>
2018-01-30 11:34:45 +0100

    examples: Also start to simplify comment formats in PHP

--- php/generate_php_examples.py
+++ php/generate_php_examples.py
@@ -410,18 +410,13 @@ class PHPExampleCallbackThresholdFunction(common.ExampleCallbackThresholdFunctio
         return None
 
     def get_php_source(self):
-        template = r"""// Configure threshold for {function_comment_name} "{option_comment}"{mininum_maximum_unit_comments}
+        template = r"""// Configure threshold for {function_comment_name} "{option_comment}"
 ${device_initial_name}->set{function_camel_case_name}CallbackThreshold({arguments}'{option_char}', {mininum_maximums});
 """
         mininum_maximums = []
-        mininum_maximum_unit_comments = []
 
         for mininum_maximum in self.get_minimum_maximums():
             mininum_maximums.append(mininum_maximum.get_php_source())
-            mininum_maximum_unit_comments.append(mininum_maximum.get_unit_comment())
-
-        if len(mininum_maximum_unit_comments) > 1 and len(set(mininum_maximum_unit_comments)) == 1:
-            mininum_maximum_unit_comments = mininum_maximum_unit_comments[:1]
 
         return template.format(device_initial_name=self.get_device().get_initial_name(),
                                function_camel_case_name=self.get_camel_case_name(),
@@ -429,8 +424,7 @@ ${device_initial_name}->set{function_camel_case_name}CallbackThreshold({argument
                                arguments=common.wrap_non_empty('', ', '.join(self.get_php_arguments()), ', '),
                                option_char=self.get_option_char(),
                                option_comment=self.get_option_comment(),
-                               mininum_maximums=', '.join(mininum_maximums),
-                               mininum_maximum_unit_comments=''.join(mininum_maximum_unit_comments))
+                               mininum_maximums=', '.join(mininum_maximums))
 
 class PHPExampleCallbackConfigurationFunction(common.ExampleCallbackConfigurationFunction, PHPExampleArgumentsMixin):
     def get_php_subroutine(self):
@@ -443,7 +437,7 @@ ${device_initial_name}->set{function_camel_case_name}CallbackConfiguration({argu
         templateB = r"""// Set period for {function_comment_name} callback to {period_sec_short} ({period_msec}ms) without a threshold
 ${device_initial_name}->set{function_camel_case_name}CallbackConfiguration({arguments}{period_msec}, FALSE, '{option_char}', {mininum_maximums});
 """
-        templateC = r"""// Configure threshold for {function_comment_name} "{option_comment}"{mininum_maximum_unit_comments}
+        templateC = r"""// Configure threshold for {function_comment_name} "{option_comment}"
 // with a debounce period of {period_sec_short} ({period_msec}ms)
 ${device_initial_name}->set{function_camel_case_name}CallbackConfiguration({arguments}{period_msec}, FALSE, '{option_char}', {mininum_maximums});
 """
@@ -458,14 +452,9 @@ ${device_initial_name}->set{function_camel_case_name}CallbackConfiguration({argu
         period_msec, period_sec_short, period_sec_long = self.get_formatted_period()
 
         mininum_maximums = []
-        mininum_maximum_unit_comments = []
 
         for mininum_maximum in self.get_minimum_maximums():
             mininum_maximums.append(mininum_maximum.get_php_source())
-            mininum_maximum_unit_comments.append(mininum_maximum.get_unit_comment())
-
-        if len(mininum_maximum_unit_comments) > 1 and len(set(mininum_maximum_unit_comments)) == 1:
-            mininum_maximum_unit_comments = mininum_maximum_unit_comments[:1]
 
         return template.format(device_initial_name=self.get_device().get_initial_name(),
                                function_camel_case_name=self.get_camel_case_name(),
@@ -476,8 +465,7 @@ ${device_initial_name}->set{function_camel_case_name}CallbackConfiguration({argu
                                period_sec_long=period_sec_long,
                                option_char=self.get_option_char(),
                                option_comment=self.get_option_comment(),
-                               mininum_maximums=', '.join(mininum_maximums),
-                               mininum_maximum_unit_comments=''.join(mininum_maximum_unit_comments))
+                               mininum_maximums=', '.join(mininum_maximums))
 
 class PHPExampleSpecialFunction(common.ExampleSpecialFunction):
     def get_php_subroutine(self):
