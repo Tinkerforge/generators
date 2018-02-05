2eef3c4f7179bbd5047f4fde981bc23c6ee39ce6
Olaf Lüke <olaf@tinkerforge.com>
2018-02-05 10:53:43 +0100

    Add Distance US Bricklet 2.0 config

---
+++ configs/bricklet_distance_us_v2_config.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Humidity Bricklet 2.0 communication config
+
+from commonconstants import THRESHOLD_OPTION_CONSTANTS
+from commonconstants import add_callback_value_function
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 299,
+    'name': 'Distance US V2',
+    'display_name': 'Distance US 2.0',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'packets': [],
+    'examples': []
+}
+
+distance_doc = {
+'en':
+"""
+TBD
+""",
+'de':
+"""
+TBD
+"""
+}
+
+add_callback_value_function(
+    packets   = com['packets'],
+    name      = 'Get Distance',
+    data_name = 'Distance',
+    data_type = 'uint16',
+    doc       = distance_doc
+)
+
