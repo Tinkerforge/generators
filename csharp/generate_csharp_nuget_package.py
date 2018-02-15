072be628c01b3a10ffffae72cec8500c76ab20f5
Matthias Bolte <matthias@tinkerforge.com>
2018-02-15 10:06:44 +0100

    csharp: Include a .NET Core 2 version of Tinkerforge.dll into the NuGet package

--- csharp/generate_csharp_nuget_package.py
+++ csharp/generate_csharp_nuget_package.py
@@ -31,11 +31,28 @@ import subprocess
 sys.path.append(os.path.split(os.getcwd())[0])
 import common
 
+NETCORE_CSPROJ = '''<?xml version="1.0" encoding="utf-8"?>
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <OutputType>library</OutputType>
+    <TargetFramework>netcoreapp2.0</TargetFramework>
+    <GenerateAssemblyTitleAttribute>false</GenerateAssemblyTitleAttribute>
+    <GenerateAssemblyConfigurationAttribute>false</GenerateAssemblyConfigurationAttribute>
+    <GenerateAssemblyCompanyAttribute>false</GenerateAssemblyCompanyAttribute>
+    <GenerateAssemblyProductAttribute>false</GenerateAssemblyProductAttribute>
+    <GenerateAssemblyVersionAttribute>false</GenerateAssemblyVersionAttribute>
+    <DocumentationFile>bin/Release/netcoreapp2.0/Tinkerforge.xml</DocumentationFile>
+  </PropertyGroup>
+</Project>
+'''
+
 def generate(root_dir):
-    tmp_dir                                = os.path.join(root_dir, 'nuget_package')
-    tmp_unzipped_20_dir                    = os.path.join(tmp_dir, 'unzipped_20')
-    tmp_unzipped_40_dir                    = os.path.join(tmp_dir, 'unzipped_40')
-    tmp_unzipped_40_source_tinkerforge_dir = os.path.join(tmp_unzipped_40_dir, 'source', 'Tinkerforge')
+    tmp_dir                                          = os.path.join(root_dir, 'nuget_package')
+    tmp_unzipped_net20_dir                           = os.path.join(tmp_dir, 'unzipped_net20')
+    tmp_unzipped_net40_dir                           = os.path.join(tmp_dir, 'unzipped_net40')
+    tmp_unzipped_net40_source_tinkerforge_dir        = os.path.join(tmp_unzipped_net40_dir, 'source', 'Tinkerforge')
+    tmp_unzipped_netcoreapp20_dir                    = os.path.join(tmp_dir, 'unzipped_netcoreapp20')
+    tmp_unzipped_netcoreapp20_source_tinkerforge_dir = os.path.join(tmp_unzipped_netcoreapp20_dir, 'source', 'Tinkerforge')
 
     # Make directories
     common.recreate_dir(tmp_dir)
@@ -47,21 +64,32 @@ def generate(root_dir):
                     '-q',
                     os.path.join(root_dir, 'tinkerforge_csharp_bindings_{0}_{1}_{2}.zip'.format(*version)),
                     '-d',
-                    tmp_unzipped_20_dir])
+                    tmp_unzipped_net20_dir])
 
-    shutil.copytree(tmp_unzipped_20_dir, tmp_unzipped_40_dir)
+    shutil.copytree(tmp_unzipped_net20_dir, tmp_unzipped_net40_dir)
+    shutil.copytree(tmp_unzipped_net20_dir, tmp_unzipped_netcoreapp20_dir)
 
-    # Make dll for NET 4.0
-    with common.ChangedDirectory(tmp_unzipped_40_dir):
+    # Make DLL for NET 4.0
+    with common.ChangedDirectory(tmp_unzipped_net40_dir):
         common.execute(['/usr/bin/mcs',
                         '/debug:full',
                         '/optimize+',
                         '/warn:4',
                         '/sdk:4',
                         '/target:library',
-                        '/doc:' + os.path.join(tmp_unzipped_40_dir, 'Tinkerforge.xml'),
-                        '/out:' + os.path.join(tmp_unzipped_40_dir, 'Tinkerforge.dll'),
-                        os.path.join(tmp_unzipped_40_source_tinkerforge_dir, '*.cs')])
+                        '/doc:' + os.path.join(tmp_unzipped_net40_dir, 'Tinkerforge.xml'),
+                        '/out:' + os.path.join(tmp_unzipped_net40_dir, 'Tinkerforge.dll'),
+                        os.path.join(tmp_unzipped_net40_source_tinkerforge_dir, '*.cs')])
+
+    # Make DLL for NET Core 2.0
+    with open(os.path.join(tmp_unzipped_netcoreapp20_source_tinkerforge_dir, 'Tinkerforge.csproj'), 'w') as f:
+        f.write(NETCORE_CSPROJ)
+
+    with common.ChangedDirectory(tmp_unzipped_netcoreapp20_source_tinkerforge_dir):
+        common.execute(['/usr/bin/dotnet',
+                        'build',
+                        '-c',
+                        'Release'])
 
     # Download nuget.exe
     with common.ChangedDirectory(tmp_dir):
