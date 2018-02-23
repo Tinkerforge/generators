9909358c687532bc12ed5473420a457b7b00a685
Matthias Bolte <matthias@tinkerforge.com>
2018-02-23 16:57:48 +0100

    Exclude stubs fromn test-all

--- test_all.py
+++ test_all.py
@@ -34,7 +34,7 @@ else:
 bindings = sorted(list(bindings))
 
 for binding in bindings:
-    if binding in ['tcpip', 'modbus', 'json', 'tvpl']:
+    if binding in ['tcpip', 'modbus', 'json', 'stubs', 'tvpl']:
         continue
 
     path_binding = os.path.join(path, binding)
