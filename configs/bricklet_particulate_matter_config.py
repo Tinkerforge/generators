ed0ca5a0a833d0e2a58991a508829b4f81b7bdb6
Olaf Lüke <olaf@tinkerforge.com>
2018-03-09 17:12:25 +0100

    Add Particulate Matter Bricklet stub config

---
+++ configs/bricklet_particulate_matter_config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Particulate Matter Bricklet communication config
+
+from commonconstants import THRESHOLD_OPTION_CONSTANTS
+from commonconstants import add_callback_value_function
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 2110,
+    'name': 'Particulate Matter',
+    'display_name': 'Particulate Matter',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'TBD',
+        'de': 'TBD'
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'packets': [],
+    'examples': []
+}
