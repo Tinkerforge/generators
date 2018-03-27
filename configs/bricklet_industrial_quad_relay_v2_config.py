e3066d170eadd2dc09b73b67dba1ba1536593fd0
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2018-03-27 09:56:51 +0200

    industrial-quad-relay-v2: Rename "pin" to "channel"

--- configs/bricklet_industrial_quad_relay_v2_config.py
+++ configs/bricklet_industrial_quad_relay_v2_config.py
@@ -69,50 +69,50 @@ Gibt die Werte zurück, wie von :func:`Set Output Value` gesetzt.
 com['packets'].append({
 'type': 'function',
 'name': 'Set Monoflop',
-'elements': [('Pin', 'uint8', 1, 'in'),
+'elements': [('Channel', 'uint8', 1, 'in'),
              ('Value', 'bool', 1, 'in'),
              ('Time', 'uint32', 1, 'in')],
 'since_firmware': [1, 0, 0],
 'doc': ['af', {
 'en':
 """
-Configures a monoflop of the specified pin.
+Configures a monoflop of the specified channel.
 
 The second parameter is the desired value of the specified
-pin. A *true* means relay closed and a *false* means relay open.
+channel. A *true* means relay closed and a *false* means relay open.
 
-The third parameter indicates the time (in ms) that the pins should hold
+The third parameter indicates the time (in ms) that the channels should hold
 the value.
 
-If this function is called with the parameters (0, 1, 1500) pin 0 will 
-close and in 1.5s pin 0 will open again
+If this function is called with the parameters (0, 1, 1500) channel 0 will
+close and in 1.5s channel 0 will open again
 
 A monoflop can be used as a fail-safe mechanism. For example: Lets assume you
 have a RS485 bus and a Quad Relay Bricklet connected to one of the slave
 stacks. You can now call this function every second, with a time parameter
-of two seconds and pin 0 closed. Pin 0 will be closed all the time. If now
-the RS485 connection is lost, then pin 0 will be opened in at most two seconds.
+of two seconds and channel 0 closed. Channel 0 will be closed all the time. If now
+the RS485 connection is lost, then channel 0 will be opened in at most two seconds.
 """,
 'de':
 """
-Konfiguriert einen Monoflop für den angegebenen Pin.
+Konfiguriert einen Monoflop für den angegebenen Channel.
 
 Der zweite Parameter ist eine der gewünschten Zuständ des
-festgelegten Pins. Eine *true bedeutet Relais geschlossen und
+festgelegten Channels. Eine *true bedeutet Relais geschlossen und
 ein *false* bedeutet Relais offen.
 
-Der dritte Parameter ist die Zeit (in ms) die der Pin den Zustand
+Der dritte Parameter ist die Zeit (in ms) die der Channel den Zustand
 halten sollen.
 
 Wenn diese Funktion mit den Parametern (0, 1, 1500) aufgerufen wird,
-wird pin 0 geschlossen und nach 1,5s wieder geöffnet.
+wird channel 0 geschlossen und nach 1,5s wieder geöffnet.
 
 Ein Monoflop kann zur Ausfallsicherung verwendet werden. Beispiel:
 Angenommen ein RS485 Bus und ein Quad Relay Bricklet ist an ein Slave
 Stapel verbunden.
 Jetzt kann diese Funktion sekündlich, mit einem Zeitparameter von 2 Sekunden,
-aufgerufen werden. Der Pin wird die gesamte Zeit im Zustand geschlossen sein.
-Wenn jetzt die RS485 Verbindung getrennt wird, wird der Pin nach spätestens
+aufgerufen werden. Der Channel wird die gesamte Zeit im Zustand geschlossen sein.
+Wenn jetzt die RS485 Verbindung getrennt wird, wird der Channel nach spätestens
 zwei Sekunden in den Zustand geöffnet wechseln.
 """
 }]
@@ -121,7 +121,7 @@ zwei Sekunden in den Zustand geöffnet wechseln.
 com['packets'].append({
 'type': 'function',
 'name': 'Get Monoflop',
-'elements': [('Pin', 'uint8', 1, 'in'),
+'elements': [('Channel', 'uint8', 1, 'in'),
              ('Value', 'bool', 1, 'out'),
              ('Time', 'uint32', 1, 'out'),
              ('Time Remaining', 'uint32', 1, 'out')],
@@ -129,7 +129,7 @@ com['packets'].append({
 'doc': ['af', {
 'en':
 """
-Returns (for the given pin) the current value and the time as set by
+Returns (for the given channel) the current value and the time as set by
 :func:`Set Monoflop` as well as the remaining time until the value flips.
 
 If the timer is not running currently, the remaining time will be returned
@@ -137,7 +137,7 @@ as 0.
 """,
 'de':
 """
-Gibt (für den angegebenen Pin) den aktuellen Zustand und die Zeit, wie von
+Gibt (für den angegebenen Channel) den aktuellen Zustand und die Zeit, wie von
 :func:`Set Monoflop` gesetzt, sowie die noch verbleibende Zeit bis zum
 Zustandswechsel, zurück.
 
@@ -149,21 +149,21 @@ Wenn der Timer aktuell nicht läuft, ist die noch verbleibende Zeit 0.
 com['packets'].append({
 'type': 'callback',
 'name': 'Monoflop Done',
-'elements': [('Pin', 'uint8', 1, 'out'),
+'elements': [('Channel', 'uint8', 1, 'out'),
              ('Value', 'bool', 1, 'out')],
 'since_firmware': [1, 0, 0],
 'doc': ['c', {
 'en':
 """
 This callback is triggered whenever a monoflop timer reaches 0. The
-:word:`parameters` contain the pin and the current value of the pin
+:word:`parameters` contain the channel and the current value of the channel
 (the value after the monoflop).
 """,
 'de':
 """
 Dieser Callback wird ausgelöst wenn ein Monoflop Timer abläuft (0 erreicht).
-:word:`parameters` enthalten den Pin und den aktuellen
-Zustand des Pins (der Zustand nach dem Monoflop).
+:word:`parameters` enthalten den Channel und den aktuellen
+Zustand des Channels (der Zustand nach dem Monoflop).
 """
 }]
 })
@@ -171,17 +171,17 @@ Zustand des Pins (der Zustand nach dem Monoflop).
 com['packets'].append({
 'type': 'function',
 'name': 'Set Selected Output Value',
-'elements': [('Pin', 'uint8', 1, 'in'),
+'elements': [('Channel', 'uint8', 1, 'in'),
              ('Value', 'bool', 1, 'in')],
 'since_firmware': [1, 0, 0],
 'doc': ['af', {
 'en':
 """
-Sets the output value of the specified pin without affecting the other pins.
+Sets the output value of the specified channel without affecting the other channels.
 """,
 'de':
 """
-Setzt den Ausgabewert des spezifizierten Pins ohne die anderen Pinne zu beeinflussen.
+Setzt den Ausgabewert des spezifizierten Channels ohne die anderen Channelne zu beeinflussen.
 """
 }]
 })
