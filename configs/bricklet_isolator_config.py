96b17faf44a9849ff331169a754dd44d89e0a2ae
Olaf Lüke <olaf@tinkerforge.com>
2018-05-17 19:36:45 +0200

    Add stub config for Isolator Bricklet

---
+++ configs/bricklet_isolator_config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Isolator Bricklet communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 2122,
+    'name': 'Isolator',
+    'display_name': 'Isolator',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'TBD',
+        'de': 'TBD'
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'packets': [],
+    'examples': []
+}
+
