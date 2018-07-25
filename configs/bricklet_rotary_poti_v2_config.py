0f971b18b841a31e1e114a6cc92ea6474a2bbcbf
Lukas Lauer <lukas@tinkerforge.com>
2018-07-25 15:35:54 +0200

    Add configs

---
+++ configs/bricklet_rotary_poti_v2_config.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# RGB LED Bricklet 2.0 communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 2140,
+    'name': 'Rotary Poti V2',
+    'display_name': 'Rotary Poti 2.0',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'packets': [],
+    'examples': []
+}
+
+
