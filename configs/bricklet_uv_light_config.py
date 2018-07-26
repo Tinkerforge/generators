5a7fea3d544a69e5308e15cc4ab8b0b462467bec
Matthias Bolte <matthias@tinkerforge.com>
2018-07-26 14:00:43 +0200

    uv-light: Fix intensity unit documentation

--- configs/bricklet_uv_light_config.py
+++ configs/bricklet_uv_light_config.py
@@ -36,10 +36,10 @@ com['packets'].append({
 'en':
 """
 Returns the UV light intensity of the sensor, the intensity is given
-in µW/cm².
+in 1/10 mW/m².
 
 To get UV Index you have to divide the value by 250. For example, a UV Light
-intensity of 500µW/cm² is equivalent to an UV Index of 2.
+intensity of 500 is equivalent to an UV Index of 2.
 
 If you want to get the intensity periodically, it is recommended to use the
 :cb:`UV Light` callback and set the period with
@@ -48,10 +48,10 @@ If you want to get the intensity periodically, it is recommended to use the
 'de':
 """
 Gibt die UV-Licht-Intensität des Sensors zurück. Die Intensität wird
-in der Einheit µW/cm² gegeben.
+in der Einheit 1/10 mW/m² gegeben.
 
 Die Intensität kann einfach durch 250 geteilt werden um den UV Index zu
-bestimmen. Beispiel: Eine UV-Licht-Intensität von 500µW/cm² entspricht
+bestimmen. Beispiel: Eine UV-Licht-Intensität von 500 entspricht
 einem UV Index von 2.
 
 Wenn die Intensität periodisch abgefragt werden soll, wird empfohlen
@@ -287,18 +287,18 @@ mit :func:`Set Debounce Period` gesetzt, ausgelöst.
 
 com['examples'].append({
 'name': 'Simple',
-'functions': [('getter', ('Get UV Light', 'UV light'), [(('UV Light', 'UV Light'), 'uint32', 1, None, 'µW/cm²', None)], [])]
+'functions': [('getter', ('Get UV Light', 'UV light'), [(('UV Light', 'UV Light'), 'uint32', 10, None, 'mW/m²', None)], [])]
 })
 
 com['examples'].append({
 'name': 'Callback',
-'functions': [('callback', ('UV Light', 'UV light'), [(('UV Light', 'UV Light'), 'uint32', 1, None, 'µW/cm²', None)], None, None),
+'functions': [('callback', ('UV Light', 'UV light'), [(('UV Light', 'UV Light'), 'uint32', 10, None, 'mW/m²', None)], None, None),
               ('callback_period', ('UV Light', 'UV light'), [], 1000)]
 })
 
 com['examples'].append({
 'name': 'Threshold',
 'functions': [('debounce_period', 10000),
-              ('callback', ('UV Light Reached', 'UV light reached'), [(('UV Light', 'UV Light'), 'uint32', 1, None, 'µW/cm²', None)], None, 'UV Index > 3. Use sunscreen!'),
+              ('callback', ('UV Light Reached', 'UV light reached'), [(('UV Light', 'UV Light'), 'uint32', 10, None, 'mW/m²', None)], None, 'UV Index > 3. Use sunscreen!'),
               ('callback_threshold', ('UV Light', 'UV light'), [], '>', [(250*3, 0)])]
 })
