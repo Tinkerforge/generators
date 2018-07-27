bd33e2063009d717cb7b4c465f1f6b33d57b0f07
Lukas Lauer <lukas@tinkerforge.com>
2018-07-27 11:09:14 +0200

    Add configs for new bricklet proto panel

---
+++ configs/bricklet_hall_effect_v2_config.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# RGB LED Bricklet 2.0 communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 2132,
+    'name': 'Hall Effect V2',
+    'display_name': 'Hall Effect 2.0',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'packets': [],
+    'examples': []
+}
+
+
---
+++ configs/bricklet_joystick_v2_config.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# RGB LED Bricklet 2.0 communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 2138,
+    'name': 'Joystick V2',
+    'display_name': 'Joystick 2.0',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'packets': [],
+    'examples': []
+}
+
+
