ad1020832e8ab28d00018e0bffc4dfa2af0001f2
Matthias Bolte <matthias@tinkerforge.com>
2018-08-23 12:19:35 +0200

    java: Add custom readme.txt

--- java/generate_java_zip.py
+++ java/generate_java_zip.py
@@ -102,6 +102,7 @@ class JavaZipGenerator(common.ZipGenerator):
             shutil.copy(os.path.join(root_dir, '..', 'configs', 'license.txt'),   self.tmp_dir)
         else:
             shutil.copy(os.path.join(self.get_config_dir(), 'changelog.txt'),   self.tmp_dir)
+            shutil.copy(os.path.join(root_dir, 'custom.txt'),                   os.path.join(self.tmp_dir, 'readme.txt'))
 
         # Make manifest
         version = self.get_changelog_version()
