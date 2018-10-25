20f5baa2c8b17bdacfc7cc571986edb59b132c54
Olaf Lüke <olaf@tinkerforge.com>
2018-10-25 17:55:03 +0200

    Add Bricklet HAT Zero config

---
+++ configs/bricklet_hat_zero_config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# HAT Zero Bricklet communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': 2141,
+    'name': 'HAT Zero',
+    'display_name': 'HAT Zero',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'comcu': True,
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'packets': [],
+    'examples': []
+}
+
