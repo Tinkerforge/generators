88b11d3ee018aa215463d3ed0565c642878b448e
Olaf Lüke <olaf@tinkerforge.com>
2019-02-18 12:59:23 +0100

    Add API documentation for CO2 Bricklet 2.0

--- configs/bricklet_co2_v2_config.py
+++ configs/bricklet_co2_v2_config.py
@@ -18,8 +18,8 @@ com = {
     'display_name': 'CO2 2.0',
     'manufacturer': 'Tinkerforge',
     'description': {
-        'en': 'Measures CO2 concentration in ppm',
-        'de': 'Misst CO2-Konzentration in ppm'
+        'en': 'Measures CO2 concentration, temperature and humidity',
+        'de': 'Misst CO2-Konzentration, Temperatur und Luftfeuchte'
     },
     'comcu': True,
     'released': False,
@@ -39,11 +39,15 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-
+Returns all values measured by the CO2 Bricklet 2.0. The values are
+CO2 concentration (in ppm), Temperature (in 0.01 °C) 
+and Humidity (in 0.01 %RH).
 """,
 'de':
 """
-
+Gibt alle Werte zurück, die das CO2 Bricklet 2.0 misst. Diese Werte umfassen:
+CO2-Konzentraion (in ppm), Temperatur (in 0,01 °C) und 
+Luftfeuchte (in 0,01 %RH).
 """
 }]
 })
@@ -56,11 +60,34 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+The CO2 concentration (among other things) depends on the ambient air pressure.
 
+To increase the accuracy of the CO2 Bricklet 2.0 you can set the current air pressure.
+You use the Barometer Bricklet 2.0 or the Air Quality Bricklet to get the current air pressure.
+
+The expected unit of the ambient air pressure value is mbar.
+
+By default air pressure compensation is disabled. Once you set a value it
+will be used for compensation. You can turn the compensation off again by 
+setting the value to 0.
+
+It is sufficient to update the value every few minutes.
 """,
 'de':
 """
+Die CO2-Konzentration hängt (unter anderem) von dem Umgebungs-Luftdruck ab.
 
+Um die Genauigkeit des CO2 Bricklet 2.0 zu verbessern ist es möglich den aktuellen
+Luftdruck zu setzen. Dazu kann das Barometer Bricklet 2.0 oder auch das Air Quality Bricklet
+genutzt werden.
+
+Die erwartete Einheit des Umgebungs-Luftdrucks ist mbar.
+
+Standardmäßg ist die Luftdruck-Kompensation deaktiviert. Sobald ein Wert gesetzt
+wird, wird dieser zur Kompensation verwerndet. Die Kompensation kann wieder
+ausgestellt werden in dem der Wert zurück auf 0 gesetzt wird.
+
+Es ist hinreichend den Wert alle paar Minuten zu aktualisieren.
 """
 }]
 })
@@ -73,11 +100,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-
+Returns the ambient air pressure as set by :func:`Set Air Pressure`.
 """,
 'de':
 """
-
+Gibt den Umgebungs-Luftdruch zurück, wie von :func:`Set Aire Pressure` gesetzt.
 """
 }]
 })
@@ -94,14 +121,38 @@ com['packets'].append({
 Sets a temperature offset with resolution 1/100°C. A offset of 10 will decrease
 the measured temperature by 0.1°C.
 
+If you install this Bricklet into an enclosure and you want to measure the ambient
+temperature, you may have to decrease the measured temperature by some value to
+compensate for the error because of the heating inside of the enclosure.
 
+We recommend that you leave the parts in the enclosure running for at least
+24 hours such that a temperature equilibrium can be reached. After that you can measure
+the temperature directly outside of enclosure and set the difference as offset.
+
+This temperature offset is used to calculate the relative humidity and
+CO2 concentration. In case the Bricklet is installed in an enclosure, we
+recommend to measure and set the temperature offset to imporve the accuracy of
+the measurements.
 """,
 'de':
 """
 Setzt ein Temperatur-Offset mit Auflösung 1/100°C. Ein Offset von 10 verringert
 die gemessene Temperatur um 0,1°C.
 
+Wenn das Bricklet in einem Gehäuse verbaut wird, aber die Umgebungstemperatur
+außerhalb des Gehäuses gemessen werden soll, dann muss vom gemessenen Temperatur
+ein bestimmter Wert abgezogen werden, um den Messfehler durch das Aufheizen des
+Gehäuses zu kompensieren.
 
+Wir empfehlen den Messaufbau im Gehäuse mindestens 24 Stunden laufen zu lassen,
+damit sich ein Temperaturgleichgewicht einstellt. Danach muss die Temperatur
+außerhalb des Gehäuses gemessen werden und die Differenz zur Temperatur innerhalb
+des Gehäuses als Offset eingestellt werden.
+
+Dieses Temperatur-Offset geht in die Berechnung der Luftfeuchte und der
+CO2-Konzentration mit ein. Um die Genauigkeit der Messwerte innerhalb eines 
+Gehäuses zu verbessern sollte der Temperatur-Offset bestimmt und
+eingestellt werden.
 """
 }]
 })
@@ -267,3 +318,14 @@ add_callback_value_function(
     data_type = 'uint16',
     doc       = humidity_doc
 )
+
+com['examples'].append({
+'name': 'Simple',
+'functions': [('getter', ('Get All Values', 'all values'), [(('CO2 Concentration', 'CO2 Concentration'), 'uint16', 1, None, 'ppm', None), (('Temperature', 'Temperature'), 'int16', 1, 100.0, '°C', None), (('Humidity', 'Humidity'), 'uint16', 1, 100.0, '%RH', None)], [])]
+})
+
+com['examples'].append({
+'name': 'Callback',
+'functions': [('callback', ('All Values', 'all values'), [(('CO2 Concentration', 'CO2 Concentration'), 'uint16', 1, None, 'ppm', None), (('Temperature', 'Temperature'), 'int16', 1, 100.0, '°C', None), (('Humidity', 'Humidity'), 'uint16', 1, 100.0, '%RH', None)], None, None),
+              ('callback_configuration', ('All Values', 'all values'), [], 1000, False, None, [])]
+})
