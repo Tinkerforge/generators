0684224dc616aeb72bad44902c509c44cc8a12b9
Olaf Lüke <olaf@tinkerforge.com>
2019-03-19 15:56:10 +0100

    Adjust HAT config to hardware version 1.4

--- configs/bricklet_hat_config.py
+++ configs/bricklet_hat_config.py
@@ -26,41 +26,9 @@ com = {
     'examples': []
 }
 
-WEEKDAY = ('Weekday', [('Monday', 1),
-                       ('Tuesday', 2),
-                       ('Wednesday', 3),
-                       ('Thursday', 4),
-                       ('Friday', 5),
-                       ('Saturday', 6),
-                       ('Sunday', 7)])
-
 com['packets'].append({
 'type': 'function',
-'name': 'Get Battery Statistics',
-'elements': [('Battery Connected', 'bool', 1, 'out'),
-             ('Capacity Full', 'int32', 1, 'out'),
-             ('Capacity Nominal', 'int32', 1, 'out'),
-             ('Capacity Remaining', 'int32', 1, 'out'),
-             ('Percentage Charge', 'int32', 1, 'out'),
-             ('Voltage Battery', 'int32', 1, 'out'),
-             ('Voltage USB', 'int32', 1, 'out'),
-             ('Voltage DC', 'int32', 1, 'out'),
-             ('Current Flow', 'int32', 1, 'out'),
-             ('Temperature Battery', 'int32', 1, 'out')],
-'since_firmware': [1, 0, 0],
-'doc': ['bf', {
-'en':
-"""
-""",
-'de':
-"""
-"""
-}]
-})
-
-com['packets'].append({
-'type': 'function',
-'name': 'Set Power Off',
+'name': 'Set Sleep Mode',
 'elements': [('Power Off Delay', 'uint32', 1, 'in'),
              ('Power Off Duration', 'uint32', 1, 'in'),
              ('Raspberry Pi Off', 'bool', 1, 'in'),
@@ -80,7 +48,7 @@ Enable Sleep Indicator => status led blinks in 1s interval => ~0.3mA
 
 com['packets'].append({
 'type': 'function',
-'name': 'Get Power Off',
+'name': 'Get Sleep Mode',
 'elements': [('Power Off Delay', 'uint32', 1, 'out'),
              ('Power Off Duration', 'uint32', 1, 'out'),
              ('Raspberry Pi Off', 'bool', 1, 'out'),
@@ -97,16 +65,11 @@ com['packets'].append({
 }]
 })
 
+
 com['packets'].append({
 'type': 'function',
-'name': 'Set Time',
-'elements': [('Year', 'uint16', 1, 'in'),
-             ('Month', 'uint8', 1, 'in'),
-             ('Day', 'uint8', 1, 'in'),
-             ('Hour', 'uint8', 1, 'in'),
-             ('Minute', 'uint8', 1, 'in'),
-             ('Second', 'uint8', 1, 'in'),
-             ('Weekday', 'uint8', 1, 'in', WEEKDAY)],
+'name': 'Set Bricklet Power',
+'elements': [('Bricklet Power', 'bool', 1, 'in')],
 'since_firmware': [1, 0, 0],
 'doc': ['bf', {
 'en':
@@ -120,14 +83,8 @@ com['packets'].append({
 
 com['packets'].append({
 'type': 'function',
-'name': 'Get Time',
-'elements': [('Year', 'uint16', 1, 'out'),
-             ('Month', 'uint8', 1, 'out'),
-             ('Day', 'uint8', 1, 'out'),
-             ('Hour', 'uint8', 1, 'out'),
-             ('Minute', 'uint8', 1, 'out'),
-             ('Second', 'uint8', 1, 'out'),
-             ('Weekday', 'uint8', 1, 'out', WEEKDAY)],
+'name': 'Get Bricklet Power',
+'elements': [('Bricklet Power', 'bool', 1, 'out')],
 'since_firmware': [1, 0, 0],
 'doc': ['bf', {
 'en':
@@ -139,15 +96,14 @@ com['packets'].append({
 }]
 })
 
+
 com['packets'].append({
 'type': 'function',
-'name': 'Set Battery Parameters',
-'elements': [('Nominal Capacity', 'uint16', 1, 'in'),
-             ('Charge Termination Current', 'uint16', 1, 'in'),
-             ('Empty Voltage', 'uint16', 1, 'in'),
-             ('Learned Parameters', 'uint16', 5, 'in')],
+'name': 'Get Voltages',
+'elements': [('Voltage USB', 'uint16', 1, 'out'),
+             ('Voltage DC', 'uint16', 1, 'out')],
 'since_firmware': [1, 0, 0],
-'doc': ['af', {
+'doc': ['bf', {
 'en':
 """
 """,
@@ -156,22 +112,3 @@ com['packets'].append({
 """
 }]
 })
-
-com['packets'].append({
-'type': 'function',
-'name': 'Get Battery Parameters',
-'elements': [('Nominal Capacity', 'uint16', 1, 'out'),
-             ('Charge Termination Current', 'uint16', 1, 'out'),
-             ('Empty Voltage', 'uint16', 1, 'out'),
-             ('Learned Parameters', 'uint16', 5, 'out')],
-'since_firmware': [1, 0, 0],
-'doc': ['af', {
-'en':
-"""
-""",
-'de':
-"""
-"""
-}]
-})
-
