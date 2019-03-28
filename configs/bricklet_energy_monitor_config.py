9b1b68552ae42b7203785dd1e58ad9e79d77e449
Olaf Lüke <olaf@tinkerforge.com>
2019-03-28 16:15:21 +0100

    Add english language documentation

--- configs/bricklet_energy_monitor_config.py
+++ configs/bricklet_energy_monitor_config.py
@@ -45,7 +45,22 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
-frequency calculated every 6 seconds
+Returns all of the measurements that are done by the Energy Monitor Bricklet.
+
+* Voltage (V): Voltage RMS with a resolution of 10mV (example: 230.05V = 23005)
+* Current (A): Current RMS with a resolution of 10mA (example: 1.42A = 142)
+* Energy (Wh): Energy (integrated over time) with a resoluton of 10mWh (example: 1.1kWh = 110000)
+* Real Power (W): Real Power with a resolution of 10mW (example: 1234.56W = 123456)
+* Apparent Power (VA): Apparent Power with a resolution of 10mVA (example: 1234.56VA = 123456)
+* Reactive Power (VAR): Reactive Power with a resolution of 10mVAR (example: 1234.56VAR = 123456)
+* Power Factor: Power Factor with a resolution of 1/1000 (example: PF 0.995 = 995)
+* Frequency (Hz): AC Frequency of the mains voltage in 1/100 Hz (example: 50Hz = 5000)
+
+The frequency is recalculated every 6s.
+
+All other values are integrated over 10 zero-crossings of the voltage sine wave.
+With a standard AC mains voltage frequecy of 50Hz this results in a 5 measurements
+per second (or an integration time of 200ms per measurement).
 """,
 'de':
 """
@@ -61,9 +76,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Sets the energy value (see :func:`Get Energy Data`) back to 0Wh.
 """,
 'de':
 """
+Setzt den Energiewert (siehe :func:`Get Energy Data`) zurück auf 0Wh
 """
 }]
 })
@@ -80,7 +97,16 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns a snapshot of the voltage and current waveform. The values
+in the returned array alternate between voltage and current. The data from
+one getter call contains 768 data points for voltage and current, which
+correspond to about 3 full sine waves.
 
+The voltage is given with a resolution of 100mV and the current is given
+with a resolution of 10mA.
+
+This data is meant to be used for a non-realtime graphical representation of
+the voltage and current waveforms.
 """,
 'de':
 """
@@ -98,9 +124,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns *true* if a voltage/current transformer is connected to the Bricklet.
 """,
 'de':
 """
+Gibt *true* zurück wenn ein Spannungs-/Stromtransformator mit dem Bricklet verbunden ist.
 """
 }]
 })
@@ -115,6 +143,20 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Sets the transformer ratio for the voltage and current transformer in 1/100 form.
+
+Example: If your mains voltage is 230V, you use 9V voltage transformer and a
+1V:30A current clamp your voltage ratio is 230/9 = 25.56 and your current ratio
+is 30/1 = 30.
+
+In this case you have to set the values 2556 and 3000 for voltage ratio and current
+ratio.
+
+The calibration is saved in non-volatile memory, you only have to set it once.
+
+By default the voltage ratio is set to TODO and the current ratio is set to TODO.
+
+Set the phase shift to 0. It is for future use and currently not supported by the Bricklet.
 """,
 'de':
 """
@@ -132,9 +174,11 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Returns the transformer calibration as set by :func:`Set Transformer Calibration`.
 """,
 'de':
 """
+Gibt die Transformator-Kalibrierung zurück, wie von :func:`Set Transformer Calibration` gesetzt.
 """
 }]
 })
@@ -147,6 +191,16 @@ com['packets'].append({
 'doc': ['bf', {
 'en':
 """
+Calling this function will start an offset calibration. The offset calibration will
+integrate the voltage and current waveform over a longer time period to find the 0
+transition point in the sine wave.
+
+The Bricklet comes with a factory-calibrated offset value, you should not have to
+call this function.
+
+If you want to re-calibrate the offset we recommend that you connect a load that
+has a smooth sinusoidal voltage and current waveform. Alternatively you can also
+short both inputs.
 """,
 'de':
 """
@@ -216,14 +270,14 @@ Gibt die Callback-Konfiguration zurück, wie mittels
 com['packets'].append({
 'type': 'callback',
 'name': 'Energy Data',
-'elements': [('Voltage', 'int32', 1, 'out'),        # mV RMS
-             ('Current', 'int32', 1, 'out'),        # mA RMS
-             ('Energy', 'int32', 1, 'out'),         # 10mWh
-             ('Real Power', 'int32', 1, 'out'),     # W
-             ('Apparent Power', 'int32', 1, 'out'), # VA
-             ('Reactive Power', 'int32', 1, 'out'), # VAR
+'elements': [('Voltage', 'int32', 1, 'out'),
+             ('Current', 'int32', 1, 'out'),
+             ('Energy', 'int32', 1, 'out'),
+             ('Real Power', 'int32', 1, 'out'),
+             ('Apparent Power', 'int32', 1, 'out'),
+             ('Reactive Power', 'int32', 1, 'out')
              ('Power Factor', 'uint16', 1, 'out'),
-             ('Frequency', 'uint16', 1, 'out')],    # 1/100Hz
+             ('Frequency', 'uint16', 1, 'out')],
 'since_firmware': [1, 0, 0],
 'doc': ['c', {
 'en':
