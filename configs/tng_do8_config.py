29e6c4dea1e38dc165d85e57a4e738ecd9989c19
Olaf Lüke <olaf@tinkerforge.com>
2019-07-05 16:28:21 +0200

    Add stub version of TNG DO8 module (for testing)

---
+++ configs/tng_do8_config.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# TNG-DI8 communication config
+
+com = {
+    'author': 'Olaf Lüke <olaf@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'TNG',
+    'device_identifier': 202,
+    'name': 'DO8',
+    'display_name': 'DO8',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'TBD',
+        'de': 'TBD'
+    },
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'features': [
+        'tng'
+    ],
+    'packets': [],
+    'examples': []
+}
+
+CONSTANT_CHANNEL = ('Channel', [('0', 0),
+                                ('1', 1),
+                                ('2', 2),
+                                ('3', 3),
+                                ('4', 4),
+                                ('5', 5),
+                                ('6', 6),
+                                ('7', 7)])
+
+com['packets'].append({
+'type': 'function',
+'name': 'Set Value',
+'elements': [('Timestamp', 'uint64', 1, 'in'),
+             ('Value', 'bool', 8, 'in')],
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+Sets the output value of all four channels. A value of *true* or *false* outputs
+logic 1 or logic 0 respectively on the corresponding channel.
+""",
+'de':
+"""
+Setzt den Zustand aller vier Kanäle. Der Wert *true* bzw. *false* erzeugen
+logisch 1 bzw. logisch 0 auf dem entsprechenden Kanal.
+"""
+}]
+})
+
+com['examples'].append({
+'name': 'Simple',
+'functions': [('setter', 'Set Value', [('bool', [True, False, True, False, True, False, True, False])], None, None)]
+})
