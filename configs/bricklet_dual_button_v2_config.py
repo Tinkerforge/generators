650eb283b56be9759125ae3a87fd0cc9fe9e023b
Erik Fleckstein <erik@tinkerforge.com>
2019-08-14 14:27:35 +0200

    dual-button-v2: example: Enable state changed callback.

--- configs/bricklet_dual_button_v2_config.py
+++ configs/bricklet_dual_button_v2_config.py
@@ -255,5 +255,6 @@ gesetzt.
 
 com['examples'].append({
 'name': 'Callback',
-'functions': [('callback', ('State Changed', 'state changed'), [(('Button L', 'Left Button'), 'uint8:constant', 1, None, None, None), (('Button R', 'Right Button'), 'uint8:constant', 1, None, None, None), (('LED L', None), 'uint8:constant', 1, None, None, None), (('LED R', None), 'uint8:constant', 1, None, None, None)], None, None)]
+'functions': [('callback', ('State Changed', 'state changed'), [(('Button L', 'Left Button'), 'uint8:constant', 1, None, None, None), (('Button R', 'Right Button'), 'uint8:constant', 1, None, None, None), (('LED L', None), 'uint8:constant', 1, None, None, None), (('LED R', None), 'uint8:constant', 1, None, None, None)], None, None),
+              ('setter', 'Set State Changed Callback Configuration', [('bool', True)], 'Enable state changed callback', None)]
 })
