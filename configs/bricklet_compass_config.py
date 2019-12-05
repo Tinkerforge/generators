eb81b1f2cb602624050c0c262a4717faa7d57c02
Matthias Bolte <matthias@tinkerforge.com>
2019-12-05 13:31:12 +0100

    Whitespace

--- configs/bricklet_compass_config.py
+++ configs/bricklet_compass_config.py
@@ -276,8 +276,8 @@ com['packets'].append({
 'type': 'function',
 'name': 'Get Calibration',
 'elements': [('Offset', 'int16', 3, 'out', [{'name': 'X', 'scale': (1, 10**8), 'unit': 'Tesla'},
-                                           {'name': 'Y', 'scale': (1, 10**8), 'unit': 'Tesla'},
-                                           {'name': 'Z', 'scale': (1, 10**8), 'unit': 'Tesla'}]),
+                                            {'name': 'Y', 'scale': (1, 10**8), 'unit': 'Tesla'},
+                                            {'name': 'Z', 'scale': (1, 10**8), 'unit': 'Tesla'}]),
              ('Gain', 'int16', 3, 'out', [{'name': 'X'}, {'name': 'Y'}, {'name': 'Z'}])],
 'since_firmware': [1, 0, 0],
 'doc': ['af', {
