0d6878933bb3ac8fe9bd966beafa8030aad10332
Matthias Bolte <matthias@tinkerforge.com>
2019-12-09 12:08:51 +0100

    Remove unused code

--- configs/tng_di8_config.py
+++ configs/tng_di8_config.py
@@ -29,15 +29,6 @@ com = {
     'examples': []
 }
 
-CONSTANT_CHANNEL = ('Channel', [('0', 0),
-                                ('1', 1),
-                                ('2', 2),
-                                ('3', 3),
-                                ('4', 4),
-                                ('5', 5),
-                                ('6', 6),
-                                ('7', 7)])
-
 com['packets'].append({
 'type': 'function',
 'name': 'Get Value',
