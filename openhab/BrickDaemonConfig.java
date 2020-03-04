4be4669c35db734351be024987a81967cd8607b0
Erik Fleckstein <erik@tinkerforge.com>
2020-03-04 14:13:28 +0100

    openhab: Add missing BrickDaemonConfig default values.

--- openhab/BrickDaemonConfig.java
+++ openhab/BrickDaemonConfig.java
@@ -1,12 +1,14 @@
 package org.eclipse.smarthome.binding.tinkerforge.internal.device;
 
+import java.math.BigDecimal;
+
 public class BrickDaemonConfig {
-    public String host;
-    public Integer port;
+    public String host = "localhost";
+    public Integer port = 4223;
 
-    public Boolean backgroundDiscovery;
-    public Integer backgroundDiscoveryInterval;
+    public Boolean backgroundDiscovery = true;
+    public BigDecimal backgroundDiscoveryInterval = BigDecimal.valueOf(10.0);
 
-    public Boolean auth;
-    public String password;
+    public Boolean auth = false;
+    public String password = "";
 }
