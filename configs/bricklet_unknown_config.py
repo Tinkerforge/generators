dee05e810e6913bd9756b0acddea7b95cc611844
Matthias Bolte <matthias@tinkerforge.com>
2020-03-04 12:13:46 +0100

    Add Unknown TNG and Bricklet devices for Brick Viewer internal use
    This is used to skip device identifier check for unknown devices.

---
+++ configs/bricklet_unknown_config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Unknown Bricklet communication config
+
+com = {
+    'author': 'Matthias Bolte <matthias@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Bricklet',
+    'device_identifier': -21,
+    'name': 'Unknown',
+    'display_name': 'Unknown',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'features': [
+        'comcu_bricklet',
+        'bricklet_get_identity'
+    ],
+    'constant_groups': [],
+    'packets': [],
+    'examples': []
+}
---
+++ configs/tng_unknown_config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# TNG Unknown communication config
+
+com = {
+    'author': 'Matthias Bolte <matthias@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'TNG',
+    'device_identifier': -201,
+    'name': 'Unknown',
+    'display_name': 'Unknown',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'features': [
+        'tng'
+    ],
+    'constant_groups': [],
+    'packets': [],
+    'examples': []
+}
