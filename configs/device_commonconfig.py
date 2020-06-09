c8eacceac52a68a679de46551381718acaa19df2
Olaf Lüke <olaf@tinkerforge.com>
2020-06-09 12:42:23 +0200

    Add Get Identitiy to TNG feature set

--- configs/device_commonconfig.py
+++ configs/device_commonconfig.py
@@ -1154,6 +1154,47 @@ UID-String zu gelangen.
 # function 255 must never be used for anything else than "Get Identity" to allow
 # for calling function 255 without knowing anything about the device.
 common_packets.append({
+'feature': 'tng',
+'type': 'function',
+'function_id': 255,
+'name': 'Get Identity',
+'elements': [('Uid', 'string', 8, 'out', {}),
+             ('Connected Uid', 'string', 8, 'out', {}),
+             ('Position', 'char', 1, 'out', {'range': ('0', '8')}), # TODO: TNG Position?
+             ('Hardware Version', 'uint8', 3, 'out', [{'name': 'Major'}, {'name': 'Minor'}, {'name': 'Revision'}]),
+             ('Firmware Version', 'uint8', 3, 'out', [{'name': 'Major'}, {'name': 'Minor'}, {'name': 'Revision'}]),
+             ('Device Identifier', 'uint16', 1, 'out', {})],
+'since_firmware': {'*': [1, 0, 0]},
+'prototype_in_device': True,
+'doc': ['af', {
+'en':
+"""
+Returns the UID, the UID where the Brick is connected to,
+the position, the hardware and firmware version as well as the
+device identifier.
+
+The position is the position in the stack from '0' (bottom) to '8' (top).
+
+The device identifier numbers can be found :ref:`here <device_identifier>`.
+|device_identifier_constant|
+""",
+'de':
+"""
+Gibt die UID, die UID zu der der Brick verbunden ist, die
+Position, die Hard- und Firmware Version sowie den Device Identifier
+zurück.
+
+Die Position ist die Position im Stack von '0' (unterster Brick) bis '8' (oberster Brick).
+
+Eine Liste der Device Identifier Werte ist :ref:`hier <device_identifier>` zu
+finden. |device_identifier_constant|
+"""
+}]
+})
+
+# function 255 must never be used for anything else than "Get Identity" to allow
+# for calling function 255 without knowing anything about the device.
+common_packets.append({
 'feature': 'brick_get_identity',
 'type': 'function',
 'function_id': 255,
