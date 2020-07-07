30db4b393a6426b6d95db8414bea3b59e4b58a44
Matthias Bolte <matthias@tinkerforge.com>
2020-07-07 13:58:12 +0200

    industrial-analog-out: Mark as fully discontinued

--- configs/bricklet_industrial_analog_out_config.py
+++ configs/bricklet_industrial_analog_out_config.py
@@ -22,7 +22,7 @@ com = {
     },
     'released': True,
     'documented': True,
-    'discontinued': False, # selling remaining stock, replaced by Industrial Analog Out Bricklet 2.0
+    'discontinued': True, # replaced by Industrial Analog Out Bricklet 2.0
     'features': [
         'bricklet_get_identity'
     ],
