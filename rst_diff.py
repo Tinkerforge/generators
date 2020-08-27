208b2a49ad3675a98764a49aeee6fc99f405babc
Matthias Bolte <matthias@tinkerforge.com>
2020-08-27 19:04:05 +0200

    zip-diff,rst-diff: Add copyright header

--- rst_diff.py
+++ rst_diff.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+rST Diff
+Copyright (C) 2019-2020 Matthias Bolte <matthias@tinkerforge.com>
+Copyright (C) 2019-2020 Erik Fleckstein <erik@tinkerforge.com>
+
+rst_diff.py: Tool for diffing API bindings documentation rST files
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
 import sys
 
 if sys.hexversion < 0x3040000:
