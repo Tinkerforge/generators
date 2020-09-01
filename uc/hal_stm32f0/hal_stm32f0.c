2eb4c62564e040dff61fee62f9cb5ef0ba3749d1
Olaf Lüke <olaf@tinkerforge.com>
2020-09-01 14:38:35 +0200

    Always use braces

--- uc/hal_stm32f0/hal_stm32f0.c
+++ uc/hal_stm32f0/hal_stm32f0.c
@@ -253,8 +253,9 @@ const char *tf_hal_strerror(int e_code) {
 #endif
 
 char tf_hal_get_port_name(TF_HalContext *hal, uint8_t port_id) {
-	if(port_id > hal->_port_count)
+	if(port_id > hal->_port_count) {
 		return '?';
+	}
 
 	return 'a' + port_id;
 }
