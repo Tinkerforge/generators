2788c61c004f668577a7b8fc32736e927fdc49ee
Matthias Bolte <matthias@tinkerforge.com>
2020-10-01 15:41:57 +0200

    javascript: Install browserify locally to ensure stable version

--- javascript/generate_javascript_zip.py
+++ javascript/generate_javascript_zip.py
@@ -5,7 +5,7 @@
 JavaScript ZIP Generator
 Copyright (C) 2014 Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
 Copyright (C) 2014 Olaf Lüke <olaf@tinkerforge.com>
-Copyright (C) 2014-2015, 2018 Matthias Bolte <matthias@tinkerforge.com>
+Copyright (C) 2014-2015, 2018, 2020 Matthias Bolte <matthias@tinkerforge.com>
 
 generate_javascript_zip.py: Generator for JavaScript ZIP
 
@@ -33,6 +33,9 @@ if sys.hexversion < 0x3040000:
 
 import os
 import shutil
+import collections
+import json
+import subprocess
 import importlib.util
 import importlib.machinery
 
@@ -105,6 +108,41 @@ class JavaScriptZipGenerator(javascript_common.JavascriptGeneratorTrait, common.
         for example in common.find_device_examples(device, r'^Example.*\.html'):
             shutil.copy(example[1], tmp_browser_examples_device)
 
+    def patch_package_lock(self, path):
+        # https://github.com/Tinkerforge/generators/commit/a05800794b2d1267bc04d6936315bc864cd60425
+        cut_off_date = '2016-11-15'
+
+        with open(path, 'r') as f:
+            source = json.loads(f.read())
+
+        target = {}
+
+        target['requires'] = source['requires']
+        target['lockfileVersion'] = source['lockfileVersion']
+        target['dependencies'] = collections.OrderedDict()
+
+        for package in source['dependencies']:
+            print(package, '...')
+
+            meta = json.loads(subprocess.check_output(['npm', 'view', '--json', package]).decode('utf-8'))
+            candidate_version = None
+            candidate_date = None
+
+            for version in sorted(meta['versions']):
+                date = meta['time'][version]
+
+                if date < cut_off_date and (candidate_version == None or date > candidate_date):
+                    candidate_version = version
+                    candidate_date = date
+
+            print(package, candidate_version, candidate_date)
+
+            if candidate_version != None:
+                target['dependencies'][package] = {'version': candidate_version}
+
+        with open(path, 'w') as f:
+            f.write(json.dumps(target, indent=2))
+
     def finish(self):
         root_dir = self.get_root_dir()
 
@@ -151,17 +189,43 @@ class JavaScriptZipGenerator(javascript_common.JavascriptGeneratorTrait, common.
         shutil.copy(os.path.join(root_dir, 'es5-shim.js'),                  self.tmp_nodejs_source_tinkerforge_dir)
         shutil.copy(os.path.join(root_dir, 'es5-sham.js'),                  self.tmp_nodejs_source_tinkerforge_dir)
 
+        # Ensure local browserify is installed
+        browserify_dir = os.path.join(root_dir, 'browserify')
+        browserify_version_path = os.path.join(browserify_dir, 'version')
+
+        try:
+            with open(browserify_version_path, 'r') as f:
+                browserify_version = int(f.read().strip())
+        except:
+            browserify_version = None
+
+        if browserify_version != 1:
+            print('installing/updating local browserify installation')
+
+            if os.path.exists(browserify_dir):
+                shutil.rmtree(browserify_dir)
+
+            os.mkdir(browserify_dir)
+
+            with open(os.path.join(browserify_dir, 'package.json'), 'w') as f:
+                f.write('{"dependencies": {"browserify": "13.1.1"}}\n')
+
+            with common.ChangedDirectory(browserify_dir):
+                common.execute(['npm', 'install'])
+
+            self.patch_package_lock(os.path.join(browserify_dir, 'package-lock.json'))
+
+            shutil.rmtree(os.path.join(browserify_dir, 'node_modules'))
+
+            with common.ChangedDirectory(browserify_dir):
+                common.execute(['npm', 'install', '--no-save'])
+
+            with open(os.path.join(browserify_dir, 'version'), 'w') as f:
+                f.write('1\n')
+
         # Make Tinkerforge.js for browser with browserify
-        retcode, output = common.check_output_and_error(['browserify', '--version'])
-
-        if retcode != 0:
-            raise common.GeneratorError('Could not get browserify version')
-
-        if tuple([int(n) for n in output.strip('\r\n').split('.')]) < (13, 1, 1):
-            raise common.GeneratorError('Need browserify version >= 13.1.1')
-
         with common.ChangedDirectory(self.tmp_nodejs_source_tinkerforge_dir):
-            args = ['browserify']
+            args = ['node', os.path.join(browserify_dir, 'node_modules', 'browserify', 'bin', 'cmd.js')]
             args.extend(sorted(os.listdir(self.tmp_nodejs_source_tinkerforge_dir)))
             args.append('-o')
             args.append(os.path.join(self.tmp_browser_source_dir, 'Tinkerforge.js'))
