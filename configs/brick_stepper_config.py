c670eb59291b5a2015c568c281882513fb8b5669
Matthias Bolte <matthias@tinkerforge.com>
2021-03-09 11:22:08 +0100

    stepper: Add replacement comment

--- configs/brick_stepper_config.py
+++ configs/brick_stepper_config.py
@@ -22,7 +22,7 @@ com = {
     },
     'released': True,
     'documented': True,
-    'discontinued': True,
+    'discontinued': True, # replaced by Silent Stepper Brick
     'features': [
         'device',
         'brick_get_identity',
