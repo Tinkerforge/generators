28d0b82c021688da95ce73d690304e410043321e
Matthias Bolte <matthias@tinkerforge.com>
2021-11-24 17:03:35 +0100

    uc: Ensure to call tf_tfp_finish_send after tf_tfp_transmit_packet

--- uc/hal_common.c
+++ uc/hal_common.c
@@ -492,6 +492,7 @@ int tf_hal_tick(TF_HAL *hal, uint32_t timeout_us) {
     TF_HALCommon *hal_common = tf_hal_get_common(hal);
     TF_Net *net = hal_common->net;
     uint8_t ignored;
+    int result;
 
     if (net != NULL) {
         tf_net_tick(net);
@@ -502,7 +503,8 @@ int tf_hal_tick(TF_HAL *hal, uint32_t timeout_us) {
                 if (hal_common->tfps[i].spitfp->send_buf[0] == 0) {
                     tf_tfp_inject_packet(&hal_common->tfps[i], &enumerate_request_header, enumerate_request);
                     // TODO: What timeout to use here? If decided, use return value to check for the timeout, maybe increase an error count
-                    (void)! tf_tfp_transmit_packet(&hal_common->tfps[i], false, deadline_us, &ignored); //ignore result for now: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66425#c34
+                    result = tf_tfp_transmit_packet(&hal_common->tfps[i], false, deadline_us, &ignored);
+                    (void)! tf_tfp_finish_send(&hal_common->tfps[i], result, deadline_us); // ignore result for now: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66425#c34
                     hal_common->send_enumerate_request[i] = false;
                 }
             }
@@ -554,9 +556,8 @@ int tf_hal_tick(TF_HAL *hal, uint32_t timeout_us) {
                 tf_tfp_inject_packet(&hal_common->tfps[i], &header, buf);
 
                 // TODO: What timeout to use here? If decided, use return value to check for the timeout, maybe increase an error count
-                (void)! tf_tfp_transmit_packet(&hal_common->tfps[i], false, deadline_us, &ignored); //ignore result for now: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66425#c34
-
-                //tf_spitfp_build_packet(&hal_common->tfps[i].spitfp, false);
+                result = tf_tfp_transmit_packet(&hal_common->tfps[i], false, deadline_us, &ignored);
+                (void)! tf_tfp_finish_send(&hal_common->tfps[i], result, deadline_us); // ignore result for now: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66425#c34
                 dispatched = true;
             }
 
