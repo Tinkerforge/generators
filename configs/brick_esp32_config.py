a253f1ab83d662cc10427b73932da6eb6b834734
Matthias Bolte <matthias@tinkerforge.com>
2022-01-31 18:02:17 +0100

    Add minimal config for ESP32 (Ethernet) Brick

---
+++ configs/brick_esp32_config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# ESP32 Brick communication config
+
+com = {
+    'author': 'Matthias Bolte <matthias@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Brick',
+    'device_identifier': 113,
+    'name': 'ESP32',
+    'display_name': 'ESP32',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': 'TBD',
+        'de': 'TBD'
+    },
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'features': [
+        'device',
+        'brick_get_identity'
+    ],
+    'constant_groups': [],
+    'packets': [],
+    'examples': []
+}
