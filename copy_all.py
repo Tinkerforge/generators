7d60764ca0113dd1323c4cf8f00b3a6f4c0f54df
Matthias Bolte <matthias@tinkerforge.com>
2022-01-31 17:33:07 +0100

    Merge esp32-firmware copy_bindings.py into copy_all.py

--- copy_all.py
+++ copy_all.py
@@ -14,6 +14,7 @@ import socket
 import zipfile
 import tempfile
 import glob
+import re
 import importlib.util
 import importlib.machinery
 
@@ -79,6 +80,93 @@ def files_are_not_the_same(src_file, dest_path):
     else:
         return text_files_are_not_the_same(src_file, dest_path)
 
+def copy_uc_files():
+    verbose = False
+    esp32_software_dir = os.path.realpath(os.path.join(generators_dir, '..', 'esp32-firmware', 'software'))
+    generators_uc_dir = os.path.realpath(os.path.join(generators_dir, 'uc'))
+    bindings_target_dir = os.path.join(esp32_software_dir, 'src', 'bindings')
+    net_arduino_esp32_target_dir = os.path.join(esp32_software_dir, 'src', 'net_arduino_esp32')
+    hal_arduino_esp32_brick_target_dir = os.path.join(esp32_software_dir, 'modules', 'backend', 'esp32_brick', 'hal_arduino_esp32_brick')
+    hal_arduino_esp32_ethernet_brick_target_dir = os.path.join(esp32_software_dir, 'modules', 'backend', 'esp32_ethernet_brick', 'hal_arduino_esp32_ethernet_brick')
+
+    def copy_files(source_dir, target_dir, exclude_pattern=None, include_pattern=None, patch_include=False, header_marker=None):
+        source_names = []
+
+        for name in sorted(os.listdir(source_dir)):
+            if exclude_pattern != None and re.match(exclude_pattern, name) != None:
+                continue
+
+            if include_pattern != None and re.match(include_pattern, name) == None:
+                continue
+
+            source_names.append(name)
+
+        for name in sorted(os.listdir(target_dir)):
+            if name in source_names:
+                continue
+
+            if exclude_pattern != None and re.match(exclude_pattern, name) != None:
+                continue
+
+            if include_pattern != None and re.match(include_pattern, name) == None:
+                continue
+
+
+            target_path = os.path.join(target_dir, name)
+
+            if verbose:
+                print('remove', target_path)
+
+            os.remove(target_path)
+
+        for name in source_names:
+            source_path = os.path.join(source_dir, name)
+            target_path = os.path.join(target_dir, name)
+
+            if os.path.exists(target_path):
+                with open(source_path, 'r') as f:
+                    source_lines = list(f.readlines())
+
+                with open(target_path, 'r') as f:
+                    target_lines = list(f.readlines())
+
+                if len(source_lines) == len(target_lines):
+                    for source_line, target_line in zip(source_lines, target_lines):
+                        if patch_include:
+                            target_line = target_line.replace('#include "bindings/', '#include "../bindings/')
+
+                        if source_line != target_line:
+                            if header_marker != None and source_line.startswith(header_marker) and target_line.startswith(header_marker):
+                                continue
+
+                            break
+                    else:
+                        if verbose:
+                            print('skipping', source_path)
+
+                        continue
+
+            print(' * {0}'.format(name))
+
+            if verbose:
+                print('copy', source_path, target_path)
+
+            with open(source_path, 'rb') as f:
+                data = f.read()
+
+            if patch_include:
+                data = data.replace(b'#include "../bindings/', b'#include "bindings/')
+
+            with open(target_path, 'wb') as f:
+                f.write(data)
+
+    copy_files(generators_uc_dir, bindings_target_dir, include_pattern=r'^.*\.(h|c)$', exclude_pattern=r'^(brick(let)?_.*\.(h|c)|display_names.c)$')
+    copy_files(os.path.join(generators_uc_dir, 'bindings'), bindings_target_dir, include_pattern=r'^(brick(let)?_.*\.(h|c)|display_names.c)$',
+               exclude_pattern=r'^bricklet_stream_test\.(h|c)$', header_marker=' * This file was automatically generated on ')
+    copy_files(os.path.join(generators_uc_dir, 'net_arduino_esp32'), net_arduino_esp32_target_dir, include_pattern=r'^.*\.(h|c|cpp)$')
+    copy_files(os.path.join(generators_uc_dir, 'hal_arduino_esp32_brick'), hal_arduino_esp32_brick_target_dir, include_pattern=r'^.*\.(h|c|cpp)$', patch_include=True)
+    copy_files(os.path.join(generators_uc_dir, 'hal_arduino_esp32_ethernet_brick'), hal_arduino_esp32_ethernet_brick_target_dir, include_pattern=r'^.*\.(h|c|cpp)$', patch_include=True)
+
 def main():
     path = generators_dir
     start_path = os.path.realpath(os.path.join(generators_dir, '..'))
@@ -126,12 +214,14 @@ def main():
                     shutil.copy(src_file, tool_path)
                     print(' * {0}'.format(f))
 
+        print('')
+        print('Copying uC bindings to esp32-firmware:')
+        copy_uc_files()
+
     doc_copy = [('_Brick_', 'Bricks'),
                 ('_Bricklet_', 'Bricklets'),
                 ('IPConnection_', '.')]
-
     to_delete = {'en': {}, 'de': {}}
-
     doc_path = os.path.join(doc_git, '{0}/source/Software')
     labview_image_path = os.path.join(doc_git, 'en/source/Images/Screenshots/LabVIEW')
 
