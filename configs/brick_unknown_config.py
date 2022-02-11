8b275bb4c4f8089f2dc9af2b8a829125abcae556
Matthias Bolte <matthias@tinkerforge.com>
2022-02-11 13:47:59 +0100

    Add Unknown Brick config

---
+++ configs/brick_unknown_config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Redistribution and use in source and binary forms of this file,
+# with or without modification, are permitted. See the Creative
+# Commons Zero (CC0 1.0) License for more details.
+
+# Unknown Brick communication config
+
+com = {
+    'author': 'Matthias Bolte <matthias@tinkerforge.com>',
+    'api_version': [2, 0, 0],
+    'category': 'Brick',
+    'device_identifier': -11,
+    'name': 'Unknown',
+    'display_name': 'Unknown',
+    'manufacturer': 'Tinkerforge',
+    'description': {
+        'en': '',
+        'de': ''
+    },
+    'released': False,
+    'documented': False,
+    'discontinued': False,
+    'features': [
+        'device',
+        'brick_get_identity'
+    ],
+    'constant_groups': [],
+    'packets': [],
+    'examples': []
+}
