3c0f6b3d5ac70781608310e4f6a752a3084bb122
Olaf Lüke <olaf@tinkerforge.com>
2022-04-07 14:42:50 +0200

    Add Factory Reset Function

--- configs/bricklet_evse_v2_config.py
+++ configs/bricklet_evse_v2_config.py
@@ -775,3 +775,23 @@ TODO
 """
 }]
 })
+
+com['packets'].append({
+'type': 'function',
+'name': 'Factory Reset',
+'response_expected': 'true',
+'elements': [('Password', 'uint32', 1, 'in')], # Password: 0x2342FACD
+'since_firmware': [1, 0, 0],
+'doc': ['bf', {
+'en':
+"""
+TODO
+""",
+'de':
+"""
+TODO
+"""
+}]
+})
+
+
