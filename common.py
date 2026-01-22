2faa28f884154805141b2c0ea80cc33b70e9bbdc
Olaf Lüke <olaf@tinkerforge.com>
2026-01-22 14:26:22 +0100

    Add ESP32 co processor exception

--- common.py
+++ common.py
@@ -1784,6 +1784,10 @@ def check_name(name, display_name=None, is_constant=False):
         elif display_name.endswith(' 3.0'):
             display_name_to_check = display_name_to_check.replace(' 3.0', ' V3')
 
+        # Exception for ESP32 co processor Bricklet
+        if ' 2.0 Co' in display_name:
+            display_name_to_check = display_name_to_check.replace(' 2.0 Co', ' V2 Co')
+
         if display_name in ['IO-4', 'IO-16', 'IO-4 2.0', 'IO-16 2.0']: # exceptions for legacy dash rules
             display_name_to_check = display_name_to_check.replace('-', '')
         else:
